dc8a82ee63ea8282d34c8c79f2ab3fa5d7399f41
Adam Langley <agl@chromium.org>
2014-07-30 16:02:14 -0700

    Add visibility rules.
    This change marks public symbols as dynamically exported. This means
    that it becomes viable to build a shared library of libcrypto and libssl
    with -fvisibility=hidden.
    
    On Windows, one not only needs to mark functions for export in a
    component, but also for import when using them from a different
    component. Because of this we have to build with
    |BORINGSSL_IMPLEMENTATION| defined when building the code. Other
    components, when including our headers, won't have that defined and then
    the |OPENSSL_EXPORT| tag becomes an import tag instead. See the #defines
    in base.h
    
    In the asm code, symbols are now hidden by default and those that need
    to be exported are wrapped by a C function.
    
    In order to support Chromium, a couple of libssl functions were moved to
    ssl.h from ssl_locl.h: ssl_get_new_session and ssl_update_cache.
    
    Change-Id: Ib4b76e2f1983ee066e7806c24721e8626d08a261

--- include/openssl/base.h
+++ include/openssl/base.h
@@ -53,6 +53,9 @@
 #ifndef OPENSSL_HEADER_BASE_H
 #define OPENSSL_HEADER_BASE_H
 
+
+/* This file should be the first included by all BoringSSL headers. */
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -86,11 +89,30 @@
 #define OPENSSL_APPLE
 #endif
 
+#if defined(WIN32)
+#define OPENSSL_WINDOWS
+#endif
+
 #define OPENSSL_IS_BORINGSSL
 #define OPENSSL_VERSION_NUMBER 0x10002000
 
+#if defined(OPENSSL_WINDOWS)
 
-/* This file should be the first included by all BoringSSL headers. */
+#if defined(BORINGSSL_IMPLEMENTATION)
+#define OPENSSL_EXPORT __declspec(dllexport)
+#else
+#define OPENSSL_EXPORT __declspec(dllimport)
+#endif
+
+#else  /* defined(OPENSSL_WINDOWS) */
+
+#if defined(BORINGSSL_IMPLEMENTATION)
+#define OPENSSL_EXPORT __attribute__((visibility("default")))
+#else
+#define OPENSSL_EXPORT
+#endif
+
+#endif /* defined(OPENSSL_WINDOWS) */
 
 typedef int ASN1_BOOLEAN;
 typedef int ASN1_NULL;
