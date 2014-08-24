6dc9d79423a14c4b7577d948f10d33cd3c0a5d1f
David Benjamin <davidben@chromium.org>
2014-08-24 03:47:07 -0400

    Add a test to ensure False Start occurs.
    This adds the missing test coverage for
    7e3305eebd7fb06d57e7f25b3bbf9c10d526f7d5.
    
    Change-Id: I8c9f1dc998afa9bb1f6fb2a7872a651037bb4844

--- ssl/test/runner/common.go
+++ ssl/test/runner/common.go
@@ -434,6 +434,11 @@ type ProtocolBugs struct {
 	// SkipHelloVerifyRequest causes a DTLS server to skip the
 	// HelloVerifyRequest message.
 	SkipHelloVerifyRequest bool
+
+	// ExpectFalseStart causes the server to, on full handshakes,
+	// expect the peer to False Start; the server Finished message
+	// isn't send until we receive one byte from the peer.
+	ExpectFalseStart bool
 }
 
 func (c *Config) serverInit() {
