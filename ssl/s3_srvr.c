9301408f4595fec7f785eaffffdfb3a588885272
David Benjamin <davidben@chromium.org>
2014-08-24 03:02:27 -0400

    Revise hash management for reading the Finished message.
    Upstream originally sampled the Finished message's hash at ChangeCipherSpec,
    but our patches to add messages between the two complicated this. Move this to
    match the tri-state pattern now established with EncryptedExtensions.
    
    Change-Id: I5c212f2f85f85bc3b3817adc93971f987a9b4663

--- ssl/s3_srvr.c
+++ ssl/s3_srvr.c
@@ -592,8 +592,11 @@ int ssl3_accept(SSL *s)
 
 		case SSL3_ST_SR_FINISHED_A:
 		case SSL3_ST_SR_FINISHED_B:
-			ret=ssl3_get_finished(s,SSL3_ST_SR_FINISHED_A,
-				SSL3_ST_SR_FINISHED_B);
+		case SSL3_ST_SR_FINISHED_C:
+			ret=ssl3_get_finished(s,
+				SSL3_ST_SR_FINISHED_A,
+				SSL3_ST_SR_FINISHED_B,
+				SSL3_ST_SR_FINISHED_C);
 			if (ret <= 0) goto end;
 			if (s->hit)
 				s->state=SSL_ST_OK;
