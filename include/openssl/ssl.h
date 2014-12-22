5429337a559b0390b4e3a7aaecc3a3fd945e0ef2
David Benjamin <davidben@chromium.org>
2014-12-21 21:27:41 -0500

    Implement all TLS ciphers with stateful AEADs.
    The EVP_CIPHER codepath should no longer be used with TLS. It still exists for
    DTLS and SSLv3. The AEAD construction in TLS does not allow for
    variable-overhead AEADs, so stateful AEADs do not include the length in the ad
    parameter. Rather the AEADs internally append the unpadded length once it is
    known. EVP_aead_rc4_md5_tls is modified to account for this.
    
    Tests are added (and RC4-MD5's regenerated) for each of the new AEADs. The
    cipher tests are all moved into crypto/cipher/test because there's now a lot of
    them and they clutter the directory listing.
    
    In ssl/, the stateful AEAD logic is also modified to account for stateful AEADs
    with a fixed IV component, and for AEADs which use a random nonce (for the
    explicit-IV CBC mode ciphers).
    
    The new implementation fixes a bug/quirk in stateless CBC mode ciphers where
    the fixed IV portion of the keyblock was generated regardless. This is at the
    end, so it's only relevant for EAP-TLS which generates a MSK from the end of
    the key block.
    
    Change-Id: I2d8b8aa11deb43bde2fd733f4f90b5d5b8cb1334

--- include/openssl/ssl.h
+++ include/openssl/ssl.h
@@ -2421,6 +2421,7 @@ OPENSSL_EXPORT void ERR_load_SSL_strings(void);
 #define SSL_F_SSL_connect 294
 #define SSL_F_ssl3_get_v2_client_hello 295
 #define SSL_F_ssl3_get_initial_bytes 296
+#define SSL_F_tls1_enc 297
 #define SSL_R_UNABLE_TO_FIND_ECDH_PARAMETERS 100
 #define SSL_R_DECRYPTION_FAILED_OR_BAD_RECORD_MAC 101
 #define SSL_R_INVALID_NULL_CMD_NAME 102
