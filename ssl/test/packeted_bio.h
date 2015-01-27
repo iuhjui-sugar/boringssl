969f7002c8ab84aa171e3e148c9183c992253a8b
David Benjamin <davidben@chromium.org>
2015-01-27 01:09:43 -0500

    Add DTLS timeout and retransmit tests.
    This extends the packet adaptor protocol to send three commands:
      type command =
        | Packet of []byte
        | Timeout of time.Duration
        | TimeoutAck
    
    When the shim processes a Timeout in BIO_read, it sends TimeoutAck, fails the
    BIO_read, returns out of the SSL stack, advances the clock, calls
    DTLSv1_handle_timeout, and continues.
    
    If the Go side sends Timeout right between sending handshake flight N and
    reading flight N+1, the shim won't read the Timeout until it has sent flight
    N+1 (it only processes packet commands in BIO_read), so the TimeoutAck comes
    after N+1. Go then drops all packets before the TimeoutAck, thus dropping one
    transmit of flight N+1 without having to actually process the packets to
    determine the end of the flight. The shim then sees the updated clock, calls
    DTLSv1_handle_timeout, and re-sends flight N+1 for Go to process for real.
    
    When dropping packets, Go checks the epoch and increments sequence numbers so
    that we can continue to be strict here. This requires tracking the initial
    sequence number of the next epoch.
    
    The final Finished message takes an additional special-case to test. DTLS
    triggers retransmits on either a timeout or seeing a stale flight. OpenSSL only
    implements the former which should be sufficient (and is necessary) EXCEPT for
    the final Finished message. If the peer's final Finished message is lost, it
    won't be waiting for a message from us, so it won't time out anything. That
    retransmit must be triggered on stale message, so we retransmit the Finished
    message in Go.
    
    Change-Id: I3ffbdb1de525beb2ee831b304670a3387877634c

--- ssl/test/packeted_bio.h
+++ ssl/test/packeted_bio.h
@@ -16,17 +16,20 @@
 #define HEADER_PACKETED_BIO
 
 #include <openssl/bio.h>
+#include <openssl/ssl.h>
 
 
 // packeted_bio_create creates a filter BIO for testing protocols which expect
-// datagram BIOs. It implements a reliable datagram socket and reads and writes
-// packets by prefixing each packet with a big-endian 32-bit length. It must be
-// layered over a reliable blocking stream BIO.
+// datagram BIOs. It implements a reliable blocking datagram socket and reads
+// and writes packets. It also accepts simulated read timeouts. If a read
+// timeout is simulated in |BIO_read|, |*out_timeout| set to the timeout and the
+// read will fail. |*out_timeout| must be zero on entry to |BIO_read|; it is an
+// error to not apply the timeout before the next |BIO_read|.
 //
-// Note: packeted_bio_create exists because a SOCK_DGRAM socketpair on OS X is
-// does not block the caller, unlike on Linux. Writes simply fail with
-// ENOBUFS. POSIX also does not guarantee that such sockets are reliable.
-BIO *packeted_bio_create();
+// Note: The read timeout simulation is intended to be used with the async BIO
+// wrapper. It doesn't simulate BIO_CTRL_DGRAM_SET_NEXT_TIMEOUT, used in DTLS's
+// blocking mode.
+BIO *packeted_bio_create(OPENSSL_timeval *out_timeout);
 
 
 #endif  // HEADER_PACKETED_BIO
