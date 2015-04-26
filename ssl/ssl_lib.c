988b59a646fe5b7117cfbb25ed6796bd3de869ed
David Benjamin <davidben@chromium.org>
2015-04-26 15:35:35 -0400

    Promote max_cert_list and max_send_fragment to functions.
    Also size them based on the limits in the quantities they control (after
    checking bounds at the API boundary).
    
    BUG=404754
    
    Change-Id: Id56ba45465a473a1a793244904310ef747f29b63

--- ssl/ssl_lib.c
+++ ssl/ssl_lib.c
@@ -1007,18 +1007,52 @@ uint32_t SSL_CTX_get_mode(const SSL_CTX *ctx) { return ctx->mode; }
 
 uint32_t SSL_get_mode(const SSL *ssl) { return ssl->mode; }
 
+size_t SSL_CTX_get_max_cert_list(const SSL_CTX *ctx) {
+  return ctx->max_cert_list;
+}
+
+void SSL_CTX_set_max_cert_list(SSL_CTX *ctx, size_t max_cert_list) {
+  if (max_cert_list >= (1u << 24)) {
+    /* No handshake message may exceed 2^24-1 bytes. */
+    max_cert_list = (1u << 24) - 1;
+  }
+  ctx->max_cert_list = (uint32_t)max_cert_list;
+}
+
+size_t SSL_get_max_cert_list(const SSL *ssl) {
+  return ssl->max_cert_list;
+}
+
+void SSL_set_max_cert_list(SSL *ssl, size_t max_cert_list) {
+  if (max_cert_list >= (1u << 24)) {
+    /* No handshake message may exceed 2^24-1 bytes. */
+    max_cert_list = (1u << 24) - 1;
+  }
+  ssl->max_cert_list = (uint32_t)max_cert_list;
+}
+
+void SSL_CTX_set_max_send_fragment(SSL_CTX *ctx, size_t max_send_fragment) {
+  if (max_send_fragment < 512) {
+    max_send_fragment = 512;
+  }
+  if (max_send_fragment > SSL3_RT_MAX_PLAIN_LENGTH) {
+    max_send_fragment = SSL3_RT_MAX_PLAIN_LENGTH;
+  }
+  ctx->max_send_fragment = (uint16_t)max_send_fragment;
+}
+
+void SSL_set_max_send_fragment(SSL *ssl, size_t max_send_fragment) {
+  if (max_send_fragment < 512) {
+    max_send_fragment = 512;
+  }
+  if (max_send_fragment > SSL3_RT_MAX_PLAIN_LENGTH) {
+    max_send_fragment = SSL3_RT_MAX_PLAIN_LENGTH;
+  }
+  ssl->max_send_fragment = (uint16_t)max_send_fragment;
+}
+
 long SSL_ctrl(SSL *s, int cmd, long larg, void *parg) {
-  long l;
-
   switch (cmd) {
-    case SSL_CTRL_GET_MAX_CERT_LIST:
-      return s->max_cert_list;
-
-    case SSL_CTRL_SET_MAX_CERT_LIST:
-      l = s->max_cert_list;
-      s->max_cert_list = larg;
-      return l;
-
     case SSL_CTRL_SET_MTU:
       if (larg < (long)dtls1_min_mtu()) {
         return 0;
@@ -1028,13 +1062,6 @@ long SSL_ctrl(SSL *s, int cmd, long larg, void *parg) {
         return larg;
       }
       return 0;
-
-    case SSL_CTRL_SET_MAX_SEND_FRAGMENT:
-      if (larg < 512 || larg > SSL3_RT_MAX_PLAIN_LENGTH) {
-        return 0;
-      }
-      s->max_send_fragment = larg;
-      return 1;
 
     case SSL_CTRL_GET_RI_SUPPORT:
       if (s->s3) {
@@ -1066,14 +1093,6 @@ long SSL_CTX_ctrl(SSL_CTX *ctx, int cmd, long larg, void *parg) {
   long l;
 
   switch (cmd) {
-    case SSL_CTRL_GET_MAX_CERT_LIST:
-      return ctx->max_cert_list;
-
-    case SSL_CTRL_SET_MAX_CERT_LIST:
-      l = ctx->max_cert_list;
-      ctx->max_cert_list = larg;
-      return l;
-
     case SSL_CTRL_SET_SESS_CACHE_SIZE:
       l = ctx->session_cache_size;
       ctx->session_cache_size = larg;
@@ -1092,13 +1111,6 @@ long SSL_CTX_ctrl(SSL_CTX *ctx, int cmd, long larg, void *parg) {
 
     case SSL_CTRL_SESS_NUMBER:
       return lh_SSL_SESSION_num_items(ctx->sessions);
-
-    case SSL_CTRL_SET_MAX_SEND_FRAGMENT:
-      if (larg < 512 || larg > SSL3_RT_MAX_PLAIN_LENGTH) {
-        return 0;
-      }
-      ctx->max_send_fragment = larg;
-      return 1;
 
     default:
       return ctx->method->ssl_ctx_ctrl(ctx, cmd, larg, parg);
