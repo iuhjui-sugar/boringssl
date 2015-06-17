9457dd79ce4672064495b25226b63305cd355001
Matt Braithwaite <mab@google.com>
2015-06-17 15:17:19 -0700

    Restore |BIO_f_base64| from OpenSSL at b4f0d1a, modulo style fixes.
    Change-Id: Ia7f4f4f6d063d882cf3d3ac0f5f33ad8d8cd9875

--- include/openssl/bio.h
+++ include/openssl/bio.h
@@ -569,6 +569,16 @@ OPENSSL_EXPORT int BIO_set_conn_port(BIO *bio, const char *port_str);
 OPENSSL_EXPORT int BIO_set_nbio(BIO *bio, int on);
 
 
+/* Filter BIOs. */
+
+/* Returns a filter |BIO| that base64-encodes data written into it,
+ * and decodes data read out of it. |BIO_gets| is not supported. Call
+ * |BIO_flush| when done writing, to signal that no more data are to
+ * be encoded. The flag |BIO_FLAGS_BASE64_NO_NL| may be set to encode
+ * all the data on one line. */
+OPENSSL_EXPORT const BIO_METHOD *BIO_f_base64(void);
+
+
 /* Datagram BIOs.
  *
  * TODO(fork): not implemented. */
