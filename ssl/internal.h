72fb40442597bf9bfc43ebc6e924451850b36eaa
Sigbjorn Vik <sigbjorn@opera.com>
2015-06-29 15:07:26 +0200

    Make key exchange bit strength available
    Change-Id: I354141da432a08f71704c9683f298b361362483d

--- ssl/internal.h
+++ ssl/internal.h
@@ -1083,6 +1083,7 @@ int ssl3_alert_code(int code);
 char ssl_early_callback_init(struct ssl_early_callback_ctx *ctx);
 int tls1_ec_curve_id2nid(uint16_t curve_id);
 int tls1_ec_nid2curve_id(uint16_t *out_curve_id, int nid);
+int tls1_ec_curve_id2name(uint16_t curve_id, char** name);
 
 /* tls1_check_curve parses ECParameters out of |cbs|, modifying it. It
  * checks the curve is one of our preferences and writes the
