a77160509ff896c58fd574bf2f62a6ad9dac5b76
Matt Braithwaite <mab@google.com>
2015-09-02 19:48:16 -0700

    Restore the NULL-SHA ciphersuite.  (Alas.)
    Change-Id: Ia5398f3b86a13fb20dba053f730b51a0e57b9aa4

--- include/openssl/ssl.h
+++ include/openssl/ssl.h
@@ -229,6 +229,9 @@ OPENSSL_EXPORT int SSL_CIPHER_is_AESGCM(const SSL_CIPHER *cipher);
  * CHACHA20_POLY1305. */
 OPENSSL_EXPORT int SSL_CIPHER_is_CHACHA20POLY1305(const SSL_CIPHER *cipher);
 
+/* SSL_CIPHER_is_NULL returns one if |cipher| does not encrypt. */
+OPENSSL_EXPORT int SSL_CIPHER_is_NULL(const SSL_CIPHER *cipher);
+
 /* SSL_CIPHER_get_name returns the OpenSSL name of |cipher|. */
 OPENSSL_EXPORT const char *SSL_CIPHER_get_name(const SSL_CIPHER *cipher);
 
