204e3ae3ae0217aee90535c2f544f3e160b82222
Brian Smith <brian@briansmith.org>
2015-11-17 22:00:09 -1000

    Share common definitions of |TOBN| and |BIGNUM_STATIC|.
    Previously, crypto/dh defined and crypto/ec had |TOBN| macros that
    did the same thing, but which took their arguments in the opposite
    order. Make the code consistently use the same macro. Also, make
    |STATIC_BIGNUM| available for internal use outside of crypto/bn.
    
    Change-Id: Ide57f6a5b74ea95b3585724c7e1a630c82a864d9

--- crypto/bn/bn.c
+++ crypto/bn/bn.c
@@ -166,11 +166,10 @@ void BN_clear(BIGNUM *bn) {
 }
 
 const BIGNUM *BN_value_one(void) {
-  static const BN_ULONG data_one = 1;
-  static const BIGNUM const_one = {(BN_ULONG *)&data_one, 1, 1, 0,
-                                   BN_FLG_STATIC_DATA};
+  static const BN_ULONG ONE_LIMBS[1] = { 1 };
+  static const BIGNUM ONE = STATIC_BIGNUM(ONE_LIMBS);
 
-  return &const_one;
+  return &ONE;
 }
 
 void BN_with_flags(BIGNUM *out, const BIGNUM *in, int flags) {
