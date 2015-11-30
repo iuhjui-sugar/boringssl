1e711593458648a1f344022db56d5c0ed757e018
David Benjamin <davidben@chromium.org>
2015-11-30 18:48:18 -0500

    Finish the transition to the new ChaCha20-Poly1305 APIs.
    The consumers have all been updated, so we can remove the compatibility
    names and point EVP_aead_chacha20_poly1305 at the standardized version.
    
    Change-Id: Ibbb70ec1860d6ac6a7e1d7b45e70fe692bf5ebe5

--- crypto/cipher/e_chacha20poly1305.c
+++ crypto/cipher/e_chacha20poly1305.c
@@ -245,7 +245,7 @@ static const EVP_AEAD aead_chacha20_poly1305 = {
     NULL,               /* get_iv */
 };
 
-const EVP_AEAD *EVP_aead_chacha20_poly1305_rfc7539(void) {
+const EVP_AEAD *EVP_aead_chacha20_poly1305(void) {
   return &aead_chacha20_poly1305;
 }
 
@@ -303,9 +303,5 @@ static const EVP_AEAD aead_chacha20_poly1305_old = {
 };
 
 const EVP_AEAD *EVP_aead_chacha20_poly1305_old(void) {
-  return &aead_chacha20_poly1305_old;
-}
-
-const EVP_AEAD *EVP_aead_chacha20_poly1305(void) {
   return &aead_chacha20_poly1305_old;
 }
