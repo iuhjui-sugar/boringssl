3cd2d08c4115d70783b57dec3210176fc918febf
David Benjamin <davidben@chromium.org>
2016-01-03 18:00:55 -0800

    Remove old_priv_decode from EVP_PKEY_ASN1_METHOD.
    This removes the dependency on DSA ASN.1 hooks which, aside from the
    bare minimum needed for SPKI and PKCS8, is still using the legacy ASN.1
    stack.
    
    BUG=499653
    
    Change-Id: I8acc4e4a2ed5860945603edac18b1926c47690a0

--- crypto/evp/internal.h
+++ crypto/evp/internal.h
@@ -124,13 +124,7 @@ struct evp_pkey_asn1_method_st {
   int (*param_copy)(EVP_PKEY *to, const EVP_PKEY *from);
   int (*param_cmp)(const EVP_PKEY *a, const EVP_PKEY *b);
 
-
   void (*pkey_free)(EVP_PKEY *pkey);
-
-  /* Legacy functions for old PEM */
-
-  int (*old_priv_decode)(EVP_PKEY *pkey, const uint8_t **pder,
-                         int derlen);
 } /* EVP_PKEY_ASN1_METHOD */;
 
 
