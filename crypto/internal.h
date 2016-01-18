6f59f9845e52d65756a5d7b410a768f21b30e118
Brian Smith <brian@briansmith.org>
2016-01-17 20:12:57 -1000

    Use |alignas| for alignment.
    MSVC doesn't have stdalign.h and so doesn't support |alignas| in C
    code. Define |alignas(x)| as a synonym for |__decltype(align(x))|
    instead for it.
    
    This also fixes -Wcast-qual warnings in rsaz_exp.c.
    
    Change-Id: Ifce9031724cb93f5a4aa1f567e7af61b272df9d5

--- crypto/internal.h
+++ crypto/internal.h
@@ -119,6 +119,7 @@
 #pragma warning(pop)
 #else
 #include <pthread.h>
+#include <stdalign.h>
 #endif
 
 #if defined(__cplusplus)
@@ -176,6 +177,10 @@ void OPENSSL_cpuid_setup(void);
 
 #if !defined(inline)
 #define inline __inline
+#endif
+
+#if defined(_MSC_VER) && !defined(__cplusplus)
+#define alignas(x) __declspec(align(x))
 #endif
 
 
