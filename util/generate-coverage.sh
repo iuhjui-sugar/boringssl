3543ba166e3583929ca49182576cc60ef1503b7d
Steven Valdez <svaldez@google.com>
2016-03-25 13:46:48 -0400

    Adding x86 (32-bit) support for line coverage.
    We also need to remove the '.file' macro from x86gas.pl to prevent the
    filenames from being overridden from those provided by the build system.
    
    Change-Id: I2b2a7a1fcfe6a7113833e90e9f1ffa70e44f958c

--- util/generate-coverage.sh
+++ util/generate-coverage.sh
@@ -27,8 +27,17 @@ if [ -n "$1" ]; then
 fi
 
 cd "$BUILD"
-cmake "$SRC" -GNinja -DCMAKE_C_FLAGS='-fprofile-arcs -ftest-coverage' \
-  -DCMAKE_CXX_FLAGS='-fprofile-arcs -ftest-coverage' -DCMAKE_ASM_FLAGS='-Wa,-g'
+if [ "$TEST_ARCH" = "x86" ]; then
+  cmake "$SRC" -GNinja -DCMAKE_TOOLCHAIN_FILE='util/32-bit-toolchain.cmake' \
+    -DCMAKE_C_FLAGS='-m32 -fprofile-arcs -ftest-coverage' \
+    -DCMAKE_CXX_FLAGS='-m32 -fprofile-arcs -ftest-coverage' \
+    -DCMAKE_ASM_FLAGS='-m32 -Wa,-g'
+else
+  cmake "$SRC" -GNinja -DCMAKE_C_FLAGS='-fprofile-arcs -ftest-coverage' \
+    -DCMAKE_CXX_FLAGS='-fprofile-arcs -ftest-coverage' \
+    -DCMAKE_ASM_FLAGS='-Wa,-g'
+fi
+
 ninja
 
 cp -r "$SRC/crypto" "$SRC/decrepit" "$SRC/include" "$SRC/ssl" "$SRC/tool" \
