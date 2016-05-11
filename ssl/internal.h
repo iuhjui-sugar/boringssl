3a1b7eb95c127675d824f01fea29cef493d6226c
Taylor Brandstetter <deadbeef@google.com>
2016-05-10 19:30:28 -0700

    Adding a method to change the initial DTLS retransmission timer value.
    This allows an application to override the default of 1 second, which
    is what's instructed in RFC 6347 but is not an absolute requirement.
    
    Change-Id: I0bbb16e31990fbcab44a29325b6ec7757d5789e5

--- ssl/internal.h
+++ ssl/internal.h
@@ -956,8 +956,13 @@ typedef struct dtls1_state_st {
    * timeout. */
   struct timeval next_timeout;
 
-  /* Timeout duration */
-  unsigned short timeout_duration;
+  /* timeout_duration_ms is the timeout duration in milliseconds. */
+  unsigned timeout_duration_ms;
+
+  /* initial_timeout_duration_ms is the default timeout duration in
+   * milliseconds. Applied to timeout_duration_ms every time the timer is
+   * restarted. */
+  unsigned initial_timeout_duration_ms;
 } DTLS1_STATE;
 
 extern const SSL3_ENC_METHOD TLSv1_enc_data;
