40706b7e8fa63463638acd44415fdbc239bf3565
Gong Su <gongsugongsu@gmail.com>
2016-07-07 18:36:31 -0400

    Support for s390x and big endian
    Change-Id: Ib60375ef32cd68917564fbc502c140ca94ef3b20

--- include/openssl/base.h
+++ include/openssl/base.h
@@ -83,7 +83,7 @@ extern "C" {
 #elif defined(__arm) || defined(__arm__) || defined(_M_ARM)
 #define OPENSSL_32_BIT
 #define OPENSSL_ARM
-#elif defined(__PPC64__) || defined(__powerpc64__)
+#elif defined(__PPC64__) || defined(__powerpc64__) || defined(__s390x__)
 #define OPENSSL_64_BIT
 #elif defined(__mips__) && !defined(__LP64__)
 #define OPENSSL_32_BIT
