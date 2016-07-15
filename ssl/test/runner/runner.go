47e5f780c92eab052258f11c324ba30d6fd291e5
David Benjamin <davidben@google.com>
2016-07-14 23:10:43 -0400

    Check for buffered handshake messages on cipher change in DTLS.
    This is the equivalent of FragmentAcrossChangeCipherSuite for DTLS. It
    is possible for us to, while receiving pre-CCS handshake messages, to
    buffer up a message with sequence number meant for a post-CCS Finished.
    When we then get to the new epoch and attempt to read the Finished, we
    will process the buffered Finished although it was sent with the wrong
    encryption.
    
    Move ssl_set_{read,write}_state to SSL_PROTOCOL_METHOD hooks as this is
    a property of the transport. Notably, read_state may fail. In DTLS
    check the handshake buffer size. We could place this check in
    read_change_cipher_spec, but TLS 1.3 has no ChangeCipherSpec message, so
    we will need to implement this at the cipher change point anyway. (For
    now, there is only an assert on the TLS side. This will be replaced with
    a proper check in TLS 1.3.)
    
    Change-Id: Ia52b0b81e7db53e9ed2d4f6d334a1cce13e93297

--- ssl/test/runner/runner.go
+++ ssl/test/runner/runner.go
@@ -6141,8 +6141,6 @@ func addChangeCipherSpecTests() {
 	// rejected. Test both with and without handshake packing to handle both
 	// when the partial post-CCS message is in its own record and when it is
 	// attached to the pre-CCS message.
-	//
-	// TODO(davidben): Fix and test DTLS as well.
 	for _, packed := range []bool{false, true} {
 		var suffix string
 		if packed {
@@ -6225,6 +6223,25 @@ func addChangeCipherSpecTests() {
 			expectedError: ":UNEXPECTED_RECORD:",
 		})
 	}
+
+	// Test that, in DTLS, ChangeCipherSpec is not allowed when there are
+	// messages in the handshake queue. Do this by testing the server
+	// reading the client Finished, reversing the flight so Finished comes
+	// first.
+	testCases = append(testCases, testCase{
+		protocol: dtls,
+		testType: serverTest,
+		name:     "SendUnencryptedFinished-DTLS",
+		config: Config{
+			MaxVersion: VersionTLS12,
+			Bugs: ProtocolBugs{
+				SendUnencryptedFinished:   true,
+				ReverseHandshakeFragments: true,
+			},
+		},
+		shouldFail:    true,
+		expectedError: ":BUFFERED_MESSAGES_ON_CIPHER_CHANGE:",
+	})
 
 	// Test that early ChangeCipherSpecs are handled correctly.
 	testCases = append(testCases, testCase{
