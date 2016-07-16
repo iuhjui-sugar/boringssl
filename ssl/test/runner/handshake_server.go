90bda4d4374f452c00e06c3c655e45decab4f918
Nick Harper <nharper@chromium.org>
2016-07-16 17:47:31 +0200

    Implement HelloRetryRequest in go
    Change-Id: Ibde837040d2332bc8570589ba5be9b32e774bfcf

--- ssl/test/runner/handshake_server.go
+++ ssl/test/runner/handshake_server.go
@@ -352,8 +352,46 @@ Curves:
 		}
 
 		if selectedKeyShare == nil {
-			// TODO(davidben,nharper): Implement HelloRetryRequest.
-			return errors.New("tls: HelloRetryRequest not implemented")
+			// Send HelloRetryRequest
+			hrrMsg := helloRetryRequestMsg{}
+			hrrMsg.vers = VersionTLS13
+			hrrMsg.cipherSuite = hs.hello.cipherSuite
+			hrrMsg.selectedGroup = selectedCurve
+			hs.writeServerHash(hrrMsg.marshal())
+			c.writeRecord(recordTypeHandshake, hrrMsg.marshal())
+
+			// Read new ClientHello
+			newMsg, err := c.readHandshake()
+			if err != nil {
+				return err
+			}
+			newClientHello, ok := newMsg.(*clientHelloMsg)
+			if !ok {
+				c.sendAlert(alertUnexpectedMessage)
+				return unexpectedMessageError(newClientHello, newMsg)
+			}
+			hs.writeClientHash(newClientHello.marshal())
+
+			// Check that the new ClientHello matches the old ClientHello, except for
+			// the addition of the new KeyShareEntry at the end of the list, and
+			// removing the EarlyDataIndication extension (if present).
+			// [tls13-spec 4.1.1]
+			newKeyShares := newClientHello.keyShares
+			if len(newKeyShares) == 0 || newKeyShares[len(newKeyShares)-1].group != selectedCurve {
+				return errors.New("tls: KeyShare from HelloRetryRequest not present in new ClientHello")
+			}
+			oldClientHelloCopy := *hs.clientHello
+			oldClientHelloCopy.raw = nil
+			oldClientHelloCopy.hasEarlyData = false
+			oldClientHelloCopy.earlyDataContext = nil
+			newClientHelloCopy := *newClientHello
+			newClientHelloCopy.raw = nil
+			newClientHelloCopy.keyShares = newKeyShares[:len(newKeyShares)-1]
+			if !oldClientHelloCopy.equal(&newClientHelloCopy) {
+				return errors.New("tls: new ClientHello does not match")
+			}
+
+			selectedKeyShare = &newKeyShares[len(newKeyShares)-1]
 		}
 
 		// Once a curve has been selected and a key share identified,
