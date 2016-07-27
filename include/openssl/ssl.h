6ad612d96f5b806569c98ae9cdd474fb0fd1ce42
Steven Valdez <svaldez@google.com>
2016-07-27 11:10:52 -0400

    Adding NewSessionTicket.
    We will now send tickets as a server and accept them as a
    client. Correctly offering and resuming them in the handshake will be
    implemented in a follow-up.
    
    Change-Id: I304320a29c4ffe564fa9c00642a4ace96ff8d871

--- include/openssl/ssl.h
+++ include/openssl/ssl.h
@@ -572,7 +572,7 @@ OPENSSL_EXPORT int DTLSv1_handle_timeout(SSL *ssl);
 #define DTLS1_VERSION 0xfeff
 #define DTLS1_2_VERSION 0xfefd
 
-#define TLS1_3_DRAFT_VERSION 13
+#define TLS1_3_DRAFT_VERSION 14
 
 /* SSL_CTX_set_min_version sets the minimum protocol version for |ctx| to
  * |version|. */
@@ -3702,7 +3702,10 @@ struct ssl_session_st {
   uint8_t original_handshake_hash[EVP_MAX_MD_SIZE];
   unsigned original_handshake_hash_len;
 
-  uint32_t tlsext_tick_lifetime_hint; /* Session lifetime hint in seconds */
+  uint32_t ticket_lifetime_hint; /* Session lifetime hint in seconds */
+
+  uint32_t ticket_flags;
+  uint32_t ticket_age_add;
 
   /* extended_master_secret is true if the master secret in this session was
    * generated using EMS and thus isn't vulnerable to the Triple Handshake
