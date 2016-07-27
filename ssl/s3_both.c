61d141d8ffb2f09eb6e37ffa3e5649fca40a3e30
David Benjamin <davidben@google.com>
2016-07-27 14:03:33 -0400

    Use SSL3_HM_HEADER_LENGTH a bit more.
    Somewhat clearer what it's for than just 4.
    
    Change-Id: Ie7bb89ccdce188d61741da203acd624b49b69058

--- ssl/s3_both.c
+++ ssl/s3_both.c
@@ -549,7 +549,7 @@ again:
   }
 
   /* Read the message header, if we haven't yet. */
-  int ret = extend_handshake_buffer(ssl, 4);
+  int ret = extend_handshake_buffer(ssl, SSL3_HM_HEADER_LENGTH);
   if (ret <= 0) {
     return ret;
   }
@@ -565,7 +565,7 @@ again:
   }
 
   /* Read the message body, if we haven't yet. */
-  ret = extend_handshake_buffer(ssl, 4 + msg_len);
+  ret = extend_handshake_buffer(ssl, SSL3_HM_HEADER_LENGTH + msg_len);
   if (ret <= 0) {
     return ret;
   }
@@ -576,8 +576,8 @@ again:
                       ssl->init_buf->data, ssl->init_buf->length);
 
   ssl->s3->tmp.message_type = ((const uint8_t *)ssl->init_buf->data)[0];
-  ssl->init_msg = (uint8_t*)ssl->init_buf->data + 4;
-  ssl->init_num = ssl->init_buf->length - 4;
+  ssl->init_msg = (uint8_t*)ssl->init_buf->data + SSL3_HM_HEADER_LENGTH;
+  ssl->init_num = ssl->init_buf->length - SSL3_HM_HEADER_LENGTH;
 
   /* Ignore stray HelloRequest messages. Per RFC 5246, section 7.4.1.1, the
    * server may send HelloRequest at any time. */
