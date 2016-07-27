a2d1eb50a594fed91de5575cfae4ba4eadf8eae3
Steven Valdez <svaldez@google.com>
2016-07-27 11:10:52 -0400

    Adding NewSessionTicket.
    We will now send tickets as a server and accept them as a
    client. Correctly offering and resuming them in the handshake will be
    implemented in a follow-up.
    
    Change-Id: I304320a29c4ffe564fa9c00642a4ace96ff8d871

--- ssl/test/bssl_shim.cc
+++ ssl/test/bssl_shim.cc
@@ -1057,8 +1057,8 @@ static bool CheckHandshakeProperties(SSL *ssl, bool is_resume) {
     bool expect_new_session =
         !config->expect_no_session &&
         (!SSL_session_reused(ssl) || config->expect_ticket_renewal) &&
-        /* TODO(svaldez): Implement Session Resumption. */
-        SSL_version(ssl) != TLS1_3_VERSION;
+        // Session tickets are sent post-handshake in TLS 1.3.
+        GetProtocolVersion(ssl) >= TLS1_3_VERSION;
     if (expect_new_session != GetTestState(ssl)->got_new_session) {
       fprintf(stderr,
               "new session was%s cached, but we expected the opposite\n",
@@ -1216,6 +1216,14 @@ static bool CheckHandshakeProperties(SSL *ssl, bool is_resume) {
     }
   }
   return true;
+}
+
+static uint16_t GetProtocolVersion(const SSL *ssl) {
+  uint16_t version = SSL_version(ssl);
+  if (!SSL_is_dtls(ssl)) {
+    return version;
+  }
+  return 0x0201 + ~version;
 }
 
 // DoExchange runs a test SSL exchange against the peer. On success, it returns
@@ -1569,9 +1577,22 @@ static bool DoExchange(ScopedSSL_SESSION *out_session, SSL_CTX *ssl_ctx,
 
   if (!config->is_server && !config->false_start &&
       !config->implicit_handshake &&
+      // Session tickets are sent post-handshake in TLS 1.3.
+      GetProtocolVersion(ssl) < TLS1_3_VERSION &&
       GetTestState(ssl.get())->got_new_session) {
     fprintf(stderr, "new session was established after the handshake\n");
     return false;
+  }
+
+  if (SSL_version(ssl.get()) == TLS1_3_VERSION && !config->is_server) {
+    bool expect_new_session =
+        !config->expect_no_session && !config->shim_shuts_down;
+    if (expect_new_session != GetTestState(ssl.get())->got_new_session) {
+      fprintf(stderr,
+              "new session was%s cached, but we expected the opposite\n",
+              GetTestState(ssl.get())->got_new_session ? "" : " not");
+      return false;
+    }
   }
 
   if (out_session) {
