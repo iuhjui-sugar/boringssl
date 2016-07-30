a1eff4cfd4e44c44812ca15de12f51cdac964a59
Brian Smith <brian@briansmith.org>
2016-07-29 16:19:46 -1000

    Calculate inverse in |BN_MONT_CTX_set| in constant time w.r.t. modulus.
    Simplify the calculation of the Montgomery constants in
    |BN_MONT_CTX_set|, making the inversion constant-time. It should also
    be faster by avoiding any use of the |BIGNUM| API in favor of using
    only 64-bit arithmetic.
    
    Now it's obvious how it works. /s
    
    Change-Id: I59a1e1c3631f426fbeabd0c752e0de44bcb5fd75

---
+++ crypto/bn/montgomery_inv.c
@@ -0,0 +1,124 @@
+/* Copyright 2016 Brian Smith.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+
+#include <openssl/bn.h>
+
+#include <assert.h>
+
+#include "internal.h"
+#include "../internal.h"
+
+
+static uint64_t bn_neg_mod_inv_u64(uint64_t b);
+
+OPENSSL_COMPILE_ASSERT(BN_MONT_CTX_N0_LIMBS == 1 || BN_MONT_CTX_N0_LIMBS == 2,
+                       BN_MONT_CTX_N0_LIMBS_VALUE_INVALID);
+OPENSSL_COMPILE_ASSERT(sizeof(uint64_t) ==
+                       BN_MONT_CTX_N0_LIMBS * sizeof(BN_ULONG),
+                       BN_MONT_CTX_N0_LIMBS_DOES_NOT_MATCH_UINT64_T);
+
+/* lg r */
+#define LG_LITTLE_R (BN_MONT_CTX_N0_LIMBS * BN_BITS2)
+
+uint64_t bn_mont_n0(const BIGNUM *n) {
+  /* r == 2**(BN_MONT_CTX_N0_LIMBS * BN_BITS2) and kLgR == lg(r). This ensures
+   * that we can do integer division by |r| by simply ignoring
+   * |BN_MONT_CTX_N0_LIMBS| limbs. Similarly, we can calculate values modulo
+   * |r| by just looking at the lowest |BN_MONT_CTX_N0_LIMBS| limbs. This is
+   * what makes Montgomery multiplication efficient.
+   *
+   * As shown in Algorithm 1 of "Fast Prime Field Elliptic Curve Cryptography
+   * with 256 Bit Primes" by Shay Gueron and Vlad Krasnov, in the loop of a
+   * multi-limb Montgomery multiplication of |a * b (mod N)|, given the
+   * unreduced product |t == a * b|, we repeatedly calculate:
+   *
+   *    t1 := t % r         |t1| is |t|'s lowest limb (see previous paragraph).
+   *    t2 := t1*n0*N
+   *    t3 := t + t2
+   *    t := t3 / r         copy all limbs of |t3| except the lowest to |t|.
+   *
+   * In the last step, it would only make sense to ignore the lowest limb of
+   * |t3| if it were zero. The middle steps ensure that this is the case:
+   *
+   *                            t3 ==  0 (mod r)
+   *                        t + t2 ==  0 (mod r)
+   *                   t + t1*n0*N ==  0 (mod r)
+   *                       t1*n0*N == -t (mod r)
+   *                        t*n0*N == -t (mod r)
+   *                          n0*N == -1 (mod r)
+   *                            n0 == -1/n (mod r)
+   *
+   * Thus, in each iteration of the loop, we multiply by the constant factor
+   * |n0|, the negative inverse of |N| (mod r). */
+
+  /* n_mod_r = n % r. As explained above, this is done by taking the lowest
+   * |BN_MONT_CTX_N0_LIMBS| limbs of |n|. */
+  uint64_t n_mod_r = n->d[0];
+#if BN_MONT_CTX_N0_LIMBS == 2
+  if (n->top > 1) {
+    n_mod_r |= (uint64_t)n->d[1] << BN_BITS2;
+  }
+#endif
+
+  return bn_neg_mod_inv_u64(n_mod_r);
+}
+
+/* bn_neg_mod_inv_u64 calculates the negative inverse modulo |r|; i.e. it
+ * calculates |v| such that |u*r - v*b == 1|. |r| is defined in |bn_mont_n0|.
+ * |b| must be odd.
+ *
+ * Most GCD implementations return values such that |u*a + v*b == 1|, so the
+ * caller would have to negate the resultant |v| for the purpose of Montgomery
+ * multiplication. This implementation does the negation implicitly by doing
+ * the computations as a difference instead of a sum.
+ *
+ * This is derived from |xbinGCD| in the "Montgomery Multiplication" chapter of
+ * "Hacker's Delight" by Henry S. Warren, Jr.:
+ * http://www.hackersdelight.org/MontgomeryMultiplication.pdf.
+ *
+ * This is inspired by Joppe W. Bos's "Constant Time Modular Inversion"
+ * http://www.joppebos.com/files/CTInversion.pdf so that the inversion is
+ * constant-time with respect to |b|. We assume |a| is not secret. We assume
+ * uint64_t additions, subtractions, shifts, and bitwise operations are all
+ * constant time, which may be a large leap of faith on 32-bit targets. We
+ * avoid division and multiplication, which tend to be the most problematic in
+ * terms of timing leaks. */
+static uint64_t bn_neg_mod_inv_u64(uint64_t b) {
+  /* alpha == 2**(lg r - 1) == r / 2. */
+  static const uint64_t alpha = (uint64_t)1 << (LG_LITTLE_R - 1);
+
+  assert(b % 2 == 1);
+  const uint64_t beta = b;
+
+  /* |a /= 2|, if |a| were passed as a parameter. */
+  uint64_t u = 1;
+  uint64_t v = 0;
+
+  /* The invariant maintained from here on is: r = u*2*alpha - v*beta. */
+  for (size_t i = 0; i < LG_LITTLE_R; ++i) {
+    uint64_t u_is_odd = UINT64_C(0) - (u & 1); /* Either 0xff..ff or 0. */
+
+    /* Delete a common factor of 2 in u and v if |u| is even. Otherwise, set
+     * |u = (u + beta) / 2| and |v = (v / 2) + alpha|. */
+
+    /* The addition can overflow, so use Dietz's method for it. */
+    uint64_t beta_if_u_is_odd = beta & u_is_odd; /* Either |beta| or 0. */
+    u = ((u ^ beta_if_u_is_odd) >> 1) + (u & beta_if_u_is_odd);
+
+    uint64_t alpha_if_u_is_odd = alpha & u_is_odd; /* Either |alpha| or 0. */
+    v = (v >> 1) + alpha_if_u_is_odd;
+  }
+
+  return v;
+}
