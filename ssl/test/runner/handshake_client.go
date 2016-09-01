9ace5a13819faa5904a8953526ff02f256f92c88
Steven Valdez <svaldez@google.com>
2016-09-01 12:29:49 -0400

    Updating NewSessionTicket message and updating PSK to Draft 15.
    Adding a draftVersion field in order to prevent cross-draft resumption,
    this is repurposing the previous ticketFlags field, which is okay since
    it hasn't been used in production and will also act as an unknown draft
    version if resuming from old tickets.
    
    BUG=77
    
    Change-Id: Id8c45e98c4c22cdd437cbba1e9375239e123b261

--- ssl/test/runner/handshake_client.go
+++ ssl/test/runner/handshake_client.go
@@ -252,10 +252,8 @@ NextCipherSuite:
 		if session.vers >= VersionTLS13 || c.config.Bugs.SendBothTickets {
 			// TODO(nharper): Support sending more
 			// than one PSK identity.
-			if session.ticketFlags&ticketAllowDHEResumption != 0 || c.config.Bugs.SendBothTickets {
-				hello.pskIdentities = [][]uint8{ticket}
-				hello.cipherSuites = append(hello.cipherSuites, ecdhePSKSuite(session.cipherSuite))
-			}
+			hello.pskIdentities = [][]uint8{ticket}
+			hello.cipherSuites = append(hello.cipherSuites, ecdhePSKSuite(session.cipherSuite))
 		}
 
 		if session.vers < VersionTLS13 || c.config.Bugs.SendBothTickets {
