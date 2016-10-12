15cb5217e8eff41068d1a7886d6d6a5d0ad2ec8c
David Benjamin <davidben@google.com>
2016-10-12 10:35:18 -0400

    Fix fuzzer signatures.
    They take a const pointer.
    
    BUG=chromium:655016
    
    Change-Id: Id6c7584c7a875e822b1fbff72163c888d02a9f44

--- fuzz/cert.cc
+++ fuzz/cert.cc
@@ -15,7 +15,7 @@
 #include <openssl/mem.h>
 #include <openssl/x509.h>
 
-extern "C" int LLVMFuzzerTestOneInput(uint8_t *buf, size_t len) {
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *buf, size_t len) {
   const uint8_t *bufp = buf;
   X509 *x509 = d2i_X509(NULL, &bufp, len);
   if (x509 != NULL) {
--- fuzz/privkey.cc
+++ fuzz/privkey.cc
@@ -14,7 +14,7 @@
 
 #include <openssl/evp.h>
 
-extern "C" int LLVMFuzzerTestOneInput(uint8_t *buf, size_t len) {
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *buf, size_t len) {
   const uint8_t *bufp = buf;
   EVP_PKEY_free(d2i_AutoPrivateKey(NULL, &bufp, len));
   return 0;
