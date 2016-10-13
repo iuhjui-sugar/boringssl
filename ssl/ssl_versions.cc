30360176c55c52f17d1e25e0b03b44b9214859ed
Steven Valdez <svaldez@google.com>
2016-10-13 14:33:35 -0400

    Enabling TLS 1.3 (DRAFT).
    Change-Id: I2e4f0db3b8630f990911c8e104f60c048bb7450d

--- ssl/ssl_versions.cc
+++ ssl/ssl_versions.cc
@@ -134,7 +134,7 @@ static int set_max_version(const SSL_PROTOCOL_METHOD *method, uint16_t *out,
                            uint16_t version) {
   /* Zero is interpreted as the default maximum version. */
   if (version == 0) {
-    *out = TLS1_2_VERSION;
+    *out = TLS1_3_VERSION;
     return 1;
   }
 
@@ -250,11 +250,14 @@ int SSL_version(const SSL *ssl) {
 
 static const char *ssl_get_version(int version) {
   switch (version) {
-    /* Report TLS 1.3 draft version as TLS 1.3 in the public API. */
     case TLS1_3_DRAFT_VERSION:
+      return "TLSv1.3 Draft 18";
+
     case TLS1_3_EXPERIMENT_VERSION:
+      return "TLSv1.3 Experiment";
+
     case TLS1_3_RECORD_TYPE_EXPERIMENT_VERSION:
-      return "TLSv1.3";
+      return "TLSv1.3 Record Type Experiment";
 
     case TLS1_2_VERSION:
       return "TLSv1.2";
