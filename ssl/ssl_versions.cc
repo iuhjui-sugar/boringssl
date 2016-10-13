fe87cf59cb2d64f09c92bbec0f8cbb50dddcfc15
Steven Valdez <svaldez@google.com>
2016-10-13 14:33:35 -0400

    Enabling TLS 1.3 (DRAFT).
    Change-Id: I2e4f0db3b8630f990911c8e104f60c048bb7450d

--- ssl/ssl_versions.cc
+++ ssl/ssl_versions.cc
@@ -105,11 +105,18 @@ static bool method_supports_version(const SSL_PROTOCOL_METHOD *method,
 
 static const char *ssl_version_to_string(uint16_t version) {
   switch (version) {
+    // Report TLS 1.3 draft version as TLS 1.3 in the public API.
     case TLS1_3_DRAFT_VERSION:
+      return "TLSv1.3 Draft 18";
+
     case TLS1_3_EXPERIMENT_VERSION:
+      return "TLSv1.3 Experiment";
+
     case TLS1_3_EXPERIMENT2_VERSION:
+      return "TLSv1.3 Experiment 2";
+
     case TLS1_3_EXPERIMENT3_VERSION:
-      return "TLSv1.3";
+      return "TLSv1.3 Experiment 3";
 
     case TLS1_2_VERSION:
       return "TLSv1.2";
@@ -199,7 +206,7 @@ static bool set_max_version(const SSL_PROTOCOL_METHOD *method, uint16_t *out,
                             uint16_t version) {
   // Zero is interpreted as the default maximum version.
   if (version == 0) {
-    *out = TLS1_2_VERSION;
+    *out = TLS1_3_VERSION;
     return true;
   }
 
@@ -420,7 +427,14 @@ int SSL_version(const SSL *ssl) {
   return wire_version_to_api(ssl_version(ssl));
 }
 
+char version_name[64];
+
 const char *SSL_get_version(const SSL *ssl) {
+  if (ssl->early_data_read > 0) {
+    snprintf(version_name, 64, "TLSv1.3 Early Data %d", ssl->early_data_read);
+    return version_name;
+  }
+
   return ssl_version_to_string(ssl_version(ssl));
 }
 
