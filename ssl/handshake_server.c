c162f51aa4522b957ccfdb13435017a15399bb59
Brian Smith <brian@briansmith.org>
2016-10-28 10:34:06 -1000

    TLS: Choose the max version supported by the client, not first.
    This change is based on interpreting TLS 1.3 draft 18.
    
    Change-Id: I727961aff2f7318bcbbc8bf6d62b7d6ad3e62da9

--- ssl/handshake_server.c
+++ ssl/handshake_server.c
@@ -578,6 +578,9 @@ static int negotiate_version(
       return 0;
     }
 
+    /* Choose the newest commonly-supported version advertised by the client.
+     * The client orders the versions according to its preferences, but we're
+     * not required to honor the client's preferences. */
     int found_version = 0;
     while (CBS_len(&versions) != 0) {
       uint16_t ext_version;
@@ -591,9 +594,10 @@ static int negotiate_version(
       }
       if (min_version <= ext_version &&
           ext_version <= max_version) {
-        version = ext_version;
-        found_version = 1;
-        break;
+        if (version < ext_version) {
+          version = ext_version;
+          found_version = 1;
+        }
       }
     }
 
