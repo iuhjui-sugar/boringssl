c05dd417f4cf7fa4edb9795d30d94149e8817940
Jean-Francois Thibert <jfthibert@google.com>
2016-11-10 16:26:54 -0500

    Ensure alignment of poly1305 state to uint32_t
    An unaligned internal structure could be created if the type was embedded
    into another structure, this avoids any slow unaligned access.
    
    Change-Id: I687c6e9158eb26008c8e8d131e1198d7d616a61e

--- include/openssl/poly1305.h
+++ include/openssl/poly1305.h
@@ -22,7 +22,7 @@ extern "C" {
 #endif
 
 
-typedef uint8_t poly1305_state[512];
+typedef uint32_t poly1305_state[128];
 
 /* CRYPTO_poly1305_init sets up |state| so that it can be used to calculate an
  * authentication tag with the one-time key |key|. Note that |key| is a
