63a077df6a3f6f25040266ae850aa01a8229bcd9
David Benjamin <davidben@google.com>
2016-11-12 12:36:06 +0900

    Align TLS 1.2 and 1.3 server session validity checks.
    Having that logic in two different places is a nuisance when we go to
    add new checks like resumption stuff. Along the way, this adds missing
    tests for the ClientHello cipher/session consistency check. (We'll
    eventually get it for free once the cipher/resumption change is
    unblocked, but get this working in the meantime.)
    
    This also fixes a bug where the session validity checks happened in the
    wrong order relative to whether tickets_supported or renew_ticket was
    looked at. Fix that by lifting that logic closer to the handshake.
    
    Change-Id: I3f4b59cfe01064f9125277dc5834e62a36e64aae

--- ssl/test/runner/runner.go
+++ ssl/test/runner/runner.go
@@ -5446,6 +5446,7 @@ func addResumptionVersionTests() {
 		config: Config{
 			MaxVersion: VersionTLS12,
 			Bugs: ProtocolBugs{
+				ExpectNewTicket: true,
 				FilterTicket: func(in []byte) ([]byte, error) {
 					return SetShimTicketVersion(in, VersionTLS13)
 				},
@@ -5485,6 +5486,7 @@ func addResumptionVersionTests() {
 		config: Config{
 			MaxVersion: VersionTLS12,
 			Bugs: ProtocolBugs{
+				ExpectNewTicket: true,
 				FilterTicket: func(in []byte) ([]byte, error) {
 					return SetShimTicketCipherSuite(in, TLS_AES_128_GCM_SHA256)
 				},
@@ -5504,6 +5506,7 @@ func addResumptionVersionTests() {
 		config: Config{
 			MaxVersion: VersionTLS12,
 			Bugs: ProtocolBugs{
+				ExpectNewTicket: true,
 				FilterTicket: func(in []byte) ([]byte, error) {
 					return SetShimTicketCipherSuite(in, TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384)
 				},
@@ -5534,6 +5537,46 @@ func addResumptionVersionTests() {
 			base64.StdEncoding.EncodeToString(TestShimTicketKey),
 		},
 		expectResumeRejected: true,
+	})
+
+	// Clients must not advertise a session without also advertising the
+	// cipher.
+	testCases = append(testCases, testCase{
+		testType:      serverTest,
+		name:          "Resume-Server-UnofferedCipher",
+		resumeSession: true,
+		config: Config{
+			MaxVersion:   VersionTLS12,
+			CipherSuites: []uint16{TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256},
+		},
+		resumeConfig: &Config{
+			MaxVersion:   VersionTLS12,
+			CipherSuites: []uint16{TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256},
+			Bugs: ProtocolBugs{
+				SendCipherSuites: []uint16{TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256},
+			},
+		},
+		shouldFail:    true,
+		expectedError: ":REQUIRED_CIPHER_MISSING:",
+	})
+
+	testCases = append(testCases, testCase{
+		testType:      serverTest,
+		name:          "Resume-Server-UnofferedCipher-TLS13",
+		resumeSession: true,
+		config: Config{
+			MaxVersion:   VersionTLS13,
+			CipherSuites: []uint16{TLS_CHACHA20_POLY1305_SHA256},
+		},
+		resumeConfig: &Config{
+			MaxVersion:   VersionTLS13,
+			CipherSuites: []uint16{TLS_CHACHA20_POLY1305_SHA256},
+			Bugs: ProtocolBugs{
+				SendCipherSuites: []uint16{TLS_AES_128_GCM_SHA256},
+			},
+		},
+		shouldFail:    true,
+		expectedError: ":REQUIRED_CIPHER_MISSING:",
 	})
 
 	// Sessions may not be resumed at a different cipher.
