648d2d1b3de08f9adbb9f3557210f5fa9b447714
Adam Langley <agl@google.com>
2016-12-01 08:24:24 -0800

    Add generic AES-GCM-SIV support.
    AES-GCM-SIV is an AEAD with nonce-misuse resistance. It can reuse
    hardware support for AES-GCM and thus encrypt at ~66% the speed, and
    decrypt at 100% the speed, of AES-GCM.
    
    See https://tools.ietf.org/html/draft-irtf-cfrg-gcmsiv-02
    
    This implementation is generic, not optimised, and reuses existing AES
    and GHASH support as much as possible. It is guarded by !OPENSSL_SMALL,
    at least for now.
    
    Change-Id: Ia9f77b256ef5dfb8588bb9ecfe6ee0e827626f57

--- crypto/cipher/aead_test.cc
+++ crypto/cipher/aead_test.cc
@@ -299,9 +299,20 @@ struct KnownAEAD {
   bool limited_implementation;
 };
 
+#if defined(OPENSSL_SMALL)
+const EVP_AEAD EVP_aead_aes_128_gcm_siv(void) {
+  return NULL;
+}
+const EVP_AEAD EVP_aead_aes_256_gcm_siv(void) {
+  return NULL;
+}
+#endif
+
 static const struct KnownAEAD kAEADs[] = {
   { "aes-128-gcm", EVP_aead_aes_128_gcm, false },
   { "aes-256-gcm", EVP_aead_aes_256_gcm, false },
+  { "aes-128-gcm-siv", EVP_aead_aes_128_gcm_siv, false },
+  { "aes-256-gcm-siv", EVP_aead_aes_256_gcm_siv, false },
   { "chacha20-poly1305", EVP_aead_chacha20_poly1305, false },
   { "chacha20-poly1305-old", EVP_aead_chacha20_poly1305_old, false },
   { "aes-128-cbc-sha1-tls", EVP_aead_aes_128_cbc_sha1_tls, true },
@@ -342,6 +353,11 @@ static int Main(int argc, char **argv) {
   }
 
   const EVP_AEAD *const aead = known_aead->func();
+  if (aead == NULL) {
+    // AEAD is not compiled in this configuration.
+    printf("PASS\n");
+    return 0;
+  }
 
   if (!TestCleanupAfterInitFailure(aead)) {
     return 1;
