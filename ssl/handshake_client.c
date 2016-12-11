6e9c9b097f12b567153db4b7a7c012963649c1d7
David Benjamin <davidben@google.com>
2016-12-11 02:55:20 -0500

    Preserve the peer signature algorithm across resumes.
    So we can report it cleanly out of DevTools, it should behave like
    SSL_get_curve_id and be reported on resumption too.
    
    BUG=chromium:658905
    
    Change-Id: I0402e540a1e722e09eaebadf7fb4785d8880c389

--- ssl/handshake_client.c
+++ ssl/handshake_client.c
@@ -1299,7 +1299,7 @@ static int ssl3_get_server_key_exchange(SSL_HANDSHAKE *hs) {
       if (!tls12_check_peer_sigalg(ssl, &al, signature_algorithm)) {
         goto f_err;
       }
-      ssl->s3->tmp.peer_signature_algorithm = signature_algorithm;
+      ssl->s3->new_session->peer_signature_algorithm = signature_algorithm;
     } else if (pkey->type == EVP_PKEY_RSA) {
       signature_algorithm = SSL_SIGN_RSA_PKCS1_MD5_SHA1;
     } else if (pkey->type == EVP_PKEY_EC) {
