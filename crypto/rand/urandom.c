c2aaea5bfc59b8bc5cc1af10b6eacad082a29b1b
Adam Langley <alangley@gmail.com>
2016-12-15 15:45:11 -0800

    Call __msan_unpoison on the output of getrandom.
    MSAN doesn't hook |syscall| and thus doesn't know that the kernel has
    filled the output buffer when |getrandom| is called.
    
    This change tells MSAN to trust that the memory that |getrandom| writes
    to has been initialised. This should avoid false-positives when code
    operates on |RAND_bytes| output.
    
    Change-Id: I0a74ebb21bcd1de1f28eda69558ee27f82db807a

--- crypto/rand/urandom.c
+++ crypto/rand/urandom.c
@@ -237,12 +237,18 @@ static struct rand_buffer *get_thread_local_buffer(void) {
   return buf;
 }
 
+#if defined(USE_SYS_getrandom) && defined(__has_feature)
+#if __has_feature(memory_sanitizer)
+void __msan_unpoison(void *, size_t);
+#endif
+#endif
+
 /* fill_with_entropy writes |len| bytes of entropy into |out|. It returns one
  * on success and zero on error. */
 static char fill_with_entropy(uint8_t *out, size_t len) {
-  ssize_t r;
-
   while (len > 0) {
+    ssize_t r;
+
     if (urandom_fd == kHaveGetrandom) {
 #if defined(USE_SYS_getrandom)
       do {
@@ -251,6 +257,16 @@ static char fill_with_entropy(uint8_t *out, size_t len) {
 #else
       abort();
 #endif
+
+#if defined(USE_SYS_getrandom) && defined(__has_feature)
+#if __has_feature(memory_sanitizer)
+      if (urandom_fd == kHaveGetrandom) {
+        /* MSAN doesn't recognise |syscall| and thus doesn't notice that we
+         * have initialised the output buffer. */
+        __msan_unpoison(out, r);
+      }
+#endif  /* memory_sanitizer */
+#endif  /* USE_SYS_getrandom && __has_feature */
     } else {
       do {
         r = read(urandom_fd, out, len);
