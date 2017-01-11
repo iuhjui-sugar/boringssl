572cbf22170eb52c6c2bd7fb9599784856fd1132
Steven Valdez <svaldez@google.com>
2017-01-11 11:34:52 -0500

    Data-less Zero-RTT (Server).
    This adds support on the server to accept data-less early data.
    
    Change-Id: Id85d192d8e0360b8de4b6971511b5e8a0e8012f7

--- ssl/tls13_server.c
+++ ssl/tls13_server.c
@@ -42,6 +42,8 @@ enum server_hs_state_t {
   state_send_server_certificate_verify,
   state_complete_server_certificate_verify,
   state_send_server_finished,
+  state_process_second_client_flight,
+  state_process_end_of_early_data,
   state_process_client_certificate,
   state_process_client_certificate_verify,
   state_process_channel_id,
@@ -136,7 +138,7 @@ static const SSL_CIPHER *choose_tls13_cipher(
 static enum ssl_hs_wait_t do_select_parameters(SSL_HANDSHAKE *hs) {
   SSL *const ssl = hs->ssl;
   /* The short record header extension is incompatible with early data. */
-  if (ssl->s3->skip_early_data && ssl->s3->short_header) {
+  if (hs->received_early_data_extension && ssl->s3->short_header) {
     OPENSSL_PUT_ERROR(SSL, SSL_R_UNEXPECTED_EXTENSION);
     return ssl_hs_error;
   }
@@ -228,7 +230,6 @@ static enum ssl_hs_wait_t do_select_parameters(SSL_HANDSHAKE *hs) {
       return ssl_hs_error;
     }
     ssl->s3->session_reused = 1;
-    SSL_SESSION_free(session);
 
     /* Resumption incorporates fresh key material, so refresh the timeout. */
     ssl_session_renew_timeout(ssl, hs->new_session,
@@ -262,10 +263,33 @@ static enum ssl_hs_wait_t do_select_parameters(SSL_HANDSHAKE *hs) {
     hs->new_session->early_alpn_len = ssl->s3->alpn_selected_len;
   }
 
+  if (hs->received_early_data_extension) {
+    if (!ssl->ctx->enable_early_data ||
+        !ssl->s3->session_reused ||
+        ssl->s3->alpn_selected_len != session->early_alpn_len ||
+        OPENSSL_memcmp(ssl->s3->alpn_selected, session->early_alpn,
+                       ssl->s3->alpn_selected_len) != 0) {
+      hs->early_data_state = ssl_early_data_reject;
+    } else {
+      hs->early_data_state = ssl_early_data_accept;
+    }
+  } else {
+    hs->early_data_state = ssl_early_data_off;
+  }
+
+  if (ssl->s3->session_reused) {
+    SSL_SESSION_free(session);
+  }
+
   /* Incorporate the PSK into the running secret. */
   if (ssl->s3->session_reused) {
     if (!tls13_advance_key_schedule(hs, hs->new_session->master_key,
                                     hs->new_session->master_key_length)) {
+      return ssl_hs_error;
+    }
+
+    if (ssl->ctx->enable_early_data &&
+        !tls13_derive_early_secrets(hs)) {
       return ssl_hs_error;
     }
   } else if (!tls13_advance_key_schedule(hs, kZeroes, hs->hash_len)) {
@@ -273,6 +297,11 @@ static enum ssl_hs_wait_t do_select_parameters(SSL_HANDSHAKE *hs) {
   }
 
   ssl->method->received_flight(ssl);
+
+  if (hs->early_data_state == ssl_early_data_accept &&
+      !tls13_derive_early_secrets(hs)) {
+    return ssl_hs_error;
+  }
 
   /* Resolve ECDHE and incorporate it into the secret. */
   int need_retry;
@@ -474,8 +503,36 @@ static enum ssl_hs_wait_t do_send_server_finished(SSL_HANDSHAKE *hs) {
     return ssl_hs_error;
   }
 
+  hs->tls13_state = state_process_second_client_flight;
+  return ssl_hs_flush;
+}
+
+static enum ssl_hs_wait_t do_process_second_client_flight(SSL_HANDSHAKE *hs) {
+  SSL *const ssl = hs->ssl;
+  if (hs->early_data_state == ssl_early_data_accept) {
+    if (!tls13_set_traffic_key(ssl, evp_aead_open, hs->early_secret, hs->hash_len)) {
+      return ssl_hs_error;
+    }
+    hs->tls13_state = state_process_end_of_early_data;
+    return ssl_hs_read_eoed;
+  }
+
+  if (!tls13_set_traffic_key(ssl, evp_aead_open, hs->client_handshake_secret,
+                             hs->hash_len)) {
+    return ssl_hs_error;
+  }
   hs->tls13_state = state_process_client_certificate;
-  return ssl_hs_flush_and_read_message;
+  return ssl_hs_read_message;
+}
+
+static enum ssl_hs_wait_t do_process_end_of_early_data(SSL_HANDSHAKE *hs) {
+  SSL *const ssl = hs->ssl;
+  if (!tls13_set_traffic_key(ssl, evp_aead_open, hs->client_handshake_secret,
+                             hs->hash_len)) {
+    return ssl_hs_error;
+  }
+  hs->tls13_state = state_process_client_certificate;
+  return ssl_hs_read_message;
 }
 
 static enum ssl_hs_wait_t do_process_client_certificate(SSL_HANDSHAKE *hs) {
@@ -647,9 +704,15 @@ enum ssl_hs_wait_t tls13_server_handshake(SSL_HANDSHAKE *hs) {
       break;
       case state_complete_server_certificate_verify:
         ret = do_send_server_certificate_verify(hs, 0 /* complete */);
-      break;
+        break;
       case state_send_server_finished:
         ret = do_send_server_finished(hs);
+        break;
+      case state_process_second_client_flight:
+        ret = do_process_second_client_flight(hs);
+        break;
+      case state_process_end_of_early_data:
+        ret = do_process_end_of_early_data(hs);
         break;
       case state_process_client_certificate:
         ret = do_process_client_certificate(hs);
