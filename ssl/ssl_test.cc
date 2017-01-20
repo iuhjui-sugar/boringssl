7f970179e1d32a5589f7528ac79703bbdbeab803
David Benjamin <davidben@google.com>
2017-01-19 19:05:47 -0500

    Do a cursory conversion of a few tests to GTest.
    For now, this is the laziest conversion possible. The intent is to just
    get the build setup ready so that we can make everything working in our
    consumers. The intended end state is:
    
    - The standalone build produces three test targets, one per library:
      {crypto,ssl,decrepit}_tests.
    
    - Each FOO_test is made up of:
        FOO/**/*_test.cc
        crypto/test/gtest_main.cc
        test_support
    
    - generate_build_files.py emits variables crypto_test_sources and
      ssl_test_sources. These variables are populated with FindCFiles,
      looking for *_test.cc.
    
    - The consuming file assembles those variables into the two test targets
      (plus decrepit) from there. This avoids having generate_build_files.py
      emit actual build rules.
    
    - Our standalone builders, Chromium, and Android just run the top-level
      test targets using whatever GTest-based reporting story they have.
    
    In transition, we start by converting one of two tests in each library
    to populate the three test targets. Those are added to all_tests.json
    and all_tests.go hacked to handle them transparently. This keeps our
    standalone builder working.
    
    generate_build_files.py, to start with, populates the new source lists
    manually and subtracts them out of the old machinery. We emit both for
    the time being. When this change rolls in, we'll write all the build
    glue needed to build the GTest-based tests and add it to consumers'
    continuous builders.
    
    Next, we'll subsume a file-based test and get the consumers working with
    that. (I.e.  make sure the GTest targets can depend on a data file.)
    
    Once that's all done, we'll be sure all this will work. At that point,
    we start subsuming the remaining tests into the GTest targets and, as we
    get bored, rewriting tests to use GTest properly rather than silly
    arangement.
    
    When all non-GTest tests are gone, the old generate_build_files.py hooks
    will be removed, consumers updated, and standalone builders converted to
    not rely on all_tests.go.
    
    BUG=129
    
    Change-Id: I881b26b07a8739cc0b52dbb51a30956908e1b71a

--- ssl/ssl_test.cc
+++ ssl/ssl_test.cc
@@ -21,6 +21,8 @@
 #include <utility>
 #include <vector>
 
+#include <gtest/gtest.h>
+
 #include <openssl/base64.h>
 #include <openssl/bio.h>
 #include <openssl/cipher.h>
@@ -3159,9 +3161,8 @@ static bool ForEachVersion(bool (*test_func)(bool is_dtls,
   return true;
 }
 
-int main() {
-  CRYPTO_library_init();
-
+// TODO(davidben): Convert this file to GTest properly.
+TEST(SSLTest, AllTests) {
   if (!TestCipherRules() ||
       !TestCurveRules() ||
       !TestSSL_SESSIONEncoding(kOpenSSLSession) ||
@@ -3210,9 +3211,6 @@ int main() {
       !ForEachVersion(TestAutoChain) ||
       !ForEachVersion(TestSSLWriteRetry)) {
     ERR_print_errors_fp(stderr);
-    return 1;
+    ADD_FAILURE() << "Tests failed";
   }
-
-  printf("PASS\n");
-  return 0;
 }
