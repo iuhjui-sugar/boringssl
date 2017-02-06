7ce8758a391ae8e31d3f46a9c6bc783c15594b7a
Alessandro Ghedini <alessandro@ghedini.me>
2017-02-06 13:40:10 +0000

    Remove support for setting per-connection default session timeout
    As previously discussed, it turns out we don't actually need this, so
    there's no point in keeping it.
    
    Change-Id: If549c917b6bd818cd36948e37cb7839c8d122b1a

--- include/openssl/ssl.h
+++ include/openssl/ssl.h
@@ -1753,20 +1753,6 @@ OPENSSL_EXPORT void SSL_CTX_set_session_psk_dhe_timeout(SSL_CTX *ctx,
  * sessions created in |ctx|. */
 OPENSSL_EXPORT long SSL_CTX_get_timeout(const SSL_CTX *ctx);
 
-/* SSL_set_session_timeout sets the default lifetime, in seconds, of a TLS 1.2
- * (or earlier) session created in |ssl| to |timeout|, and returns the old
- * value.
- *
- * By default the value |SSL_DEFAULT_SESSION_TIMEOUT| is used, which can be
- * overridden at the context level by calling |SSL_CTX_set_timeout|.
- *
- * If |timeout| is zero the newly created session will not be resumable. */
-OPENSSL_EXPORT long SSL_set_session_timeout(SSL *ssl, long timeout);
-
-/* SSL_set_session_psk_dhe_timeout sets the lifetime, in seconds, of TLS 1.3
- * sessions created in |ssl| to |timeout|. */
-OPENSSL_EXPORT void SSL_set_session_psk_dhe_timeout(SSL *ssl, long timeout);
-
 /* SSL_CTX_set_session_id_context sets |ctx|'s session ID context to |sid_ctx|.
  * It returns one on success and zero on error. The session ID context is an
  * application-defined opaque byte string. A session will not be used in a
@@ -4268,14 +4254,6 @@ struct ssl_st {
    * hash of the peer's certificate and then discard it to save memory and
    * session space. Only effective on the server side. */
   unsigned retain_only_sha256_of_client_certs:1;
-
-  /* session_timeout is the default lifetime in seconds of the session
-   * created in this connection at TLS 1.2 and earlier. */
-  long session_timeout;
-
-  /* session_psk_dhe_timeout is the default lifetime in seconds of sessions
-   * created in this connection at TLS 1.3. */
-  long session_psk_dhe_timeout;
 
   /* OCSP response to be sent to the client, if requested. */
   CRYPTO_BUFFER *ocsp_response;
