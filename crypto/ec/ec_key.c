6c490aa59ec77cf6cbfeaa971d2b453148ab395a
Steven Valdez <svaldez@google.com>
2017-04-05 16:36:54 -0400

    Enforce minimum EC group size when generating keys.
    This is part of ongoing FIPS certification work.
    
    Change-Id: I3134b2ed1b2000bf2413c066c6560832c0ff03ae

--- crypto/ec/ec_key.c
+++ crypto/ec/ec_key.c
@@ -425,6 +425,13 @@ int EC_KEY_generate_key(EC_KEY *eckey) {
   }
 
   const BIGNUM *order = EC_GROUP_get0_order(eckey->group);
+
+  /* Check that the order of the EC group is FIPS 186-4 (B.4.2) compliant. */
+  if (BN_num_bits(order) < 160) {
+    OPENSSL_PUT_ERROR(EC, ERR_R_INTERNAL_ERROR);
+    goto err;
+  }
+
   if (!BN_rand_range_ex(priv_key, 1, order)) {
     goto err;
   }
