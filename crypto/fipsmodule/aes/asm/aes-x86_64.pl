cfe3ece7271d0bd9cbd1dfe322ec403f485c4d0c
David Benjamin <davidben@google.com>
2017-04-25 18:32:32 -0400

    Replace OPENSSL_ia32cap_addr with OPENSSL_ia32cap_addr_delta.
    OPENSSL_ia32cap_addr avoids any relocations within the module, at the
    cost of a runtime TEXTREL, which causes problems in some cases.
    (Notably, if someone links us into a binary which uses the GCC "ifunc"
    attribute, the loader crashes.)
    
    This replaces assembly uses of OPENSSL_ia32cap_addr with a more
    complicated scheme when building in FIPS mode. The
    OPENSSL_ia32cap_addr_delta symbol (which is reachable relocation-free
    from the module) stores the difference between OPENSSL_ia32cap_P and its
    own address. Thus the module can find OPENSSL_ia32cap_P with a LEAQ and
    MOVQ combination.
    
    We cannot easily emit OPENSSL_ia32cap_addr_delta cross-platform (Windows
    and macOS toolchains seem to dislike the 64-bit "PC-relative"
    relocation). It is also not a 100% safe transformation as ADDQ clobbers
    flags. Thus, it is implemented as a perlasm pseudo-instruction that
    expands to a BORINGSSL_FIPS #ifdef. When patching the assembly, we
    should check that the flags register may be safely clobbered.
    
    Change-Id: I75497f02b2794560ad2aee24f11426ecb3ea50dd

--- crypto/fipsmodule/aes/asm/aes-x86_64.pl
+++ crypto/fipsmodule/aes/asm/aes-x86_64.pl
@@ -1640,9 +1640,9 @@ my $mark="80+240(%rsp)";	# copy of aes_key->rounds
 
 $code.=<<___;
 .align	16
+.extern	OPENSSL_ia32cap_P
 .globl	asm_AES_cbc_encrypt
 .type	asm_AES_cbc_encrypt,\@function,6
-.extern	OPENSSL_ia32cap_addr
 .hidden	asm_AES_cbc_encrypt
 asm_AES_cbc_encrypt:
 	cmp	\$0,%rdx	# check length
@@ -1664,7 +1664,7 @@ asm_AES_cbc_encrypt:
 	cmp	\$0,%r9
 	cmoveq	%r10,$sbox
 
-	mov	OPENSSL_ia32cap_addr(%rip),%r10
+	leaq_clobber_flags_OPENSSL_ia32cap_P	%r10
 	mov	(%r10), %r10d
 	cmp	\$$speed_limit,%rdx
 	jb	.Lcbc_slow_prologue
