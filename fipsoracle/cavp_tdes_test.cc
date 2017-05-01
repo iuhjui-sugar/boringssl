c77fc0129cf77a8f545bf96258b2173af0591e4a
Martin Kreichgauer <martinkr@google.com>
2017-05-01 15:31:43 -0700

    fipsoracle: Add MCT mode for TDES.
    Change-Id: I0dafd669a6d4e435d7597c0db26ef467e4beef0d

--- fipsoracle/cavp_tdes_test.cc
+++ fipsoracle/cavp_tdes_test.cc
@@ -32,7 +32,6 @@ struct TestCtx {
   enum Mode {
     kKAT,  // Known Answer Test
     kMCT,  // Monte Carlo Test
-    kMMT,  // Multi Message Test
   };
   bool has_iv;
   Mode mode;
@@ -50,41 +49,89 @@ static bool TestKAT(FileTest *t, void *arg) {
     kDecrypt,
   } operation = t->HasInstruction("ENCRYPT") ? kEncrypt : kDecrypt;
 
-  std::string count;
-  std::vector<uint8_t> key, iv, in, result;
-  const std::string op_label = operation == kEncrypt ? "PLAINTEXT" : "CIPHERTEXT";
-  if (!t->GetAttribute(&count, "COUNT") ||
-      !t->GetBytes(&key, "KEYs") ||
-      (ctx->has_iv && !t->GetBytes(&iv, "IV")) ||
-      !t->GetBytes(&in, op_label)) {
+  if (t->HasAttribute("NumKeys")) {
+    // Another file format quirk: NumKeys is a single attribute line immediately
+    // following an instruction and should probably have been an instruction
+    // instead.
+    std::string num_keys;
+    t->GetAttribute(&num_keys, "NumKeys");
+    t->InjectInstruction("NumKeys", num_keys);
+
+    std::string header = operation == kEncrypt ? "[ENCRYPT]" : "[DECRYPT]";
+    printf("%s\r\n\r\n", header.c_str());
+
+    return true;
+  }
+
+  std::string num_keys_str;
+  if (t->HasInstruction("NumKeys")) {
+    t->GetInstruction(&num_keys_str, "NumKeys");
+  }
+  const int num_keys = strtoul(num_keys_str.c_str(), nullptr, 0);
+  if (num_keys != 0 && num_keys != 2 && num_keys != 3) {
+    t->PrintLine("invalid NumKeys value");
     return false;
   }
-  std::vector<uint8_t> triple_key(key);
-  triple_key.insert(triple_key.end(), key.begin(), key.end());
-  triple_key.insert(triple_key.end(), key.begin(), key.end());
 
-  const EVP_CIPHER *cipher = ctx->cipher;
+  std::string count;
+  std::vector<uint8_t> keys, key1, key2, key3, iv, in, result;
+  const std::string in_label =
+      operation == kEncrypt ? "PLAINTEXT" : "CIPHERTEXT";
+  // clang-format off
+  if (!t->GetAttribute(&count, "COUNT") ||
+      (num_keys == 0 && !t->GetBytes(&keys, "KEYs")) ||
+      (num_keys > 0 &&
+       (!t->GetBytes(&key1, "KEY1") ||
+        !t->GetBytes(&key2, "KEY2") ||
+        !t->GetBytes(&key3, "KEY3"))) ||
+      (ctx->has_iv && !t->GetBytes(&iv, "IV")) ||
+      !t->GetBytes(&in, in_label)) {
+    return false;
+  }
+  // clang-format on
+  std::vector<uint8_t> key;
+  if (num_keys > 0) {
+    key.insert(key.end(), key1.begin(), key1.end());
+    key.insert(key.end(), key2.begin(), key2.end());
+    if (num_keys == 3) {
+      key.insert(key.end(), key3.begin(), key3.end());
+    }
+  } else {
+    key.insert(key.end(), keys.begin(), keys.end());
+    key.insert(key.end(), keys.begin(), keys.end());
+    key.insert(key.end(), keys.begin(), keys.end());
+  }
 
-  if (!CipherOperation(cipher, &result, operation == kEncrypt, triple_key, iv,
+  if (!CipherOperation(ctx->cipher, &result, operation == kEncrypt, key, iv,
                        in)) {
     return false;
   }
-  const std::string result_label =
-      operation == kEncrypt ? "CIPHERTEXT" : "PLAINTEXT";
 
-  // TDES fax files output format differs from its input format, so we
-  // construct it manually rather than printing CurrentTestToString().
-  if (t->IsAtNewInstructionBlock()) {
+  // TDES fax files output format differs from file to file, and the input
+  // format is inconsistent with the output, so we construct the output manually
+  // rather than printing CurrentTestToString().
+  if (t->IsAtNewInstructionBlock() && num_keys == 0) {
+    // If num_keys > 0, header is printed when parsing NumKeys.
     std::string header = operation == kEncrypt ? "[ENCRYPT]" : "[DECRYPT]";
     printf("%s\r\n", header.c_str());
   }
-  printf("COUNT = %s\r\nKEYs = %s\r\n", count.c_str(),
-         EncodeHex(key.data(), key.size()).c_str());
+  const std::string result_label =
+      operation == kEncrypt ? "CIPHERTEXT" : "PLAINTEXT";
+  printf("COUNT = %s\r\n", count.c_str());
+  if (num_keys < 2) {
+    printf("KEYs = %s\r\n", EncodeHex(keys.data(), keys.size()).c_str());
+  } else {
+    printf("KEY1 = %s\r\nKEY2 = %s\r\nKEY3 = %s\r\n",
+           EncodeHex(key1.data(), key1.size()).c_str(),
+           EncodeHex(key2.data(), key2.size()).c_str(),
+           EncodeHex(key3.data(), key3.size()).c_str());
+  }
   if (ctx->has_iv) {
     printf("IV = %s\r\n", EncodeHex(iv.data(), iv.size()).c_str());
   }
-  printf("%s = %s\r\n%s = %s\r\n\r\n", op_label.c_str(),
-         EncodeHex(in.data(), in.size()).c_str(), result_label.c_str(),
+  printf("%s = %s\r\n", in_label.c_str(),
+         EncodeHex(in.data(), in.size()).c_str());
+  printf("%s = %s\r\n\r\n", result_label.c_str(),
          EncodeHex(result.data(), result.size()).c_str());
 
   // Check if sample response file matches.
@@ -108,11 +155,150 @@ static bool TestKAT(FileTest *t, void *arg) {
   return true;
 }
 
+static void xor_key_with_odd_parity_lsb(std::vector<uint8_t> *key,
+                                        const std::vector<uint8_t> &value) {
+  for (size_t i = 0; i < key->size(); i++) {
+    uint8_t v = (*key)[i] ^ value[i];
+
+    // Use LSB to establish odd parity.
+    v |= 0x01;
+    for (uint8_t j = 1; j < 8; j++) {
+      v ^= ((v >> j) & 0x01);
+    }
+    (*key)[i] = v;
+  }
+}
+
+static bool TestMCT(FileTest *t, void *arg) {
+  TestCtx *ctx = reinterpret_cast<TestCtx *>(arg);
+
+  if (t->HasInstruction("ENCRYPT") == t->HasInstruction("DECRYPT")) {
+    t->PrintLine("Want either ENCRYPT or DECRYPT");
+    return false;
+  }
+  enum {
+    kEncrypt,
+    kDecrypt,
+  } operation = t->HasInstruction("ENCRYPT") ? kEncrypt : kDecrypt;
+
+  if (t->HasAttribute("NumKeys")) {
+    // Another file format quirk: NumKeys is a single attribute line immediately
+    // following an instruction and should probably have been an instruction
+    // instead.
+    std::string num_keys;
+    t->GetAttribute(&num_keys, "NumKeys");
+    t->InjectInstruction("NumKeys", num_keys);
+    return true;
+  }
+
+  std::string num_keys_str;
+  if (t->HasInstruction("NumKeys")) {
+    t->GetInstruction(&num_keys_str, "NumKeys");
+  }
+  const int num_keys = strtoul(num_keys_str.c_str(), nullptr, 0);
+  if (num_keys != 0 && num_keys != 2 && num_keys != 3) {
+    t->PrintLine("invalid NumKeys value");
+    return false;
+  }
+
+  std::string count;
+  std::vector<uint8_t> key1, key2, key3, iv, in, result;
+  const std::string in_label =
+      operation == kEncrypt ? "PLAINTEXT" : "CIPHERTEXT";
+  // clang-format off
+  if (!t->GetBytes(&key1, "KEY1") ||
+      !t->GetBytes(&key2, "KEY2") ||
+      !t->GetBytes(&key3, "KEY3") ||
+      (ctx->has_iv && !t->GetBytes(&iv, "IV")) ||
+      !t->GetBytes(&in, in_label)) {
+    return false;
+  }
+  // clang-format on
+
+  for (int i = 0; i < 400; i++) {
+    std::vector<uint8_t> current_iv = iv, current_in = in, prev_result,
+                         prev_prev_result;
+
+    std::vector<uint8_t> key(key1);
+    key.insert(key.end(), key2.begin(), key2.end());
+    key.insert(key.end(), key3.begin(), key3.end());
+
+    for (int j = 0; j < 10000; j++) {
+      prev_prev_result = prev_result;
+      prev_result = result;
+      const EVP_CIPHER *cipher = ctx->cipher;
+      if (!CipherOperation(cipher, &result, operation == kEncrypt, key,
+                           current_iv, current_in)) {
+        t->PrintLine("CipherOperation failed");
+        return false;
+      }
+      if (ctx->has_iv) {
+        if (operation == kEncrypt) {
+          if (j == 0) {
+            current_in = current_iv;
+          } else {
+            current_in = prev_result;
+          }
+          current_iv = result;
+        } else {  // operation == kDecrypt
+          current_iv = current_in;
+          current_in = result;
+        }
+      } else {
+        current_in = result;
+      }
+    }
+
+    // Output result for COUNT = i.
+    const std::string result_label =
+        operation == kEncrypt ? "CIPHERTEXT" : "PLAINTEXT";
+    if (i == 0) {
+      const std::string op_label =
+          operation == kEncrypt ? "ENCRYPT" : "DECRYPT";
+      printf("[%s]\n\n", op_label.c_str());
+    }
+    printf("COUNT = %d\r\nKEY1 = %s\r\nKEY2 = %s\r\nKEY3 = %s\r\n", i,
+           EncodeHex(key1.data(), key1.size()).c_str(),
+           EncodeHex(key2.data(), key2.size()).c_str(),
+           EncodeHex(key3.data(), key3.size()).c_str());
+    if (ctx->has_iv) {
+      printf("IV = %s\r\n", EncodeHex(iv.data(), iv.size()).c_str());
+    }
+    printf("%s = %s\r\n", in_label.c_str(),
+           EncodeHex(in.data(), in.size()).c_str());
+    printf("%s = %s\r\n\r\n", result_label.c_str(),
+           EncodeHex(result.data(), result.size()).c_str());
+
+
+    xor_key_with_odd_parity_lsb(&key1, result);
+    xor_key_with_odd_parity_lsb(&key2, prev_result);
+    if (num_keys == 3) {
+      xor_key_with_odd_parity_lsb(&key3, prev_prev_result);
+    } else {
+      xor_key_with_odd_parity_lsb(&key3, result);
+    }
+
+    if (ctx->has_iv) {
+      if (operation == kEncrypt) {
+        in = prev_result;
+        iv = result;
+      } else {
+        iv = current_iv;
+        in = current_in;
+      }
+    } else {
+      in = result;
+    }
+  }
+
+  return true;
+}
+
 static int usage(char *arg) {
-  fprintf(
-      stderr,
-      "usage: %s (kat|mct|mmt) <cipher> <test file> [<sample response file>]\n",
-      arg);
+  fprintf(stderr,
+          "usage: %s (kat|mct) <cipher> <test file> [<sample response "
+          "file>]\n",
+          arg);
   return 1;
 }
 
@@ -127,8 +313,6 @@ int main(int argc, char **argv) {
   enum TestCtx::Mode test_mode;
   if (tm == "kat") {
     test_mode = TestCtx::kKAT;
-  } else if (tm == "mmt") {
-    test_mode = TestCtx::kMMT;
   } else if (tm == "mct") {
     test_mode = TestCtx::kMCT;
   } else {
@@ -142,7 +326,7 @@ int main(int argc, char **argv) {
     fprintf(stderr, "invalid cipher: %s\n", argv[2]);
     return 1;
   }
-  bool has_iv = cipher_name != "des-ede3";
+  bool has_iv = cipher_name != "des-ede" && cipher_name != "des-ede3";
   TestCtx ctx = {cipher, nullptr, has_iv, test_mode};
 
   if (argc == 5) {
@@ -159,6 +343,6 @@ int main(int argc, char **argv) {
   }
   printf("\r\n\r\n");
 
-  // TODO(martinkr): Add MMT, MCT.
-  return FileTestMainSilent(TestKAT, &ctx, argv[3]);
+  FileTestFunc test_fn = test_mode == TestCtx::kKAT ? &TestKAT : &TestMCT;
+  return FileTestMainSilent(test_fn, &ctx, argv[3]);
 }
