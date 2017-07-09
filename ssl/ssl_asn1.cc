aeacda28edf0739c9e2d6e7bbf52d9f0df65e966
David Benjamin <davidben@google.com>
2017-07-09 16:17:55 -0400

    Switch a number of files to C++.
    http://i1.kym-cdn.com/photos/images/original/000/242/631/382.gif
    
    In the first step, switch C files to C++ individually, keeping
    everything in internal.h C-compatible. We'll make minimal changes needed
    to get things compiling (notably a lot of goto errs will need to turn to
    bssl::UniquePtr right away), but more aggressive changes will happen in
    later steps.
    
    (To avoid a rebase, I'm intentionally avoiding files that would conflict
    with CLs in flight right now.)
    
    Bug: 132
    Change-Id: Id4cfd722e7b57d1df11f27236b4658b5d39b5fd2

--- ssl/ssl_asn1.cc
+++ ssl/ssl_asn1.cc
@@ -425,7 +425,7 @@ int SSL_SESSION_to_bytes(const SSL_SESSION *in, uint8_t **out_data,
     static const char kNotResumableSession[] = "NOT RESUMABLE";
 
     *out_len = strlen(kNotResumableSession);
-    *out_data = BUF_memdup(kNotResumableSession, *out_len);
+    *out_data = (uint8_t *)BUF_memdup(kNotResumableSession, *out_len);
     if (*out_data == NULL) {
       return 0;
     }
