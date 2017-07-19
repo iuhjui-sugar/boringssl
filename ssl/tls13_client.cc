90aba2e23a302fec4ddb2bce0005cb0adc2d1ff1
David Benjamin <davidben@google.com>
2017-07-18 22:45:18 -0400

    C++-ify SSL_AEAD_CTX.
    This adds several utilities as replacements for new and delete and makes
    bssl::UniquePtr work with our private types.
    
    Later work can convert more incrementally. I did this one more
    aggressively to see how it'd work. Unfortunately, in doing so, I needed
    to remove the NULL SSL_AEAD_CTX "method" receiver trick to appease
    clang. The null cipher is now represented by a concrete SSL_AEAD_CTX.
    The long-lived references to SSL_AEAD_CTX are not yet in types with
    constructors, so they still bare Delete rather than UniquePtr for now.
    
    Though this does mean we may be able to move the sequence number into
    SSLAEADContext later which is one less object for DTLS to carry around.
    
    Bug: 132
    Change-Id: I506b404addafb692055d5709b0ca6d5439a4e6be

--- ssl/tls13_client.cc
+++ ssl/tls13_client.cc
@@ -155,7 +155,10 @@ static enum ssl_hs_wait_t do_process_hello_retry_request(SSL_HANDSHAKE *hs) {
 
 static enum ssl_hs_wait_t do_send_second_client_hello(SSL_HANDSHAKE *hs) {
   SSL *const ssl = hs->ssl;
-  if (!ssl->method->set_write_state(ssl, NULL) ||
+  /* Restore the null cipher. We may have switched due to 0-RTT. */
+  bssl::UniquePtr<SSLAEADContext> null = SSLAEADContext::CreateNull();
+  if (!null ||
+      !ssl->method->set_write_state(ssl, std::move(null)) ||
       !ssl_write_client_hello(hs)) {
     return ssl_hs_error;
   }
