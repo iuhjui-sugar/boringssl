85aeeb9836a817e06e43428b33b40469352ec3a6
Martin Kreichgauer <martinkr@google.com>
2017-08-18 14:24:36 -0700

    Zero memory in |OPENSSL_free|.
    Allocations by |OPENSSL_malloc| are prefixed with their length.
    |OPENSSL_free| zeros the allocation before calling free(), eliminating
    the need for a separate call to |OPENSSL_cleanse| for sensitive data.
    
    This change will be followed up by the cleanup in
    https://boringssl-review.googlesource.com/c/boringssl/+/19824.
    
    Change-Id: Ie272f07e9248d7d78af9aea81dacec0fdb7484c4

--- crypto/mem.c
+++ crypto/mem.c
@@ -75,31 +75,91 @@ OPENSSL_MSVC_PRAGMA(warning(pop))
 
 #include "internal.h"
 
+#define OPENSSL_MALLOC_PREFIX 8
+#define OPENSSL_MALLOC_PREFIX_CLEANSE_ON_FREE_FLAG \
+  (((size_t)1) << (__WORDSIZE - 1))
+#define OPENSSL_MALLOC_PREFIX_CLEANSE_ON_FREE(ptr) \
+      (*(size_t *)ptr & OPENSSL_MALLOC_PREFIX_CLEANSE_ON_FREE_FLAG)
+#define OPENSSL_MALLOC_PREFIX_SIZE(ptr) \
+      (*(size_t *)ptr & ~OPENSSL_MALLOC_PREFIX_CLEANSE_ON_FREE_FLAG)
 
-void *OPENSSL_realloc_clean(void *ptr, size_t old_size, size_t new_size) {
+static void *malloc_impl(size_t size, int cleanse_on_free) {
+  // Allocations are prefixed with an 8-byte prefix containing a size_t, of
+  // which the MSB indicates whether the buffer should be zeroed on free(), and
+  // the other 7-bytes encode the allocated length.
+  if (size & OPENSSL_MALLOC_PREFIX_CLEANSE_ON_FREE_FLAG ||
+      size + OPENSSL_MALLOC_PREFIX < size) {
+    return NULL;
+  }
+  void *ptr = malloc(size + OPENSSL_MALLOC_PREFIX);
   if (ptr == NULL) {
+    return NULL;
+  }
+
+  *(size_t *)ptr = size;
+  if (cleanse_on_free) {
+    *(size_t *)ptr |= OPENSSL_MALLOC_PREFIX_CLEANSE_ON_FREE_FLAG;
+  }
+
+  return ((uint8_t *)ptr) + OPENSSL_MALLOC_PREFIX;
+}
+
+void *OPENSSL_malloc(size_t size) {
+  return malloc_impl(size, 1 /* cleanse_on_free */);
+}
+
+void *OPENSSL_malloc_without_zero_on_free(size_t size) {
+  return malloc_impl(size, 0 /* cleanse_on_free */);
+}
+
+void OPENSSL_free(void *orig_ptr) {
+  if (orig_ptr == NULL) {
+    return;
+  }
+
+  void *ptr = ((uint8_t *)orig_ptr) - OPENSSL_MALLOC_PREFIX;
+
+  size_t size = OPENSSL_MALLOC_PREFIX_SIZE(ptr);
+
+  if (OPENSSL_MALLOC_PREFIX_CLEANSE_ON_FREE(ptr)) {
+    OPENSSL_cleanse(ptr, size + OPENSSL_MALLOC_PREFIX);
+  }
+  free(ptr);
+}
+
+void *OPENSSL_realloc(void *const orig_ptr, size_t new_size) {
+  if (orig_ptr == NULL) {
     return OPENSSL_malloc(new_size);
   }
 
-  if (new_size == 0) {
-    return NULL;
-  }
-
-  // We don't support shrinking the buffer. Note the memcpy that copies
-  // |old_size| bytes to the new buffer, below.
-  if (new_size < old_size) {
-    return NULL;
-  }
+  void *ptr = ((uint8_t *)orig_ptr) - OPENSSL_MALLOC_PREFIX;
+  size_t old_size = OPENSSL_MALLOC_PREFIX_SIZE(ptr);
 
   void *ret = OPENSSL_malloc(new_size);
   if (ret == NULL) {
     return NULL;
   }
 
-  OPENSSL_memcpy(ret, ptr, old_size);
-  OPENSSL_cleanse(ptr, old_size);
-  OPENSSL_free(ptr);
+  size_t to_copy = new_size;
+  if (old_size < to_copy) {
+    to_copy = old_size;
+  }
+
+  memcpy(ret, orig_ptr, to_copy);
+  OPENSSL_free(orig_ptr);
+
   return ret;
+}
+
+void *OPENSSL_realloc_clean(void *const orig_ptr, size_t old_size,
+                            size_t new_size) {
+  void *ptr = ((uint8_t *)orig_ptr) - OPENSSL_MALLOC_PREFIX;
+  size_t actual_size = OPENSSL_MALLOC_PREFIX_SIZE(ptr);
+  if (actual_size != old_size) {
+    return NULL;
+  }
+
+  return OPENSSL_realloc(orig_ptr, new_size);
 }
 
 void OPENSSL_cleanse(void *ptr, size_t len) {
@@ -155,15 +215,15 @@ size_t OPENSSL_strnlen(const char *s, size_t len) {
   return len;
 }
 
-#if defined(OPENSSL_WINDOWS)
-
-char *OPENSSL_strdup(const char *s) { return _strdup(s); }
-
-#else
-
-char *OPENSSL_strdup(const char *s) { return strdup(s); }
-
-#endif
+char *OPENSSL_strdup(const char *s) {
+  const size_t len = strlen(s) + 1;
+  char *ret = OPENSSL_malloc(len);
+  if (ret == NULL) {
+    return NULL;
+  }
+  OPENSSL_memcpy(ret, s, len);
+  return ret;
+}
 
 int OPENSSL_tolower(int c) {
   if (c >= 'A' && c <= 'Z') {
