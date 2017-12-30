61e63e6f7065373ca654f065701ace511483df53
Adam Langley <alangley@gmail.com>
2017-12-30 08:04:39 -0800

    Add support for dummy PQ padding.
    This extension will be used to measure the latency impact of potentially
    sending a post-quantum key share by default. At this time it's purely
    measuring the impact of the client sending the key share, not the server
    replying with a ciphertext.
    
    We could use the existing padding extension for this but that extension
    doesn't allow the server to echo it, so we would need a different
    extension in the future anyway. Thus we just create one now.
    
    We can assume that modern clients will be using TLS 1.3 by the time that
    PQ key-exchange is established and thus the key share will be sent in
    all ClientHello messages. However, since TLS 1.3 isn't quite here yet,
    this extension is also sent for TLS 1.0–1.2 ClientHellos. The latency
    impact should be the same either way.
    
    Change-Id: Ie4a17551f6589b28505797e8c54cddbe3338dfe5

--- ssl/test/runner/runner.go
+++ ssl/test/runner/runner.go
@@ -6750,6 +6750,34 @@ func addExtensionTests() {
 		shouldFail:    true,
 		expectedError: ":INVALID_SCT_LIST:",
 	})
+
+	for _, version := range allVersions(tls) {
+		if version.version < VersionTLS12 {
+			continue
+		}
+
+		for _, paddingLen := range []int{1, 9700} {
+			flags := []string{
+				"-max-version", version.shimFlag(tls),
+				"-dummy-pq-padding-len", strconv.Itoa(paddingLen),
+			}
+			if version.tls13Variant != 0 {
+				flags = append(flags, "-tls13-variant", strconv.Itoa(version.tls13Variant))
+			}
+
+			testCases = append(testCases, testCase{
+				name:     fmt.Sprintf("DummyPQPadding-%d-%s", paddingLen, version.name),
+				testType: clientTest,
+				config: Config{
+					MaxVersion: version.version,
+					Bugs: ProtocolBugs{
+						ExpectDummyPQPaddingLength: paddingLen,
+					},
+				},
+				flags: flags,
+			})
+		}
+	}
 }
 
 func addResumptionVersionTests() {
