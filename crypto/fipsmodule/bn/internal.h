f3c04763b3d419ccd9a803869f48e83882fbd538
David Benjamin <davidben@google.com>
2018-01-25 15:01:39 -0500

    Split BN_uadd into a bn_uadd_fixed.
    This is to be used in constant-time RSA CRT.
    
    Bug: 233
    Change-Id: Ibade5792324dc6aba38cab6971d255d41fb5eb91

--- crypto/fipsmodule/bn/internal.h
+++ crypto/fipsmodule/bn/internal.h
@@ -371,6 +371,10 @@ int bn_less_than_montgomery_R(const BIGNUM *bn, const BN_MONT_CTX *mont);
 // Note this means that, e.g., repeatedly multiplying by one will cause widths
 // to increase without bound.
 
+// bn_uadd_fixed behaves like |BN_uadd|, but it pessimally sets
+// |r->width| = |a->width| + |b->width| + 1.
+int bn_uadd_fixed(BIGNUM *r, const BIGNUM *a, const BIGNUM *b);
+
 // bn_mul_fixed behaves like |BN_mul|, but it rejects negative inputs and
 // pessimally sets |r->width| to |a->width| + |b->width|, to avoid leaking
 // information about |a| and |b|.
