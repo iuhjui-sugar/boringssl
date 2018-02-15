880797d2a712e551c6c2ad4fd1f558b5f8e5938c
David Benjamin <davidben@google.com>
2018-02-14 19:50:38 -0500

    Reject compressed ECDH coordinates in TLS.
    We don't advertise compressed coordinates (and point format negotiation
    was deprecated in TLS 1.3), so reject them. Both Internet Explorer and
    Firefox appear to reject them already.
    
    Later I hope to add an easier to use ECDH API that acts on bytes, not
    EC_POINT. This clears the way for that API to only accept compressed
    coordinates. Compressed coordinates never got deployed over NIST curves,
    for better or worse. At this point, there is no sense in changing that
    as new protocols should use curve25519.
    
    Change-Id: Id2f1be791ddcf155d596f4eb0b79351766c5cdab

--- ssl/test/runner/common.go
+++ ssl/test/runner/common.go
@@ -1551,6 +1551,10 @@ type ProtocolBugs struct {
 	// require that the client sent a dummy PQ padding extension of this
 	// length.
 	ExpectDummyPQPaddingLength int
+
+	// SendCompressionMethod, if true, causes ECDH key shares over NIST
+	// curves to use compressed coordinates.
+	SendCompressedCoordinates bool
 }
 
 func (c *Config) serverInit() {
