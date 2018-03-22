0c139ebd851c88e87fc11d0fa14a81b932f4bb92
Yuki Nagai <unynagai@gmail.com>
2018-03-22 12:39:32 +0900

    Carthage Support.
    This commit is needed for trying to make Firebase carthage supportable as dynamic framework.
    
    Change-Id: I33730fb3e752c13fb3ef854026b9d7c0b0cd193a

---
+++ xcode/BoringSSL/BoringSSL.h
@@ -0,0 +1,18 @@
+//
+//  BoringSSL.h
+//  BoringSSL
+//
+//  Created by Yuki Nagai on 2018/03/22.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BoringSSL.
+FOUNDATION_EXPORT double BoringSSLVersionNumber;
+
+//! Project version string for BoringSSL.
+FOUNDATION_EXPORT const unsigned char BoringSSLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoringSSL/PublicHeader.h>
+
+
