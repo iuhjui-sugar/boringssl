65c13390383d4a01fbdcffc7e16657351772f5ac
Matthew Braithwaite <mab@google.com>
2018-04-13 15:51:30 -0700

    SSL_CONFIG: new struct for sheddable handshake configuration.
    |SSL_CONFIG| is a container for bits of configuration that are
    unneeded after the handshake completes.  By default it is retained for
    the life of the |SSL|, but it may be shed at the caller's option by
    calling SSL_shed_handshake_config().  This is incompatible with
    renegotiation, and with SSL_clear().
    
    |SSL_CONFIG| is reachable by |ssl->config| and by |hs->config|.  The
    latter is always non-NULL.  To avoid null checks, I've changed the
    signature of a number of functions from |SSL*| arguments to
    |SSL_HANDSHAKE*| arguments.
    
    When configuration has been shed, setters that touch |SSL_CONFIG|
    return an error value if that is possible.  Setters that return |void|
    do nothing.
    
    Getters that request |SSL_CONFIG| values will fail with an |assert| if
    the configuration has been shed.  When asserts are compiled out, they
    will return an error value.
    
    The aim of this commit is to simplify analysis of split-handshakes by
    making it obvious that some bits of state have no effects beyond the
    handshake.  It also cuts down on memory usage.
    
    Of note: |SSL_CTX| is still reachable after the configuration has been
    shed, and a couple things need to be retained only for the sake of
    post-handshake hooks.  Perhaps these can be fixed in time.
    
    Change-Id: Idf09642e0518945b81a1e9fcd7331cc9cf7cc2d6
    Bug: 123

--- include/openssl/ssl.h
+++ include/openssl/ssl.h
@@ -1892,7 +1892,7 @@ OPENSSL_EXPORT int SSL_set_session_id_context(SSL *ssl, const uint8_t *sid_ctx,
                                               size_t sid_ctx_len);
 
 // SSL_get0_session_id_context returns a pointer to |ssl|'s session ID context
-// and sets |*out_len| to its length.
+// and sets |*out_len| to its length.  It returns NULL on error.
 OPENSSL_EXPORT const uint8_t *SSL_get0_session_id_context(const SSL *ssl,
                                                           size_t *out_len);
 
@@ -2296,7 +2296,7 @@ OPENSSL_EXPORT void SSL_set_custom_verify(
 OPENSSL_EXPORT int SSL_CTX_get_verify_mode(const SSL_CTX *ctx);
 
 // SSL_get_verify_mode returns |ssl|'s verify mode, set by |SSL_CTX_set_verify|
-// or |SSL_set_verify|.
+// or |SSL_set_verify|.  It returns -1 on error.
 OPENSSL_EXPORT int SSL_get_verify_mode(const SSL *ssl);
 
 // SSL_CTX_get_verify_callback returns the callback set by
@@ -3295,6 +3295,12 @@ OPENSSL_EXPORT void (*SSL_CTX_get_keylog_callback(const SSL_CTX *ctx))(
 // set explicitly by the test. The |ssl| pointer passed to |cb| is always null.
 OPENSSL_EXPORT void SSL_CTX_set_current_time_cb(
     SSL_CTX *ctx, void (*cb)(const SSL *ssl, struct timeval *out_clock));
+
+// SSL_set_shed_handshake_config allows some of the configuration of |ssl| to be
+// freed after its handshake completes.  When configuration shedding is enabled,
+// it is an error to enable renegotiation, and it is an error to call
+// |SSL_clear|.
+OPENSSL_EXPORT void SSL_set_shed_handshake_config(SSL *ssl, int enable);
 
 enum ssl_renegotiate_mode_t {
   ssl_renegotiate_never = 0,
