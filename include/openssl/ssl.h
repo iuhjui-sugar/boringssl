93395f2f80eeb8d8f14a12b225d60a3ca15dadf1
David Benjamin <davidben@google.com>
2018-04-16 19:54:42 -0400

    Remove legacy SHA-2 CBC ciphers.
    All CBC ciphers in TLS are broken and insecure. TLS 1.2 introduced
    AEAD-based ciphers which avoid their many problems. It also introduced
    new CBC ciphers based on HMAC-SHA256 and HMAC-SHA384 that share the same
    flaws as the original HMAC-SHA1 ones. These serve no purpose. Old
    clients don't support them, they have the highest overhead of all TLS
    ciphers, and new clients can use AEADs anyway.
    
    Remove them from libssl. This is the smaller, more easily reverted
    portion of the removal. If it survives a week or so, we can unwind a lot
    more code elsewhere in libcrypto. This removal will allow us to clear
    some indirect calls from crypto/cipher_extra/tls_cbc.c, aligning with
    the recommendations here:
    
    https://github.com/HACS-workshop/spectre-mitigations/blob/master/crypto_guidelines.md#2-avoid-indirect-branches-in-constant-time-code
    
    Update-Note: The follow cipher suites are removed:
    - TLS_RSA_WITH_AES_128_CBC_SHA256
    - TLS_RSA_WITH_AES_256_CBC_SHA256
    - TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256
    - TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384
    - TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256
    - TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384
    
    Change-Id: I7ade0fc1fa2464626560d156659893899aab6f77

--- include/openssl/ssl.h
+++ include/openssl/ssl.h
@@ -1319,11 +1319,8 @@ OPENSSL_EXPORT int SSL_CIPHER_get_bits(const SSL_CIPHER *cipher,
 //   whose bulk cipher use the corresponding encryption scheme. Note that
 //   |AES|, |AES128|, and |AES256| match both CBC and GCM ciphers.
 //
-//   |SHA1|, |SHA256|, and |SHA384| match legacy cipher suites using the
-//   corresponding hash function in their MAC. AEADs are matched by none of
-//   these.
-//
-//   |SHA| is an alias for |SHA1|.
+//   |SHA1| matches legacy cipher suites using HMAC-SHA256. |SHA| is an alias
+//   for |SHA1|.
 //
 // Although implemented, authentication-only ciphers match no rules and must be
 // explicitly selected by name.
