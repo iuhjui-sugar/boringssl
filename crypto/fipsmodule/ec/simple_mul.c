d401033a4f8c3c14cf8ff25f9a7bb6dc48a023e5
David Benjamin <davidben@google.com>
2018-04-24 23:40:01 -0400

    Implement constant-time generic multiplication.
    This is slower, but constant-time.
    
    Before:
    Did 7548 ECDH P-384 operations in 10032522us (752.4 ops/sec)
    Did 13464 ECDSA P-384 signing operations in 10078959us (1335.9 ops/sec)
    Did 2929 ECDH P-521 operations in 10015568us (292.4 ops/sec)
    Did 5150 ECDSA P-521 signing operations in 10002507us (514.9 ops/sec)
    
    After:
    Did 6969 ECDH P-384 operations in 10000783us (696.8 ops/sec)
    Did 12463 ECDSA P-384 signing operations in 10015738us (1244.3 ops/sec)
    Did 2727 ECDH P-521 operations in 10002397us (272.6 ops/sec)
    Did 4848 ECDSA P-521 signing operations in 10042460us (482.8 ops/sec)
    
    But we're still faster than the old BIGNUM implementation. EC_FELEM
    more than paid for both the loss of points_make_affine and this CL.
    
    Bug: 239
    Change-Id: I65d71a731aad16b523928ee47618822d503ea704

---
+++ crypto/fipsmodule/ec/simple_mul.c
@@ -0,0 +1,105 @@
+/* Copyright (c) 2018, Google Inc.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+
+#include <openssl/ec.h>
+
+#include <assert.h>
+
+#include "internal.h"
+#include "../bn/internal.h"
+#include "../../internal.h"
+
+
+static void ec_GFp_simple_mul_single(const EC_GROUP *group, EC_RAW_POINT *r,
+                                     const EC_RAW_POINT *p,
+                                     const EC_SCALAR *scalar) {
+  // Compute a table of the first 16 multiples of |p| (and infinity) for
+  // |ec_GFp_nistp_recode_scalar_bits|.
+  EC_RAW_POINT precomp[17];
+  ec_GFp_simple_point_set_to_infinity(group, &precomp[0]);
+  ec_GFp_simple_point_copy(&precomp[1], p);
+  for (size_t j = 2; j < OPENSSL_ARRAY_SIZE(precomp); j++) {
+    if (j & 1) {
+      ec_GFp_simple_add(group, &precomp[j], &precomp[1], &precomp[j - 1]);
+    } else {
+      ec_GFp_simple_dbl(group, &precomp[j], &precomp[j / 2]);
+    }
+  }
+
+  // Evaluate the table.
+  unsigned bits = BN_num_bits(&group->order);
+  int r_is_at_infinity = 1;
+  for (unsigned i = bits; i <= bits; i--) {
+    if (!r_is_at_infinity) {
+      ec_GFp_simple_dbl(group, r, r);
+    }
+    if (i % 5 == 0) {
+      // Compute the next window.
+      const size_t width = group->order.width;
+      uint8_t window = bn_is_bit_set_words(scalar->words, width, i + 4) << 5;
+      window |= bn_is_bit_set_words(scalar->words, width, i + 3) << 4;
+      window |= bn_is_bit_set_words(scalar->words, width, i + 2) << 3;
+      window |= bn_is_bit_set_words(scalar->words, width, i + 1) << 2;
+      window |= bn_is_bit_set_words(scalar->words, width, i) << 1;
+      window |= bn_is_bit_set_words(scalar->words, width, i - 1);
+      uint8_t sign, digit;
+      ec_GFp_nistp_recode_scalar_bits(&sign, &digit, window);
+
+      // Select the entry in constant-time.
+      EC_RAW_POINT tmp;
+      for (size_t j = 0; j < OPENSSL_ARRAY_SIZE(precomp); j++) {
+        BN_ULONG mask = constant_time_eq_w(j, digit);
+        ec_felem_select(group, &tmp.X, mask, &precomp[j].X, &tmp.X);
+        ec_felem_select(group, &tmp.Y, mask, &precomp[j].Y, &tmp.Y);
+        ec_felem_select(group, &tmp.Z, mask, &precomp[j].Z, &tmp.Z);
+      }
+
+      // Negate if necessary.
+      EC_FELEM neg_Y;
+      ec_felem_neg(group, &neg_Y, &tmp.Y);
+      BN_ULONG sign_mask = sign;
+      sign_mask = 0u - sign_mask;
+      ec_felem_select(group, &tmp.Y, sign_mask, &neg_Y, &tmp.Y);
+
+      if (r_is_at_infinity) {
+        ec_GFp_simple_point_copy(r, &tmp);
+        r_is_at_infinity = 0;
+      } else {
+        ec_GFp_simple_add(group, r, r, &tmp);
+      }
+    }
+  }
+  if (r_is_at_infinity) {
+    ec_GFp_simple_point_set_to_infinity(group, r);
+  }
+}
+
+void ec_GFp_simple_mul(const EC_GROUP *group, EC_RAW_POINT *r,
+                       const EC_SCALAR *g_scalar, const EC_RAW_POINT *p,
+                       const EC_SCALAR *p_scalar) {
+  assert(g_scalar != NULL || p_scalar != NULL);
+  if (p_scalar == NULL) {
+    ec_GFp_simple_mul_single(group, r, &group->generator->raw, g_scalar);
+  } else if (g_scalar == NULL) {
+    ec_GFp_simple_mul_single(group, r, p, p_scalar);
+  } else {
+    // Support constant-time two-point multiplication for compatibility.  This
+    // does not actually come up in keygen, ECDH, or ECDSA, so we implement it
+    // the naive way.
+    ec_GFp_simple_mul_single(group, r, &group->generator->raw, g_scalar);
+    EC_RAW_POINT tmp;
+    ec_GFp_simple_mul_single(group, &tmp, p, p_scalar);
+    ec_GFp_simple_add(group, r, r, &tmp);
+  }
+}
