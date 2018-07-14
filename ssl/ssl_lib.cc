8f1596431c56daad25da3147e3e27890f562fa9e
Steven Valdez <svaldez@chromium.org>
2018-07-14 11:23:01 -0400

    Initial custom_stream API and QUIC labels.
    Change-Id: Ib2b2c6b618b3e33a74355fb53fdbd2ffafcc5c56

--- ssl/ssl_lib.cc
+++ ssl/ssl_lib.cc
@@ -779,6 +779,30 @@ BIO *SSL_get_rbio(const SSL *ssl) { return ssl->rbio.get(); }
 
 BIO *SSL_get_wbio(const SSL *ssl) { return ssl->wbio.get(); }
 
+int SSL_provide_data(SSL *ssl, enum ssl_encryption_level_t level,
+                     const uint8_t *data, size_t len) {
+  if (ssl->ctx->stream_method == nullptr || level != ssl->s3->read_level) {
+    OPENSSL_PUT_ERROR(SSL, ERR_R_INTERNAL_ERROR);
+    return 0;
+  }
+
+  uint8_t alert;
+  if (!tls_can_accept_handshake_data(ssl, &alert)) {
+    ssl_send_alert(ssl, SSL3_AL_FATAL, alert);
+    return 0;
+  }
+
+  // Re-create the handshake buffer if needed.
+  if (!ssl->s3->hs_buf) {
+    ssl->s3->hs_buf.reset(BUF_MEM_new());
+    if (!ssl->s3->hs_buf) {
+      return 0;
+    }
+  }
+
+  return BUF_MEM_append(ssl->s3->hs_buf.get(), data, len);
+}
+
 int SSL_do_handshake(SSL *ssl) {
   ssl_reset_error_state(ssl);
 
@@ -1191,6 +1215,9 @@ int SSL_get_error(const SSL *ssl, int ret_code) {
       return SSL_ERROR_HANDBACK;
 
     case SSL_READING: {
+      if (ssl->ctx->stream_method) {
+        return SSL_ERROR_WANT_READ;
+      }
       BIO *bio = SSL_get_rbio(ssl);
       if (BIO_should_read(bio)) {
         return SSL_ERROR_WANT_READ;
@@ -2294,6 +2321,15 @@ char *SSL_get_shared_ciphers(const SSL *ssl, char *buf, int len) {
   }
   buf[0] = '\0';
   return buf;
+}
+
+int SSL_CTX_set_custom_stream_method(SSL_CTX *ctx,
+                                 const SSL_STREAM_METHOD *stream_method) {
+  if (ctx->method->is_dtls || ctx->conf_min_version != TLS1_3_VERSION) {
+    return 0;
+  }
+  ctx->stream_method = stream_method;
+  return 1;
 }
 
 int SSL_get_ex_new_index(long argl, void *argp, CRYPTO_EX_unused *unused,
