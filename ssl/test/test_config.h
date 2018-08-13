ec022dca01d4bd088815f63cf5e411fc4cda1c5d
Steven Valdez <svaldez@google.com>
2018-08-13 10:54:48 -0400

    Implement TLS 1.3 anti-downgrade signal.
    Change-Id: Ib4739350948ec339457d993daef582748ed8f100

--- ssl/test/test_config.h
+++ ssl/test/test_config.h
@@ -156,7 +156,8 @@ struct TestConfig {
   bool use_custom_verify_callback = false;
   std::string expect_msg_callback;
   bool allow_false_start_without_alpn = false;
-  bool expect_draft_downgrade = false;
+  bool ignore_tls13_downgrade = false;
+  bool expect_tls13_downgrade = false;
   int dummy_pq_padding_len = 0;
   bool handoff = false;
   bool expect_dummy_pq_padding = false;
