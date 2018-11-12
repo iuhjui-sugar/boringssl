ec1db278f8ff32297a96c3a46fb9c9d8b5d6fdc8
Adam Langley <alangley@gmail.com>
2018-11-12 13:53:42 -0800

    Add initial HRSS support.
    This change includes support for a variant of [HRSS], a post-quantum KEM
    based on NTRU. It includes changes suggested in [SXY].
    
    (CLA for HRSS's assembly code noted in b/119426559.)
    
    [HRSS] https://eprint.iacr.org/2017/1005
    [SXY] https://eprint.iacr.org/2017/1005.pdf
    
    Change-Id: I85d813733b066d5c578484bdd248de3f764194db

--- ssl/test/runner/common.go
+++ ssl/test/runner/common.go
@@ -163,6 +163,7 @@ const (
 	CurveP384   CurveID = 24
 	CurveP521   CurveID = 25
 	CurveX25519 CurveID = 29
+	CurveCECPQ2 CurveID = 16696
 )
 
 // TLS Elliptic Curve Point Formats
@@ -1630,6 +1631,14 @@ type ProtocolBugs struct {
 	// FailIfHelloRetryRequested causes a handshake failure if a server requests a
 	// hello retry.
 	FailIfHelloRetryRequested bool
+
+	// FailedIfCECPQ2Offered will cause a server to reject a ClientHello if CECPQ2
+	// is supported.
+	FailIfCECPQ2Offered bool
+
+	// ExpectKeyShares, if not nil, lists (in order) the curves that a ClientHello
+	// should have key shares for.
+	ExpectedKeyShares []CurveID
 }
 
 func (c *Config) serverInit() {
@@ -1709,7 +1718,7 @@ func (c *Config) maxVersion(isDTLS bool) uint16 {
 	return ret
 }
 
-var defaultCurvePreferences = []CurveID{CurveX25519, CurveP256, CurveP384, CurveP521}
+var defaultCurvePreferences = []CurveID{CurveX25519, CurveCECPQ2, CurveP256, CurveP384, CurveP521}
 
 func (c *Config) curvePreferences() []CurveID {
 	if c == nil || len(c.CurvePreferences) == 0 {
