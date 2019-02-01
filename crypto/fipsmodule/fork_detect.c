0221fa4ed5a0f31d415624155119c776267a5586
David Benjamin <davidben@google.com>
2019-02-01 00:45:23 +0000

    Harden against fork via MADV_WIPEONFORK.
    Linux 4.14 and up support MADV_WIPEONFORK, which can be used to reliably
    and efficiently detect forks. Use it to harden the PRNG and RSA blinding
    caches.
    
    On the RSA side, we currently cache blinding values. (Alas, removing the cache
    gives a *very* noticeable perf hit. There is some low-hanging fruit to trim a
    few Montgomery reductions, but it didn't offset much last I toyed with it.)
    Now, on Linux 4.14 and up, this cache is fork-safe.
    
    Since not all platforms that support fork also support fork detection,
    this should only be used as a hardening measure. Now, when detection is
    present, BoringSSL will skip doing per-call entropy draws from the
    kernel. (This might regress protection against VM cloning when no fast
    RDRAND is available. However, we need to do something for AMD machines.
    Hypervisors that clone VMs are going to need to signal the kernel to
    wipe WIPEONFORK pages.)
    
    Upgrade-Note: BoringSSL now calls some more syscalls on Linux. If this offends
    sandboxes, let us know. We can loosen the sandbox or add a mechanism to prime
    the MADV_WIPEONFORK page before entering it.
    
    Change-Id: I6ba43951aeaa2b9b81f74f9e5a7a0ce2de0438a4

---
+++ crypto/fipsmodule/fork_detect.c
@@ -0,0 +1,127 @@
+/* Copyright (c) 2020, Google Inc.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+
+#if !defined(_GNU_SOURCE)
+#define _GNU_SOURCE  // needed for madvise() and MAP_ANONYMOUS on Linux.
+#endif
+
+#include <openssl/base.h>
+
+#include "fork_detect.h"
+
+#if defined(OPENSSL_LINUX)
+#include <sys/mman.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <openssl/type_check.h>
+
+#include "delocate.h"
+#include "../internal.h"
+
+
+#if defined(MADV_WIPEONFORK)
+OPENSSL_STATIC_ASSERT(MADV_WIPEONFORK == 18, "MADV_WIPEONFORK is not 18");
+#else
+#define MADV_WIPEONFORK 18
+#endif
+
+DEFINE_STATIC_ONCE(g_fork_detect_once);
+DEFINE_STATIC_MUTEX(g_fork_detect_lock);
+DEFINE_BSS_GET(char *, g_fork_detect_addr);
+DEFINE_BSS_GET(uint64_t, g_fork_generation);
+
+static void init_fork_detect(void) {
+  if (secure_getenv("BORINGSSL_IGNORE_MADV_WIPEONFORK")) {
+    return;
+  }
+
+  long page_size = sysconf(_SC_PAGESIZE);
+  if (page_size <= 0) {
+    return;
+  }
+
+  void *addr = mmap(NULL, (size_t)page_size, PROT_READ | PROT_WRITE,
+                    MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  if (addr == MAP_FAILED) {
+    return;
+  }
+
+  if (madvise(addr, (size_t)page_size, MADV_WIPEONFORK) != 0) {
+    munmap(addr, (size_t)page_size);
+    return;
+  }
+
+  *((char *) addr) = 1;
+  *g_fork_detect_addr_bss_get() = addr;
+  *g_fork_generation_bss_get() = 1;
+}
+
+uint64_t CRYPTO_get_fork_generation(void) {
+  // In a single-threaded process, there are obviously no races because there's
+  // only a single mutator in the address space.
+  //
+  // In a multi-threaded environment, |CRYPTO_once| ensures that the flag byte
+  // is initialised atomically, even if multiple threads enter this function
+  // concurrently.
+  //
+  // If a multi-threaded process forks, only a single thread exists in the new
+  // process. That thread may only call async-signal-safe functions prior to
+  // execve(), which excludes calling any BoringSSL functions. Thus the cleared
+  // flag byte can only be observed in a single-threaded context.
+  //
+  // However, if a single-threaded process forks and the child goes
+  // multi-threaded, those threads can race to increment the fork-detection
+  // count. Thus the generation counter and flag byte are protected by a lock.
+
+  CRYPTO_once(g_fork_detect_once_bss_get(), init_fork_detect);
+  char *const flag_ptr = *g_fork_detect_addr_bss_get();
+  if (flag_ptr == NULL) {
+    // Our kernel is too old to support |MADV_WIPEONFORK|.
+    return 0;
+  }
+
+  struct CRYPTO_STATIC_MUTEX *const lock = g_fork_detect_lock_bss_get();
+  uint64_t *const generation_ptr = g_fork_generation_bss_get();
+
+  CRYPTO_STATIC_MUTEX_lock_read(lock);
+  uint64_t current_generation = *generation_ptr;
+  if (*flag_ptr) {
+    CRYPTO_STATIC_MUTEX_unlock_read(lock);
+    return current_generation;
+  }
+
+  CRYPTO_STATIC_MUTEX_unlock_read(lock);
+  CRYPTO_STATIC_MUTEX_lock_write(lock);
+  current_generation = *generation_ptr;
+  if (*flag_ptr == 0) {
+    // A fork has occurred.
+    *flag_ptr = 1;
+
+    current_generation++;
+    if (current_generation == 0) {
+      current_generation = 1;
+    }
+    *generation_ptr = current_generation;
+  }
+  CRYPTO_STATIC_MUTEX_unlock_write(lock);
+
+  return current_generation;
+}
+
+#else   // !OPENSSL_LINUX
+
+uint64_t CRYPTO_get_fork_generation(void) { return 0; }
+
+#endif  // OPENSSL_LINUX
