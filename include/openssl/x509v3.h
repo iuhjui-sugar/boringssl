4a894b3b02fbc3bfe20fd5c133bef816efb177a5
David Benjamin <davidben@google.com>
2019-04-16 14:28:31 -0500

    Silently ignore X509_CHECK_FLAG_ALWAYS_CHECK_SUBJECT.
    This flag is backwards. We want to check the common name less, not more. See if
    anything was actually relying on this.
    
    Update-Note: X509_CHECK_FLAG_ALWAYS_CHECK_SUBJECT is now ignored.
    Change-Id: I8288d57540f8117059e58d72cc173aa4d3077fb6

--- include/openssl/x509v3.h
+++ include/openssl/x509v3.h
@@ -703,7 +703,7 @@ OPENSSL_EXPORT void X509_email_free(STACK_OF(OPENSSL_STRING) *sk);
 OPENSSL_EXPORT STACK_OF(OPENSSL_STRING) *X509_get1_ocsp(X509 *x);
 /* Flags for X509_check_* functions */
 
-/* Always check subject name for host match even if subject alt names present */
+/* Deprecated: this flag does nothing */
 #define X509_CHECK_FLAG_ALWAYS_CHECK_SUBJECT	0x1
 /* Disable wildcard matching for dnsName fields and common name. */
 #define X509_CHECK_FLAG_NO_WILDCARDS	0x2
