29e354ae93a42aa3c52b27892f2d0fb15d83196b
Adam Langley <alangley@gmail.com>
2019-06-04 15:39:06 -0700

    Add AES-KWP
    KWP is Key Wrap with Padding, defined in RFC 5649 and SP 800-38F. Like
    Key Wrap, it's a poor-man's AEAD and shouldn't be used. However, some
    existing systems use it and we need to interoperate.
    
    The interface of the added functions is a little unfortunate, but they
    match the interfaces of the existing Key Wrap functions which, in turn,
    match functions in OpenSSL. Hopefully this way, if OpenSSL ever add
    support, we'll line up.
    
    Change-Id: I3496c288f32230a891261586ca2e9c4ee8456c09

--- crypto/fipsmodule/aes/key_wrap.c
+++ crypto/fipsmodule/aes/key_wrap.c
@@ -48,6 +48,7 @@
 
 #include <openssl/aes.h>
 
+#include <assert.h>
 #include <limits.h>
 #include <string.h>
 
@@ -100,18 +101,17 @@ int AES_wrap_key(const AES_KEY *key, const uint8_t *iv, uint8_t *out,
   return (int)in_len + 8;
 }
 
-int AES_unwrap_key(const AES_KEY *key, const uint8_t *iv, uint8_t *out,
-                   const uint8_t *in, size_t in_len) {
+// aes_unwrap_key_inner performs steps one and two from
+// https://tools.ietf.org/html/rfc3394#section-2.2.2
+static int aes_unwrap_key_inner(const AES_KEY *key, uint8_t *out,
+                                uint8_t *out_iv, const uint8_t *in,
+                                size_t in_len) {
   // See RFC 3394, section 2.2.2. Additionally, note that section 2 requires the
   // plaintext be at least two 8-byte blocks, so the ciphertext must be at least
   // three blocks.
 
   if (in_len > INT_MAX || in_len < 24 || in_len % 8 != 0) {
-    return -1;
-  }
-
-  if (iv == NULL) {
-    iv = kDefaultIV;
+    return 0;
   }
 
   uint8_t A[AES_BLOCK_SIZE];
@@ -133,9 +133,96 @@ int AES_unwrap_key(const AES_KEY *key, const uint8_t *iv, uint8_t *out,
     }
   }
 
-  if (CRYPTO_memcmp(A, iv, 8) != 0) {
+  memcpy(out_iv, A, 8);
+  return 1;
+}
+
+int AES_unwrap_key(const AES_KEY *key, const uint8_t *iv, uint8_t *out,
+                   const uint8_t *in, size_t in_len) {
+  uint8_t calculated_iv[8];
+  if (!aes_unwrap_key_inner(key, out, calculated_iv, in, in_len)) {
+    return -1;
+  }
+
+  if (iv == NULL) {
+    iv = kDefaultIV;
+  }
+  if (CRYPTO_memcmp(calculated_iv, iv, 8) != 0) {
     return -1;
   }
 
   return (int)in_len - 8;
+}
+
+// kPaddingConstant is used in Key Wrap with Padding. See
+// https://tools.ietf.org/html/rfc5649#section-3
+static const uint8_t kPaddingConstant[4] = {0xa6, 0x59, 0x59, 0xa6};
+
+int AES_wrap_key_padded(const AES_KEY *key, uint8_t *out, const uint8_t *in,
+                        size_t in_len) {
+  // See https://tools.ietf.org/html/rfc5649#section-4.1
+  const uint32_t in_len32_be = CRYPTO_bswap4(in_len);
+  const uint64_t in_len64 = in_len;
+  const size_t padded_len = (in_len + 7) & ~7;
+  const size_t out_len = padded_len + 8;
+
+  if (in_len == 0 || in_len64 > 0xffffffffu || in_len + 7 < in_len ||
+      padded_len + 8 < padded_len || out_len > INT_MAX) {
+    return -1;
+  }
+
+  uint8_t block[AES_BLOCK_SIZE];
+  memcpy(block, kPaddingConstant, sizeof(kPaddingConstant));
+  memcpy(block + 4, &in_len32_be, sizeof(in_len32_be));
+
+  if (in_len <= 8) {
+    memset(block + 8, 0, 8);
+    memcpy(block + 8, in, in_len);
+    AES_encrypt(block, out, key);
+    return AES_BLOCK_SIZE;
+  }
+
+  uint8_t *padded_in = OPENSSL_malloc(padded_len);
+  if (padded_in == NULL) {
+    return -1;
+  }
+  assert(padded_len >= 8);
+  memset(padded_in + padded_len - 8, 0, 8);
+  memcpy(padded_in, in, in_len);
+  const int ret = AES_wrap_key(key, block, out, padded_in, padded_len);
+  OPENSSL_free(padded_in);
+  return ret;
+}
+
+int AES_unwrap_key_padded(const AES_KEY *key, uint8_t *out, const uint8_t *in,
+                          size_t in_len) {
+  uint8_t iv[8];
+  if (in_len == AES_BLOCK_SIZE) {
+    uint8_t block[AES_BLOCK_SIZE];
+    AES_decrypt(in, block, key);
+    memcpy(iv, block, sizeof(iv));
+    memcpy(out, block + 8, 8);
+  } else if (!aes_unwrap_key_inner(key, out, iv, in, in_len)) {
+    return -1;
+  }
+
+  if (memcmp(iv, kPaddingConstant, sizeof(kPaddingConstant)) != 0) {
+    return -1;
+  }
+
+  uint32_t claimed_len;
+  memcpy(&claimed_len, iv + 4, sizeof(claimed_len));
+  claimed_len = CRYPTO_bswap4(claimed_len);
+  if (claimed_len <= in_len - 16 || in_len - 8 < claimed_len ||
+      claimed_len > INT_MAX) {
+    return -1;
+  }
+
+  const size_t padded_len = in_len - 8;
+  static const uint8_t kZeros[7] = {0};
+  if (memcmp(out + claimed_len, kZeros, padded_len - claimed_len) != 0) {
+    return -1;
+  }
+
+  return (int)claimed_len;
 }
