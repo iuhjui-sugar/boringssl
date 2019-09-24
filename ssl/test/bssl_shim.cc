8a03536815a4f468a957ddd33c71e9594b9da933
Daniel McArdle <dmcardle@chromium.org>
2019-09-24 11:13:17 -0400

    In-Progress ESNI draft 04.
    Change-Id: Ie6e84fef75f62fa3fa750844641e9d7ce3f930a4

--- ssl/test/bssl_shim.cc
+++ ssl/test/bssl_shim.cc
@@ -676,7 +676,6 @@ static bool CheckHandshakeProperties(SSL *ssl, bool is_resume,
             SSL_pq_experiment_signal_seen(ssl) ? "" : "no ");
     return false;
   }
-
   return true;
 }
 
@@ -1174,7 +1173,6 @@ int main(int argc, char **argv) {
   }
 
   bssl::UniquePtr<SSL_CTX> ssl_ctx;
-
   bssl::UniquePtr<SSL_SESSION> session;
   for (int i = 0; i < initial_config.resume_count + 1; i++) {
     bool is_resume = i > 0;
