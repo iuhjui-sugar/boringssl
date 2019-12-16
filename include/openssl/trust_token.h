8fd6d4fc96eee2ebd6edb20b08600a9432c375c5
Steven Valdez <svaldez@google.com>
2019-12-16 12:12:31 -0500

    In-Progress Trust Token rewrite.
    Change-Id: If236cc8beaf33a80bdad2991c3163f9dd0cb7571

---
+++ include/openssl/trust_token.h
@@ -0,0 +1,120 @@
+/* Copyright (c) 2019, Google Inc.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+
+#ifndef OPENSSL_HEADER_TRUST_TOKEN_H
+#define OPENSSL_HEADER_TRUST_TOKEN_H
+
+#include <vector>
+
+#include <openssl/base.h>
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+struct trust_token_st {
+  uint32_t data;
+};
+
+// Initialize a client using the 'cleartext' protocol and public key
+// |public_key|.
+OPENSSL_EXPORT TT_CTX *TRUST_TOKEN_Clear_InitClient(uint32_t public_key);
+
+// Initialize an issuer using the 'cleartext' protocol and private key
+// |private_key|.
+OPENSSL_EXPORT TT_CTX *TRUST_TOKEN_Clear_InitIssuer(uint32_t private_key);
+
+/* OPENSSL_EXPORT bool TRUST_TOKEN_PrivacyPass_InitKey( */
+/*     uint16_t ciphersuite, std::vector<uint8_t> *priv_key, */
+/*     std::vector<uint8_t> *pub_key); */
+// Initialize a client using the 'PrivacyPass' protocol and public key
+// |public_key|.
+//OPENSSL_EXPORT TT_CTX *TRUST_TOKEN_Client_InitPrivacyPass;
+
+// Initialize an issuer using the 'PrivacyPass' protocol and private key
+// |private_key|.
+//OPENSSL_EXPORT TT_CTX *TRUST_TOKEN_Issuer_InitPrivacyPass;
+
+// TRUST_TOKEN_free releases memory associated with |ctx|.
+OPENSSL_EXPORT void TRUST_TOKEN_free(TT_CTX *ctx);
+
+// TRUST_TOKEN_Client_BeginIssuance produces a request for |count| trust tokens
+// and serializes the request into a newly allocated buffer and sets |*out| to
+// that buffer and |*out_len| to its length. The caller takes ownership of the
+// buffer and must call |OPENSSL_free| when done. It returns true on success and
+// false on error.
+OPENSSL_EXPORT bool TRUST_TOKEN_Client_BeginIssuance(TT_CTX *ctx,
+                                                     std::vector<uint8_t> *out,
+                                                     size_t count);
+
+// TRUST_TOKEN_Issuer_PerformIssuance ingests a |request| for token issuance and
+// generates valid tokens, producing a list of blinded tokens and storing the
+// response into a newly allocated buffer and setting |*out| to that buffer and
+// |*out_len| to its length. The caller takes ownership of the buffer and must
+// call |OPENSSL_free| when done. It returns true on success and false on error.
+OPENSSL_EXPORT bool TRUST_TOKEN_Issuer_PerformIssuance(
+    TT_CTX *ctx, std::vector<uint8_t> *out,
+    const std::vector<uint8_t> request);
+
+// TRUST_TOKEN_Client_FinishIssuance consumes a |response| from the issuer and
+// extracts the tokens, allocating a buffer to store pointers to each token and
+// setting |*tokens| to that buffer and |*tokens_len| to its length. The caller
+// takes ownership of the buffer and must call |OPENSSL_free| when done. It
+// returns true on success and false on error.
+OPENSSL_EXPORT bool TRUST_TOKEN_Client_FinishIssuance(
+    TT_CTX *ctx, std::vector<TRUST_TOKEN *> *tokens,
+    const std::vector<uint8_t> response);
+
+OPENSSL_EXPORT bool TRUST_TOKEN_Client_BeginRedemption(
+    TT_CTX *ctx, std::vector<uint8_t> *out, const TRUST_TOKEN *token,
+    const std::vector<uint8_t> data);
+
+// TODO: Add timestamp.
+OPENSSL_EXPORT bool TRUST_TOKEN_Issuer_PerformRedemption(
+    TT_CTX *ctx, std::vector<uint8_t> *out, const std::vector<uint8_t> request);
+
+// TODO: Add a way to extract the SRR.
+OPENSSL_EXPORT bool TRUST_TOKEN_Client_FinishRedemption(
+    TT_CTX *ctx, bool *result, const std::vector<uint8_t> response);
+
+// Protocol, 
+
+/* // Generates a PrivacyPass key. (TODO: Parameterize to support multiple key types).  */
+/* OPENSSL_EXPORT TRUST_TOKEN_Issuer_CreateKey(id); */
+
+/* PrivateMetadataKeys { */
+/*   PKey *trueKey; */
+/*   PKey *falseKey; */
+/* }; */
+
+/* MetadataKeyConfig { */
+/*   PrivateMetadataKeys[3] keys; */
+/* }; */
+
+/* TRUST_TOKEN_Issuer_SetMetadataKeys(TT_CTX *ctx, const MetadataKeyConfig *keys, size_t len) - Sets the key to use for this instantiation of PrivacyPass. */
+
+
+
+/* Signing Request Data */
+/* TRUST_TOKEN_GenerateKeypair() → (pub, priv) - Generates a public / private keypair for signing request data. */
+
+/* TRUST_TOKEN_SignData(PrivateKey, const uint8_t* data, size_t len) → signature - Generates a signature over the given data.*/
+
+#if defined(__cplusplus)
+}  // extern C
+#endif
+
+#define TRUST_TOKEN_R_OVER_BATCHSIZE 100
+
+#endif  // OPENSSL_HEADER_TRUST_TOKEN_H
---
+++ trust_token/trust_token.cc
@@ -0,0 +1,120 @@
+/* Copyright (c) 2019, Google Inc.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <openssl/bytestring.h>
+#include <openssl/err.h>
+#include <openssl/mem.h>
+#include <openssl/trust_token.h>
+
+#include "internal.h"
+
+
+void TRUST_TOKEN_free(TT_CTX *ctx) {
+  ctx->method->tt_free(ctx);
+  OPENSSL_free(ctx);
+}
+
+bool TRUST_TOKEN_Client_BeginIssuance(TT_CTX *ctx, std::vector<uint8_t> *out,
+                                      size_t count) {
+  return ctx->method->client_begin_issuance(ctx, out, count);
+}
+
+bool TRUST_TOKEN_Issuer_PerformIssuance(TT_CTX *ctx, std::vector<uint8_t> *out,
+                                        const std::vector<uint8_t> request) {
+  return ctx->method->issuer_do_issuance(ctx, out, request);
+}
+
+bool TRUST_TOKEN_Client_FinishIssuance(TT_CTX *ctx,
+                                       std::vector<TRUST_TOKEN *> *tokens,
+                                       const std::vector<uint8_t> response) {
+  return ctx->method->client_finish_issuance(ctx, tokens, response);
+}
+
+
+bool TRUST_TOKEN_Client_BeginRedemption(TT_CTX *ctx, std::vector<uint8_t> *out,
+                                        const TRUST_TOKEN *token,
+                                        const std::vector<uint8_t> data) {
+  std::vector<uint8_t> inner;
+  if (!ctx->method->client_begin_redemption(ctx, &inner, token)) {
+    return false;
+  }
+  CBB request;
+  if (!CBB_init(&request, 0) ||
+      !CBB_add_u16(&request, inner.size()) ||
+      !CBB_add_bytes(&request, inner.data(), inner.size()) ||
+      !CBB_add_u16(&request, data.size()) ||
+      !CBB_add_bytes(&request, data.data(), data.size())) {
+    return false;
+  }
+  uint8_t *der;
+  size_t der_len;
+  if (!CBB_finish(&request, &der, &der_len)) {
+    return false;
+  }
+  OPENSSL_free(der);
+  out->assign(der, der + der_len);
+  return true;
+}
+
+bool TRUST_TOKEN_Issuer_PerformRedemption(TT_CTX *ctx,
+                                          std::vector<uint8_t> *out,
+                                          const std::vector<uint8_t> request) {
+  CBS in(request);
+  uint16_t inner_request_len;
+  if (!CBS_get_u16(&in, &inner_request_len)) {
+    return false;
+  }
+
+  std::vector<uint8_t> inner;
+  inner.assign(CBS_data(&in), CBS_data(&in) + inner_request_len);
+  bool result;
+  if (!ctx->method->issuer_do_redemption(ctx, &result, inner)) {
+    return false;
+  }
+  CBB response;
+  if (!CBB_init(&response, 0) ||
+      !CBB_add_u8(&response, result)) {
+    return false;
+  }
+  if (result) {
+    // TODO: Add SRR.
+    // Timestamp + DATA + Signature
+  }
+
+  uint8_t *der;
+  size_t der_len;
+  if (!CBB_finish(&response, &der, &der_len)) {
+    return false;
+  }
+  OPENSSL_free(der);
+  out->assign(der, der + der_len);
+  return true;
+}
+
+bool TRUST_TOKEN_Client_FinishRedemption(TT_CTX *ctx, bool *result, const std::vector<uint8_t> response) {
+  CBS in(response);
+  uint8_t res;
+  if (!CBS_get_u8(&in, &res)) {
+    return false;
+  }
+  *result = (res == 1);
+  if (*result) {
+    // TODO: Extract SRR.
+  }
+  return true;
+}
