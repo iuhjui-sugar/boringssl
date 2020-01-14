11ec533d877d2400d50a6d16aba4af81a3a6779b
Augusto Righetto <aurighet@microsoft.com>
2020-01-14 12:22:21 -0800

    Calling RtlGenRandom on Desktop Apps and BCryptGenRandom on UWP Apps
    This CL conditions the API used for generating entropy on the targeted Windows partition.
    BCryptGenRandom will be used on UWP apps and RtlGenRandom will be used on non-UWP apps (Desktop).
    On non-UWP configurations, RtlGenRandom is used instead of BCryptGenRandom to avoid accessing resources that may be unavailable inside the Chromium sandbox.
    
    Bug: 307
    Change-Id: I49f445198b7b4f300a752f45e221a2875d17099e

--- crypto/rand_extra/windows.c
+++ crypto/rand_extra/windows.c
@@ -23,12 +23,22 @@ OPENSSL_MSVC_PRAGMA(warning(push, 3))
 
 #include <windows.h>
 
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP) && \
+    !WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+#ifndef STATUS_SUCCESS
+#define STATUS_SUCCESS ((NTSTATUS)0x00000000L)
+#endif
+#include <bcrypt.h>
+OPENSSL_MSVC_PRAGMA(comment(lib, "bcrypt.lib"))
+#else
 // #define needed to link in RtlGenRandom(), a.k.a. SystemFunction036.  See the
 // "Community Additions" comment on MSDN here:
 // http://msdn.microsoft.com/en-us/library/windows/desktop/aa387694.aspx
 #define SystemFunction036 NTAPI SystemFunction036
 #include <ntsecapi.h>
 #undef SystemFunction036
+#endif /* WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP) && \
+          !WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) */
 
 OPENSSL_MSVC_PRAGMA(warning(pop))
 
@@ -41,7 +51,18 @@ void CRYPTO_sysrand(uint8_t *out, size_t requested) {
     if (requested < output_bytes_this_pass) {
       output_bytes_this_pass = (ULONG)requested;
     }
+    // On non-UWP configurations, use RtlGenRandom instead of BCryptGenRandom
+    // to avoid accessing resources that may be unavailable inside the
+    // Chromium sandbox. See https://crbug.com/boringssl/307
+#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP) && \
+    !WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+    if (BCryptGenRandom(
+            /*hAlgorithm=*/NULL, out, output_bytes_this_pass,
+            BCRYPT_USE_SYSTEM_PREFERRED_RNG) != STATUS_SUCCESS) {
+#else
     if (RtlGenRandom(out, output_bytes_this_pass) == FALSE) {
+#endif /* WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_APP) && \
+          !WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) */
       abort();
     }
     requested -= output_bytes_this_pass;
