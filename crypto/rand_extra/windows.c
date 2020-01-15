fd9b7416449b60989909f993a14364802d4a139c
Augusto Righetto <aurighet@microsoft.com>
2020-01-14 16:41:57 -0800

    Support building as a Windows UWP app.
    RtlGenRandom is a legacy API that might be altered and is unavailable
    for UWP apps. BCryptGenRandom is the recommended API for generating
    random numbers on UWP. Unfortunately, BCryptGenRandom doesn't have the
    same performance level as RtlGenRandom. In order to allow UWP apps to
    benefit from BoringSSL, this change replaces RtlGenRandom with
    BCryptGenRandom only for UWP builds. To the best of my knowledge,
    BCryptGenRandom with BCRYPT_USE_SYSTEM_PREFERRED_RNG should behave
    similarly to RtlGenRandom.
    
    Bug: 307
    Change-Id: I49f445198b7b4f300a752f45e221a2875d17099e

--- crypto/rand_extra/windows.c
+++ crypto/rand_extra/windows.c
@@ -18,9 +18,16 @@
 
 #include <limits.h>
 #include <stdlib.h>
-
 OPENSSL_MSVC_PRAGMA(warning(push, 3))
 
+#if defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_APP)
+#define WIN32_NO_STATUS  // need to define WIN32_NO_STATUS so that subsequent
+#include <windows.h>     // winnt.h includes (via windows.h) will not result
+#undef WIN32_NO_STATUS   // in re-definitions
+#include <bcrypt.h>
+#include <ntstatus.h>
+OPENSSL_MSVC_PRAGMA(comment(lib, "bcrypt.lib"))
+#else
 #include <windows.h>
 
 // #define needed to link in RtlGenRandom(), a.k.a. SystemFunction036.  See the
@@ -29,11 +36,11 @@ OPENSSL_MSVC_PRAGMA(warning(push, 3))
 #define SystemFunction036 NTAPI SystemFunction036
 #include <ntsecapi.h>
 #undef SystemFunction036
+#endif
 
 OPENSSL_MSVC_PRAGMA(warning(pop))
 
 #include "../fipsmodule/rand/internal.h"
-
 
 void CRYPTO_sysrand(uint8_t *out, size_t requested) {
   while (requested > 0) {
@@ -41,7 +48,17 @@ void CRYPTO_sysrand(uint8_t *out, size_t requested) {
     if (requested < output_bytes_this_pass) {
       output_bytes_this_pass = (ULONG)requested;
     }
+
+#if defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_APP)
+    if (BCryptGenRandom(
+            NULL,  // Alg Handle pointer; NUll is passed as
+                   // BCRYPT_USE_SYSTEM_PREFERRED_RNG flag is used
+            out,   // Address of the buffer that recieves the random number(s)
+            output_bytes_this_pass,  // Size of the buffer in bytes
+            BCRYPT_USE_SYSTEM_PREFERRED_RNG) != STATUS_SUCCESS) {
+#else
     if (RtlGenRandom(out, output_bytes_this_pass) == FALSE) {
+#endif
       abort();
     }
     requested -= output_bytes_this_pass;
