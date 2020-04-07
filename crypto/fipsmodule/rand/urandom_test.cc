f1595050951b4dcd94102318b3ed171c9f35d254
Adam Langley <alangley@gmail.com>
2020-04-07 10:53:24 -0700

    Only draw from RDRAND for additional_data if it's fast.
    We seek to incorporate entropy into every |RAND_bytes| call to avoid
    problems with fork() and VM cloning. However, on some chips, RDRAND is
    significantly slower than a system call thus crushing the performance of
    |RAND_bytes|.
    
    This change disables use of RDRAND for this opportunistic draw for
    non-Intel chips. BoringSSL will then fall back to either the OS, or
    nothing (if fork-unsafe mode has been set).
    
    RDRAND is still used for seeding the PRNG whenever it is available.
    
    This now adds a new blocking case: RDRAND may be used for seeding, but
    the syscall to get additional_data was still blocking. Previously that
    didn't matter because, if a syscall was used to get additional_data,
    then a blocking one had already been used to seed. Thus the syscall for
    additional_data is now non-blocking.
    
    Also, we had both |hwrand| and |rdrand| names hanging around. We don't
    support entropy instructions other than RDRAND, so unify around |rdrand|
    naming. If we ever do add support for more we can properly abstract at
    that time.
    
    Change-Id: I91121b270a2ebc667543dad1324f37285daad893

--- crypto/fipsmodule/rand/urandom_test.cc
+++ crypto/fipsmodule/rand/urandom_test.cc
@@ -32,10 +32,6 @@
 #define PTRACE_O_EXITKILL (1 << 20)
 #endif
 
-#if defined(OPENSSL_NO_ASM)
-static int have_rdrand() { return 0; }
-#endif
-
 // This test can be run with $OPENSSL_ia32cap=~0x4000000000000000 in order to
 // simulate the absence of RDRAND of machines that have it.
 
@@ -415,19 +411,24 @@ static std::vector<Event> TestFunctionPRNGModel(unsigned flags) {
   const size_t kAdditionalDataLength = 32;
 
   if (!have_rdrand()) {
-    if (!sysrand(true, kAdditionalDataLength) ||
+    if (!sysrand(false, kAdditionalDataLength) ||
         // Initialise CRNGT.
         (is_fips && !sysrand(true, 16)) ||
         !sysrand(true, kSeedLength) ||
         // Second entropy draw.
-        !sysrand(true, kAdditionalDataLength)) {
+        !sysrand(false, kAdditionalDataLength)) {
       return ret;
     }
   } else {
-    // Opportuntistic entropy draw in FIPS mode because RDRAND was used.
-    // In non-FIPS mode it's just drawn from |CRYPTO_sysrand| in a blocking
-    // way.
-    if (!sysrand(!is_fips, CTR_DRBG_ENTROPY_LEN)) {
+    if (
+        // First additional data.
+        (!have_fast_rdrand() && !sysrand(false, kAdditionalDataLength)) ||
+        // Opportuntistic entropy draw in FIPS mode because RDRAND was used.
+        // In non-FIPS mode it's just drawn from |CRYPTO_sysrand| in a blocking
+        // way.
+        !sysrand(!is_fips, CTR_DRBG_ENTROPY_LEN) ||
+        // Second entropy draw's additional data.
+        (!have_fast_rdrand() && !sysrand(false, kAdditionalDataLength))) {
       return ret;
     }
   }
