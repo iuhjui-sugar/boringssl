32b612dba124313acab694d07097a4081ae0e776
Nick Harper <nharper@chromium.org>
2020-04-30 14:15:49 -0700

    Restrict when 0-RTT will be accepted in QUIC.
    QUIC imposes additional restrictions on when 0-RTT data can be accepted.
    With this change, a QUIC server configured to support 0-RTT will only
    accept early data if the transport parameters and application protocol
    specific context are a byte-for-byte match from the original connection
    to the 0-RTT resumption attempt.
    
    Bug: 295
    Change-Id: Ie5d4688d1c9076b49f2131bb66b27c87e2ba041a

--- include/openssl/ssl.h
+++ include/openssl/ssl.h
@@ -3138,14 +3138,31 @@ OPENSSL_EXPORT int SSL_delegated_credential_used(const SSL *ssl);
 // |SSL_quic_max_handshake_flight_len| to get the maximum buffer length at each
 // encryption level.
 //
-// Note: 0-RTT support is incomplete and does not currently handle QUIC
-// transport parameters and server SETTINGS frame.
-//
 // QUIC implementations must additionally configure transport parameters with
 // |SSL_set_quic_transport_params|. |SSL_get_peer_quic_transport_params| may be
 // used to query the value received from the peer. BoringSSL handles this
 // extension as an opaque byte string. The caller is responsible for serializing
 // and parsing them. See draft-ietf-quic-transport (section 7.3) for details.
+//
+// When a QUIC 0-RTT handshake is performed, the server's decision whether or
+// not to accept early data is based on more factors than just whether a TLS
+// 0-RTT handshake can be performed. In particular, the QUIC transport layer
+// requires that if a server accepts 0-RTT data, then the transport parameters
+// sent on the resumed connection must not lower any limits compared to the
+// transport parameters that the server sent on the connection where the ticket
+// for 0-RTT was issued. In effect, the server must remember the transport
+// parameters with the ticket. Application protocols running on QUIC may impose
+// similar restrictions, for example HTTP/3's restrictions on SETTINGS frames.
+//
+// BoringSSL imposes a stricter check to enforce the rules of transport
+// parameters and application protocol parameters being compatible. BoringSSL
+// requires that the transport parameters and application protocol state be a
+// byte-for-byte match between the connection where the ticket was issued and
+// the connection where it is used for 0-RTT. If there is a mismatch, BoringSSL
+// will reject early data (but not reject the resumption attempt). This check is
+// enforced using the transport parameters set by
+// |SSL_set_quic_transport_params| and the application state provided in
+// |SSL_set_quic_early_data_context|.
 
 // ssl_encryption_level_t represents a specific QUIC encryption level used to
 // transmit handshake messages.
@@ -3292,6 +3309,23 @@ OPENSSL_EXPORT int SSL_set_quic_transport_params(SSL *ssl,
 OPENSSL_EXPORT void SSL_get_peer_quic_transport_params(
     const SSL *ssl, const uint8_t **out_params, size_t *out_params_len);
 
+// SSL_set_quic_early_data_context is used by the server to aid in deciding
+// whether to perform a 0-RTT resumption. If a server SSL configured for QUIC is
+// to support early data, then this function must be called before
+// SSL_do_handshake with the context that must remain the same from the original
+// connection to the resumption connection with early data. If the bytes passed
+// to |SSL_set_quic_early_data_context| on a connection that is attempting to
+// send early data do not match exactly the bytes passed to the function on the
+// connection where the ticket was minted, then early data is rejected.  (This
+// does not affect the decision whether or not to resume; it only affects
+// whether or not to accept early data.)
+//
+// This function returns 1 on success and 0 on failure. It is an error to call
+// this function on an SSL not configured to be a server.
+OPENSSL_EXPORT int SSL_set_quic_early_data_context(SSL *ssl,
+                                                   const uint8_t *context,
+                                                   size_t context_len);
+
 
 // Early data.
 //
@@ -3426,8 +3460,10 @@ enum ssl_early_data_reason_t BORINGSSL_ENUM_INT {
   ssl_early_data_token_binding = 11,
   // The client and server ticket age were too far apart.
   ssl_early_data_ticket_age_skew = 12,
+  // QUIC parameters differ between this connection and the original.
+  ssl_early_data_quic_parameter_mismatch = 13,
   // The value of the largest entry.
-  ssl_early_data_reason_max_value = ssl_early_data_ticket_age_skew,
+  ssl_early_data_reason_max_value = ssl_early_data_quic_parameter_mismatch,
 };
 
 // SSL_get_early_data_reason returns details why 0-RTT was accepted or rejected
