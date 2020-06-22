db30b457f506f05c79ca2c0c260313da35434bc6
Pete Bentley <prb@google.com>
2020-06-22 13:42:49 +0100

    Remove uneeded switch statement.
    Warnings for switch statements with just a default case are
    now fatal with the latest Windows toolchain used by Github
    workflows. So indirectly this was breaking Conscrypt's
    continuous integration and possibly other projects using
    BoringSSL which run CI on Windows.
    
    Example: https://github.com/google/conscrypt/runs/793502854?check_suite_focus=true
    
    Change-Id: Ia09b86f3292299089c6536862a170677a8024984

--- decrepit/bio/base64_bio.c
+++ decrepit/bio/base64_bio.c
@@ -518,12 +518,7 @@ static long b64_callback_ctrl(BIO *b, int cmd, bio_info_cb fp) {
   if (b->next_bio == NULL) {
     return 0;
   }
-  switch (cmd) {
-    default:
-      ret = BIO_callback_ctrl(b->next_bio, cmd, fp);
-      break;
-  }
-  return ret;
+  return BIO_callback_ctrl(b->next_bio, cmd, fp);
 }
 
 static const BIO_METHOD b64_method = {
