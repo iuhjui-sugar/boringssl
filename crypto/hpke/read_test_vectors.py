c0a47e25cdcdfc1916a889ff78482d950f542b47
Daniel McArdle <dmcardle@google.com>
2020-06-30 17:23:15 -0400

    Implement HPKE.
    Change-Id: I4af39be4df534f8c1c991c4df82d38c6adcf2574

---
+++ crypto/hpke/read_test_vectors.py
@@ -0,0 +1,79 @@
+# Lint as: python3
+
+import collections
+import json
+import pprint
+import string
+import sys
+
+
+def flatten_to_str(obj, keypath=[]):
+
+  def build_comment(label):
+    prefix = len(keypath) * "----"
+    postfix = (50 - len(prefix)) * "-"
+    return "# {} {} {}\n".format(prefix, label, postfix)
+
+  s = ""
+  if type(obj) in (dict, collections.OrderedDict):
+    for key in obj:
+      s += flatten_to_str(obj[key], keypath=keypath + [key])
+  elif type(obj) is list:
+    for i, elem in enumerate(obj):
+      label = "{}[{}]".format(keypath[-1], i)
+      s += build_comment(label)
+      s += flatten_to_str(elem, keypath=keypath + [label])
+  else:
+    if keypath:
+      s += "{} = {}\n".format(keypath[-1], str(obj))
+
+  return s
+
+
+def rename_odict_keys(odict, subst):
+  """Rename each of |odict|'s keys by looking up in |subst|.
+
+  Preserves order of key-value pairs.
+  """
+  return collections.OrderedDict([(subst.get(k, k), odict[k]) for k in odict])
+
+
+def read_test_vectors_and_generate_code(json_file_in_path, test_file_out_path):
+  """Reads the file at |json_file_in_path| and writes to |test_file_out_path|."""
+  with open(json_file_in_path) as file_in:
+    test_vectors = json.load(file_in, object_pairs_hook=collections.OrderedDict)
+
+  kHpkeModeBase = 0
+  kHpkeDhkemX25519Sha256 = 0x0020
+
+  test_vectors = filter(lambda v: \
+      v["mode"] == kHpkeModeBase and \
+      v["kemID"] == kHpkeDhkemX25519Sha256, test_vectors)
+
+  # Each test vector contains a list of encryptions (inputs and outputs). To
+  # express this in the BoringSSL "FileTest" format, we must flatten this
+  # structure.
+
+  with open(test_file_out_path, "w") as file_out:
+    for t in test_vectors:
+      t = rename_odict_keys(t, {"nonce": "outerNonce"})
+
+      # Drop fields that are either unnecessary to include or are internal to
+      # the implementation.
+      kDroppedAttributes = ["mode", "kemID", "zz", "secret", "key"]
+      for attr in kDroppedAttributes:
+        t.pop(attr)
+
+      file_out.write(flatten_to_str(t))
+      file_out.write("\n")
+
+
+def main(argv):
+  if len(argv) != 2:
+    raise Exception("Usage: {} TEST_VECTORS_JSON_FILE".format(argv[0]))
+
+  read_test_vectors_and_generate_code(argv[1], "hpke_test_vectors.txt")
+
+
+if __name__ == "__main__":
+  main(sys.argv)
