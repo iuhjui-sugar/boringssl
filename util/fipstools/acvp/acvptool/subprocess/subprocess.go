c4f3d99f020c7929884a4bf9a339b516f55b1f54
Adam Langley <alangley@gmail.com>
2020-10-07 14:48:48 -0700

    acvp: add 3DES-ECB support
    Change-Id: I4ffa2572acce1fdccdf4d3c33680e6d0114bd42b

--- util/fipstools/acvp/acvptool/subprocess/subprocess.go
+++ util/fipstools/acvp/acvptool/subprocess/subprocess.go
@@ -79,6 +79,7 @@ func NewWithIO(cmd *exec.Cmd, in io.WriteCloser, out io.ReadCloser) *Subprocess 
 		"ACVP-AES-ECB":  &blockCipher{"AES", 16, true, false, IterateAES},
 		"ACVP-AES-CBC":  &blockCipher{"AES-CBC", 16, true, true, IterateAESCBC},
 		"ACVP-AES-CTR":  &blockCipher{"AES-CTR", 16, false, true, nil},
+		"ACVP-TDES-ECB": &blockCipher{"3DES-ECB", 8, true, false, Iterate3DES},
 		"ACVP-AES-GCM":  &aead{"AES-GCM", false},
 		"ACVP-AES-CCM":  &aead{"AES-CCM", true},
 		"ACVP-AES-KW":   &aead{"AES-KW", false},
