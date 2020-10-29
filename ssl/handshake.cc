79cc4876aaf4fe5b2388c576803b75fcf0ee4524
Dan McArdle <dmcardle@google.com>
2020-10-29 14:31:31 -0400

    Send ECH acceptance signal from backend server
    Bug: 275
    Change-Id: I2e162673ce564db0cb75fc9b71ef11ed15037f4b

--- ssl/handshake.cc
+++ ssl/handshake.cc
@@ -126,6 +126,9 @@ BSSL_NAMESPACE_BEGIN
 
 SSL_HANDSHAKE::SSL_HANDSHAKE(SSL *ssl_arg)
     : ssl(ssl_arg),
+      ech_server_sent_accept(false),
+      ech_present(false),
+      ech_is_inner_present(false),
       scts_requested(false),
       needs_psk_binder(false),
       handshake_finalized(false),
