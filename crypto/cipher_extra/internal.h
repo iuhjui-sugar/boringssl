9b18d7d9b9327e6c5f031ad0dadeb41123594552
Vlad Krasnov <vlad@cloudflare.com>
2020-12-04 18:21:34 +0000

    Optimized ChaCha20-Poly1305 for AArch64
    On a developement ARM N1 platform I am getting:
    
    Current implementation:
    
    Did 2375000 ChaCha20-Poly1305 (16 bytes) seal operations in 1000058us (2374862.3 ops/sec): 38.0 MB/s
    Did 1063000 ChaCha20-Poly1305 (256 bytes) seal operations in 1000257us (1062726.9 ops/sec): 272.1 MB/s
    Did 260000 ChaCha20-Poly1305 (1350 bytes) seal operations in 1001796us (259533.9 ops/sec): 350.4 MB/s
    Did 50000 ChaCha20-Poly1305 (8192 bytes) seal operations in 1009494us (49529.8 ops/sec): 405.7 MB/s
    Did 25000 ChaCha20-Poly1305 (16384 bytes) seal operations in 1002504us (24937.6 ops/sec): 408.6 MB/s
    
    This implementation:
    
    Did 3232000 ChaCha20-Poly1305 (16 bytes) seal operations in 1000253us (3231182.5 ops/sec): 51.7 MB/s
    Did 1701000 ChaCha20-Poly1305 (256 bytes) seal operations in 1000112us (1700809.5 ops/sec): 435.4 MB/s
    Did 564000 ChaCha20-Poly1305 (1350 bytes) seal operations in 1001099us (563380.8 ops/sec): 760.6 MB/s
    Did 116000 ChaCha20-Poly1305 (8192 bytes) seal operations in 1007228us (115167.6 ops/sec): 943.5 MB/s
    Did 59000 ChaCha20-Poly1305 (16384 bytes) seal operations in 1002955us (58826.2 ops/sec): 963.8 MB/s
    
    For reference with OpenSSL on same platform:
    
    8192: 841.7 MB/s
    16384: 861.2 MB/s
    
    On an Apple M1:
    
    Current implementation:
    
    Did 3736000 ChaCha20-Poly1305 (16 bytes) seal operations in 1000236us (3735118.5 ops/sec): 59.8 MB/s
    Did 2133000 ChaCha20-Poly1305 (256 bytes) seal operations in 1000304us (2132351.8 ops/sec): 545.9 MB/s
    Did 588000 ChaCha20-Poly1305 (1350 bytes) seal operations in 1000908us (587466.6 ops/sec): 793.1 MB/s
    Did 125000 ChaCha20-Poly1305 (8192 bytes) seal operations in 1006660us (124173.0 ops/sec): 1017.2 MB/s
    Did 63000 ChaCha20-Poly1305 (16384 bytes) seal operations in 1003798us (62761.6 ops/sec): 1028.3 MB/s
    
    This implementation:
    
    Did 4181000 ChaCha20-Poly1305 (16 bytes) seal operations in 1000194us (4180189.0 ops/sec): 66.9 MB/s
    Did 3112000 ChaCha20-Poly1305 (256 bytes) seal operations in 1000313us (3111026.2 ops/sec): 796.4 MB/s
    Did 953000 ChaCha20-Poly1305 (1350 bytes) seal operations in 1001002us (952046.0 ops/sec): 1285.3 MB/s
    Did 193000 ChaCha20-Poly1305 (8192 bytes) seal operations in 1000250us (192951.8 ops/sec): 1580.7 MB/s
    Did 98000 ChaCha20-Poly1305 (16384 bytes) seal operations in 1000946us (97907.4 ops/sec): 1604.1 MB/s
    
    Change-Id: If5d694311bde4cfa67b7e59a31d4f96a071349a0

--- crypto/cipher_extra/internal.h
+++ crypto/cipher_extra/internal.h
@@ -153,7 +153,8 @@ union chacha20_poly1305_seal_data {
   } out;
 };
 
-#if defined(OPENSSL_X86_64) && !defined(OPENSSL_NO_ASM)
+#if (defined(OPENSSL_X86_64) || defined(OPENSSL_AARCH64)) && \
+    !defined(OPENSSL_NO_ASM)
 
 OPENSSL_STATIC_ASSERT(sizeof(union chacha20_poly1305_open_data) == 48,
                       "wrong chacha20_poly1305_open_data size");
@@ -161,8 +162,12 @@ OPENSSL_STATIC_ASSERT(sizeof(union chacha20_poly1305_seal_data) == 48 + 8 + 8,
                       "wrong chacha20_poly1305_seal_data size");
 
 OPENSSL_INLINE int chacha20_poly1305_asm_capable(void) {
+#if defined(OPENSSL_X86_64)
   const int sse41_capable = (OPENSSL_ia32cap_P[1] & (1 << 19)) != 0;
   return sse41_capable;
+#elif defined(OPENSSL_AARCH64)
+  return CRYPTO_is_NEON_capable();
+#endif
 }
 
 // chacha20_poly1305_open is defined in chacha20_poly1305_x86_64.pl. It decrypts
