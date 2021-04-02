05a29f4c11248f466c0c30fce626887c6d83fe9b
David Benjamin <davidben@google.com>
2021-04-01 23:21:53 -0400

    Add SSL_can_release_private_key.
    Callers using private key callbacks may retain non-trivial state with a
    private key. In many cases, the private key is no longer necessary
    immediately after the first round-trip (e.g. non-HRR TLS 1.3
    connections). Add a function that callers can query to drop the state a
    hair earlier.
    
    This is tested in two ways. First, the asserts in front of using the
    key, combined with existing tests, ensure we don't start reporting it
    too early. Second, I've added tests in ssl_test.cc to assert we report
    it as early as we expect to.
    
    In doing so, the number of parameters on ConnectClientAndServer()
    started getting tedious, so I've split that into a
    CreateClientAndServer() and CompleteHandshakes(). Callers that need to
    configure weird things or drive the handshake manually can call
    CreateClientAndServer() (which takes care of the BIO pair business) and
    continue from there.
    
    Bug: b/183734559
    Change-Id: I05e1edb6d269c8468ba7cde7dc90e0856694a0ca

--- ssl/handshake_server.cc
+++ ssl/handshake_server.cc
@@ -916,6 +916,7 @@ static enum ssl_hs_wait_t do_select_parameters(SSL_HANDSHAKE *hs) {
     hs->ticket_expected = renew_ticket;
     ssl->session = std::move(session);
     ssl->s3->session_reused = true;
+    hs->can_release_private_key = true;
   } else {
     hs->ticket_expected = tickets_supported;
     ssl_set_session(ssl, NULL);
@@ -1221,6 +1222,7 @@ static enum ssl_hs_wait_t do_send_server_key_exchange(SSL_HANDSHAKE *hs) {
     }
   }
 
+  hs->can_release_private_key = true;
   if (!ssl_add_message_cbb(ssl, cbb.get())) {
     return ssl_hs_error;
   }
@@ -1553,6 +1555,7 @@ static enum ssl_hs_wait_t do_read_client_key_exchange(SSL_HANDSHAKE *hs) {
   }
   hs->new_session->extended_master_secret = hs->extended_master_secret;
   CONSTTIME_DECLASSIFY(hs->new_session->secret, hs->new_session->secret_length);
+  hs->can_release_private_key = true;
 
   ssl->method->next_message(ssl);
   hs->state = state12_read_client_certificate_verify;
