9592287f5b12748f74f29c7a58377fe409186183
Steven Valdez <svaldez@chromium.org>
2021-04-29 10:52:42 -0400

    Update HPKE to draft 08.
    Change-Id: I75fc699dd211e0a0d692d88d956af7ac25a902be

--- ssl/test/runner/hpke/kem.go
+++ ssl/test/runner/hpke/kem.go
@@ -23,7 +23,7 @@ import (
 )
 
 const (
-	rfcLabel string = "HPKE-07"
+	versionLabel string = "HPKE-v1"
 )
 
 func getKDFHash(kdfID uint16) crypto.Hash {
@@ -40,7 +40,7 @@ func getKDFHash(kdfID uint16) crypto.Hash {
 
 func labeledExtract(kdfHash crypto.Hash, salt, suiteID, label, ikm []byte) []byte {
 	var labeledIKM []byte
-	labeledIKM = append(labeledIKM, rfcLabel...)
+	labeledIKM = append(labeledIKM, versionLabel...)
 	labeledIKM = append(labeledIKM, suiteID...)
 	labeledIKM = append(labeledIKM, label...)
 	labeledIKM = append(labeledIKM, ikm...)
