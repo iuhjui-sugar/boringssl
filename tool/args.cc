31effbc71ecb093484ecc54fd25c93b8a9bf7174
Dan McArdle <dmcardle@google.com>
2021-07-20 12:55:35 -0400

    Check strtoul return for overflow error in GetUnsigned()
    Currently, GetUnsigned() calls strtoul and checks whether the resulting
    unsigned long int is greater than UINT_MAX. This implicitly assumes that
    UINT_MAX < ULONG_MAX.
    
    Problematically, `unsigned long int` and `unsigned` have the same size
    on Windows [0] and on 32-bit architectures.
    
    For correctness, we now check whether strtoul failed because it would
    overflow the unsigned long int before checking whether the value fits in
    an unsigned type.
    
    [0]: https://docs.microsoft.com/en-us/cpp/cpp/data-type-ranges?view=msvc-160
    
    Change-Id: I49702febf4543bfb7991592717443e0b2adb954f

--- tool/args.cc
+++ tool/args.cc
@@ -16,6 +16,7 @@
 #include <vector>
 
 #include <limits.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -92,13 +93,16 @@ bool GetUnsigned(unsigned *out, const std::string &arg_name,
     return false;
   }
 
+  errno = 0;
   char *endptr;
   unsigned long int num = strtoul(value.c_str(), &endptr, 10);
-  if (*endptr ||
-      num > UINT_MAX) {
+  if (num == ULONG_MAX && errno == ERANGE) {
     return false;
   }
+  if (*endptr != 0 || num > UINT_MAX) {
+    return false;
+  }
+  *out = static_cast<unsigned>(num);
 
-  *out = num;
   return true;
 }
