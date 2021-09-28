d8d25337cb24c44d45b993c02b32a44c91bd67fa
Benjamin Brittain <bwb@google.com>
2021-09-28 10:00:50 -0400

    Generates "low-level" bindings for Rust using bindgen
    This is designed to be the minimal infrastructure required to support
    using BoringSSL in the Rust ecosystem without fear of ABI drift. Bindgen
    is used to generate Rust bindings in lockstep with the rest of the
    build. `rust-openssl` can consume these generated bindings with minimal
    changes.
    
    Change-Id: I1dacd36a4131e22a930ebb01da00407e8465ad7e

---
+++ rust/README.txt
@@ -0,0 +1,7 @@
+bssl-sys
+============
+
+A low-level binding crate for Rust that moves in lockstop with boringssl. Boringssl explicitly does not have a stable ABI, `bssl-sys` is the solution for preventing subtle-memory corruption bugs due to version skew.
+
+### How it works
+`bssl-sys` uses `bindgen` as part of the cmake build process to generate Rust compatibility shims for each supported target platform. It is important to generate it for each platform because `bindgen` uses llvm information for alignment which varies depending on architecture. These files are then packaged into a Rust crate.
---
+++ rust/src/lib.rs
@@ -0,0 +1,32 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+mod bn;
+mod nid;
+
+pub use bn::*;
+pub use nid::*;
+
+// Non-ABI breaking constants
+pub const DTLS1_COOKIE_LENGTH: libc::c_uint = 256;
+pub const PKCS12_DEFAULT_ITER: libc::c_int = 2048;
+
+// populated by cmake
+${INCLUDES}
+
+pub fn ERR_GET_LIB(packed_error: u32) -> i32 {
+    ((packed_error >> 24) & 0xff) as i32
+}
+
+pub fn ERR_GET_REASON(packed_error: u32) -> i32 {
+    (packed_error & 0xfff) as i32
+}
+
+pub fn ERR_GET_FUNC(packed_error: u32) -> i32 {
+    0
+}
+
+pub fn init() {
+    unsafe { CRYPTO_library_init(); }
+}
