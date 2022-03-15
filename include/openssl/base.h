c99e690b2ed70c92a21bcd710e3e1f031a0ac033
Ma Aiguo <maaiguo@uniontech.com>
2022-03-15 15:22:22 +0800

    feat: add loong architecture support
    Change-Id: I3bb21fa03482738ff2f6060af0835d9685832cef

--- include/openssl/base.h
+++ include/openssl/base.h
@@ -118,6 +118,12 @@ extern "C" {
 #define OPENSSL_32_BIT
 #elif defined(__myriad2__)
 #define OPENSSL_32_BIT
+#elif defined(__loongarch32)
+#define OPENSSL_32_BIT
+#define OPENSSL_LOONG32
+#elif defined(__loongarch64)
+#define OPENSSL_64_BIT
+#define OPENSSL_LOONG64
 #else
 // Note BoringSSL only supports standard 32-bit and 64-bit two's-complement,
 // little-endian architectures. Functions will not produce the correct answer
