5fa8789c661973b2788fdf326339cc0d76870971
Adam Langley <alangley@gmail.com>
2022-05-10 11:44:16 -0700

    Fetch entropy from a system daemon in FIPS mode on Android.
    Change-Id: I69aba15ccf57d04c66a98755b98221b8688d291a

--- crypto/rand_extra/passive.c
+++ crypto/rand_extra/passive.c
@@ -18,18 +18,86 @@
 
 #if defined(BORINGSSL_FIPS)
 
+#define ENTROPY_READ_LEN \
+  (/* last_block size */ 16 + CTR_DRBG_ENTROPY_LEN * BORINGSSL_FIPS_OVERREAD)
+
+#if defined(OPENSSL_ANDROID)
+
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+
+static int get_seed_from_daemon(uint8_t *out_entropy, size_t out_entropy_len) {
+  uint8_t buffer[ENTROPY_READ_LEN];
+  if (out_entropy_len > sizeof(buffer)) {
+    return 0;
+  }
+
+  const int sock = socket(AF_UNIX, SOCK_STREAM, 0);
+  if (sock < 0) {
+    return 0;
+  }
+
+  int ret = 0;
+  struct sockaddr_un sun;
+  memset(&sun, 0, sizeof(sun));
+  sun.sun_family = AF_UNIX;
+  static const char kSocketPath[] = "/dev/socket/prng_seeder";
+  OPENSSL_memcpy(sun.sun_path, kSocketPath, sizeof(kSocketPath));
+
+  if (connect(sock, (struct sockaddr *)&sun, sizeof(sun))) {
+    goto out;
+  }
+
+  ssize_t n;
+  do {
+    n = read(sock, buffer, sizeof(buffer));
+  } while (n == -1 && errno == EINTR);
+
+  if (n < out_entropy_len) {
+    goto out;
+  }
+
+  assert(out_entropy_len <= sizeof(buffer));
+  OPENSSL_memcpy(out_entropy, buffer, out_entropy_len);
+  ret = 1;
+
+out:
+  close(sock);
+  return ret;
+}
+
+#else
+
+static int get_seed_from_daemon(uint8_t *out_entropy, size_t out_entropy_len) {
+  return 0;
+}
+
+#endif  // OPENSSL_ANDROID
+
 // RAND_need_entropy is called by the FIPS module when it has blocked because of
 // a lack of entropy. This signal is used as an indication to feed it more.
 void RAND_need_entropy(size_t bytes_needed) {
-  uint8_t buf[/* last_block size */ 16 +
-              CTR_DRBG_ENTROPY_LEN * BORINGSSL_FIPS_OVERREAD];
+  uint8_t buf[ENTROPY_READ_LEN];
   size_t todo = sizeof(buf);
   if (todo > bytes_needed) {
     todo = bytes_needed;
   }
 
   int want_additional_input;
-  CRYPTO_get_seed_entropy(buf, todo, &want_additional_input);
+  if (get_seed_from_daemon(buf, todo)) {
+    want_additional_input = 1;
+  } else {
+    CRYPTO_get_seed_entropy(buf, todo, &want_additional_input);
+  }
+
+  if (boringssl_fips_break_test("CRNG")) {
+    // This breaks the "continuous random number generator test" defined in FIPS
+    // 140-2, section 4.9.2, and implemented in |rand_get_seed|.
+    OPENSSL_memset(buf, 0, todo);
+  }
+
   RAND_load_entropy(buf, todo, want_additional_input);
 }
 
