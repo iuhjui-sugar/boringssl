760244c5426a3a44a2dba81de2fb592eed188bb0
David Benjamin <davidben@google.com>
2022-12-26 19:38:40 -0500

    Rewrite X.509 policy tree logic.
    This reimplements policy handling using a similar DAG structure as in
    https://chromium-review.googlesource.com/c/chromium/src/+/4111415. The
    main difference is that, being C, we don't have std::set or std::map
    easily available. But the algorithm can be implemented purely with
    sorted lists, while remaining subquadratic.
    
    This implementation relies on two assumptions:
    
    1. We do not return the policy tree. This was removed in
       https://boringssl-review.googlesource.com/c/boringssl/+/53327
    
    2. We do not return the final set of certificate policies. I.e.,
       certificate policy checking is only used for evaluating policy
       constraints and X509_V_FLAG_EXPLICIT_POLICY.
    
    The second assumption is not very important. It mostly simplifies
    has_explicit_policy slightly.
    
    In addition, this new implementation removes the per-certificate policy
    cache. Instead, we just process the policy extensions anew on
    certificate verification. This avoids a mess of threading complexity,
    including a race condition in the old logic. See
    https://boringssl-review.googlesource.com/c/boringssl/+/55762 for a
    description of the race condition.
    
    Change-Id: Ifba9037588ecff5eb6ed3c34c8bd7611f60013a6

--- crypto/x509/x_x509.c
+++ crypto/x509/x_x509.c
@@ -90,8 +90,6 @@ ASN1_SEQUENCE_enc(X509_CINF, enc, 0) = {
 IMPLEMENT_ASN1_FUNCTIONS(X509_CINF)
 // X509 top level structure needs a bit of customisation
 
-extern void policy_cache_free(X509_POLICY_CACHE *cache);
-
 static int x509_cb(int operation, ASN1_VALUE **pval, const ASN1_ITEM *it,
                    void *exarg) {
   X509 *ret = (X509 *)*pval;
@@ -151,7 +149,6 @@ static int x509_cb(int operation, ASN1_VALUE **pval, const ASN1_ITEM *it,
       ASN1_OCTET_STRING_free(ret->skid);
       AUTHORITY_KEYID_free(ret->akid);
       CRL_DIST_POINTS_free(ret->crldp);
-      policy_cache_free(ret->policy_cache);
       GENERAL_NAMES_free(ret->altname);
       NAME_CONSTRAINTS_free(ret->nc);
       CRYPTO_BUFFER_free(ret->buf);
