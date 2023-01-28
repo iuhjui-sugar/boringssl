14e844ef5d0986914d385883328914d69a2f961c
Alex Gaynor <alex.gaynor@gmail.com>
2023-01-28 13:04:25 -0500

    Use bindgen's support for handling static inline functions in rust/
    Also install some rust artifacts with cmake's install support.
    
    The result of this will be that rust-openssl can consume boringssl without build contortions.
    
    Change-Id: I0fa47b4124090bb397070124bf0a887474b3e805

--- rust/bssl-sys/src/lib.rs
+++ rust/bssl-sys/src/lib.rs
@@ -5,18 +5,6 @@
 // populated by cmake
 ${INCLUDES}
 
-pub fn ERR_GET_LIB(packed_error: u32) -> i32 {
-    unsafe { ERR_GET_LIB_RUST(packed_error) }
-}
-
-pub fn ERR_GET_REASON(packed_error: u32) -> i32 {
-    unsafe { ERR_GET_REASON_RUST(packed_error) }
-}
-
-pub fn ERR_GET_FUNC(packed_error: u32) -> i32 {
-    unsafe { ERR_GET_FUNC_RUST(packed_error) }
-}
-
 pub fn init() {
     unsafe { CRYPTO_library_init(); }
 }
