d4040f092e72d64784620e6a2f20f5f5d0636592
David Benjamin <davidben@google.com>
2023-02-01 23:40:18 -0500

    Stop clang from un-constant-timing copy_from_prebuf.
    Newer versions of clang figure out that copy_from_prebuf (used in builds
    that aren't x86_64 with assembly optimizations) has a bunch of no-op
    iterations and insert a branch. Add a value barrier to stop it. This was
    caught by our valgrind-based constant-time validation.
    
    As part of this, I noticed that OPENSSL_NO_ASM builds turn off value
    barriers. This is because the value barriers use an empty inline asm
    block. While this is technically correct, it's probably unnecessary.
    
    The clang|gcc check means we know GCC-style inline assembly is
    supported.  Disabling inline asm is used by sanitizers to shut off
    unintrumentable code, but there's no uninstrumentable code in the empty
    string. It's also used by consumers who haven't figured out how to
    integrate an assembler into their build system, but that also doesn't
    apply. So just remove the condition on the value barriers so
    OPENSSL_NO_ASM also get mitigations.
    
    Update-Note: It is possible the above is wrong and some OPENSSL_NO_ASM
    relied on value barriers being disabled. If so, this will break that
    build and we'll need to reconsider.
    
    Change-Id: I6e3ea3ee705bef3afcf42d3532b17aaabbbcc60b

--- crypto/fipsmodule/bn/exponentiation.c
+++ crypto/fipsmodule/bn/exponentiation.c
@@ -852,7 +852,7 @@ static int copy_from_prebuf(BIGNUM *b, int top, const BN_ULONG *table, int idx,
   OPENSSL_memset(b->d, 0, sizeof(BN_ULONG) * top);
   const int width = 1 << window;
   for (int i = 0; i < width; i++, table += top) {
-    BN_ULONG mask = constant_time_eq_int(i, idx);
+    BN_ULONG mask = value_barrier_w(constant_time_eq_int(i, idx));
     for (int j = 0; j < top; j++) {
       b->d[j] |= table[j] & mask;
     }
