briansmith/ring

commit 0e68520eb27b1f37038e9d0772cfee1d015b50c3

Specify -Iinclude with the crypto target.
It's unclear to me whether doing it target-by-target is an improvement
in crypto/fipsmodule, but this otherwise does seem a bit tidier. This
aligns with CMake's documentation and "modern CMake" which prefers this
pattern.

Change-Id: I36c81842bff8b36eeaaf5dd3e0695fb45f3376c9
Reviewed-on: https://boringssl-review.googlesource.com/c/boringssl/+/56585
Commit-Queue: David Benjamin <davidben@google.com>
Reviewed-by: Bob Beck <bbe@google.com>

util/generate_build_files.py
@@ -487,8 +487,6 @@ if(BUILD_SHARED_LIBS)
   set(CMAKE_POSITION_INDEPENDENT_CODE TRUE)
 endif()
 
-include_directories(src/include)
-
 '''
 
   def PrintLibrary(self, out, name, files):
@@ -543,6 +541,7 @@ endif()
 
       self.PrintLibrary(cmake, 'crypto',
           files['crypto'] + ['${CRYPTO_SOURCES_ASM_USED}'])
+      cmake.write('target_include_directories(crypto PUBLIC src/include)\n')
       self.PrintLibrary(cmake, 'ssl', files['ssl'])
       self.PrintExe(cmake, 'bssl', files['tool'], ['ssl', 'crypto'])
 

crypto/CMakeLists.txt
@@ -1,5 +1,3 @@
-include_directories(../include)
-
 add_subdirectory(fipsmodule)
 add_subdirectory(test)
 
@@ -298,7 +296,7 @@ endif()
 if(OPENSSL_NASM)
   target_sources(crypto PRIVATE ${CRYPTO_SOURCES_NASM})
 endif()
-target_include_directories(crypto INTERFACE
+target_include_directories(crypto PUBLIC
   $<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/include>
   $<INSTALL_INTERFACE:include>
 )

crypto/fipsmodule/CMakeLists.txt
@@ -1,5 +1,3 @@
-include_directories(../../include)
-
 perlasm(BCM_SOURCES aarch64 aesv8-armv8 aes/asm/aesv8-armx.pl)
 perlasm(BCM_SOURCES aarch64 aesv8-gcm-armv8 modes/asm/aesv8-gcm-armv8.pl)
 perlasm(BCM_SOURCES aarch64 armv8-mont bn/asm/armv8-mont.pl)
@@ -67,9 +65,8 @@ if(FIPS_DELOCATE)
 
     bcm.c
   )
-
   add_dependencies(bcm_c_generated_asm global_target)
-
+  target_include_directories(bcm_c_generated_asm PRIVATE ../../include)
   set_target_properties(bcm_c_generated_asm PROPERTIES COMPILE_OPTIONS "-S")
   set_target_properties(bcm_c_generated_asm PROPERTIES POSITION_INDEPENDENT_CODE ON)
 
@@ -104,9 +101,7 @@ if(FIPS_DELOCATE)
 
     bcm-delocated.S
   )
-
   add_dependencies(bcm_hashunset global_target)
-
   set_target_properties(bcm_hashunset PROPERTIES POSITION_INDEPENDENT_CODE ON)
   set_target_properties(bcm_hashunset PROPERTIES LINKER_LANGUAGE C)
 
@@ -131,9 +126,8 @@ if(FIPS_DELOCATE)
 
     fips_shared_support.c
   )
-
   add_dependencies(fipsmodule global_target)
-
+  target_include_directories(fipsmodule PRIVATE ../../include)
   set_target_properties(fipsmodule PROPERTIES LINKER_LANGUAGE C)
 elseif(FIPS_SHARED)
   if(NOT BUILD_SHARED_LIBS)
@@ -147,8 +141,8 @@ elseif(FIPS_SHARED)
 
     fips_shared_support.c
   )
-
   add_dependencies(fipsmodule global_target)
+  target_include_directories(fipsmodule PRIVATE ../../include)
 
   add_library(
     bcm_library
@@ -158,8 +152,8 @@ elseif(FIPS_SHARED)
     bcm.c
     ${BCM_SOURCES_ASM_USED}
   )
-
   add_dependencies(bcm_library global_target)
+  target_include_directories(bcm_library PRIVATE ../../include)
 
   add_custom_command(
     OUTPUT bcm.o
@@ -179,6 +173,6 @@ else()
     fips_shared_support.c
     ${BCM_SOURCES_ASM_USED}
   )
-
   add_dependencies(fipsmodule global_target)
+  target_include_directories(fipsmodule PRIVATE ../../include)
 endif()

decrepit/CMakeLists.txt
@@ -1,5 +1,3 @@
-include_directories(../include)
-
 add_library(
   decrepit
 
@@ -21,9 +19,7 @@ add_library(
   x509/x509_decrepit.c
   xts/xts.c
 )
-
 add_dependencies(decrepit global_target)
-
 target_link_libraries(decrepit crypto ssl)
 
 add_executable(
@@ -38,9 +34,7 @@ add_executable(
 
   $<TARGET_OBJECTS:boringssl_gtest_main>
 )
-
 add_dependencies(decrepit_test global_target)
-
 target_link_libraries(decrepit_test test_support_lib boringssl_gtest decrepit
                       crypto)
 add_dependencies(all_tests decrepit_test)

fuzz/CMakeLists.txt
@@ -1,10 +1,7 @@
-include_directories(../include)
-
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-missing-prototypes")
-
 macro(fuzzer name)
   add_executable(${name} ${name}.cc)
   add_dependencies(${name} global_target)
+  target_compile_options(${name} PRIVATE "-Wno-missing-prototypes")
   target_link_libraries(${name} crypto ${ARGN})
   if(LIBFUZZER_FROM_DEPS)
     set_target_properties(${name} PROPERTIES LINK_FLAGS "-fsanitize=fuzzer-no-link")

rust/CMakeLists.txt
@@ -1,12 +1,8 @@
-# additional interop for things like macros and inlined functions
-include_directories(${CMAKE_CURRENT_SOURCE_DIR}/../include)
-add_library(
-  rust_wrapper
-  STATIC
-  rust_wrapper.c
-)
+# Additional interop for things like macros and inlined functions.
+add_library(rust_wrapper STATIC rust_wrapper.c)
+target_link_libraries(rust_wrapper crypto)
 
-# generate architecture specific wrappers
+# Generate architecture-specific wrappers.
 set(WRAPPER_TARGET ${CMAKE_BINARY_DIR}/rust/src/wrapper_${RUST_BINDINGS}.rs)
 set(COMMAND ${BINDGEN_EXECUTABLE} "wrapper.h"
             -o ${WRAPPER_TARGET}

ssl/CMakeLists.txt
@@ -1,5 +1,3 @@
-include_directories(../include)
-
 add_library(
   ssl
 
@@ -41,19 +39,16 @@ add_library(
   tls13_enc.cc
   tls13_server.cc
 )
-target_include_directories(ssl INTERFACE
-  $<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/include>
-  $<INSTALL_INTERFACE:include>
-)
+# Although libssl also provides headers that require an include directory, the
+# flag is already specified by libcrypto, so we omit target_include_directories
+# here.
 install(TARGETS ssl EXPORT OpenSSLTargets
   ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
   LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
   RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR}
 )
 set_property(TARGET ssl PROPERTY EXPORT_NAME SSL)
-
 add_dependencies(ssl global_target)
-
 target_link_libraries(ssl crypto)
 
 add_executable(
@@ -65,8 +60,6 @@ add_executable(
 
   $<TARGET_OBJECTS:boringssl_gtest_main>
 )
-
 add_dependencies(ssl_test global_target)
-
 target_link_libraries(ssl_test test_support_lib boringssl_gtest ssl crypto)
 add_dependencies(all_tests ssl_test)

ssl/test/CMakeLists.txt
@@ -1,5 +1,3 @@
-include_directories(../../include)
-
 add_executable(
   bssl_shim
 

iuhjui-sugar/boringssl

commit f7b7285887e7927b102fb65523122bc7f587a3af

Fix the generated CMake build
This fixes two issues introduced in
0e68520eb27b1f37038e9d0772cfee1d015b50c3. First, libssl never had a
dependency on libcrypto, so the include directory did not get passed
along. Second, although this build (unlike the other CMake build)
doesn't define an install target, gRPC includes it and then pulls it
into its own install target. That then runs afoul of CMake's check
against include directories in install targets.

To avoid this, condition the src/include directory on
$<BUILD_INTERFACE:...> but omit a corresponding
$<INSTALL_INTERFACE:...>. Since we're not the ones providing an install
target, we don't actually know the value to use.

Per [0], using the generator expression means we need to manually make
it absolute ourselves.

[0] https://cmake.org/cmake/help/latest/command/target_include_directories.html

Change-Id: I2a25cc8382116c5957d94f65641220559b7af87d

util/generate_build_files.py
@@ -489,7 +489,7 @@ endif()
 
 '''
 
-  def PrintLibrary(self, out, name, files):
+  def PrintLibrary(self, out, name, files, libs=[]):
     out.write('add_library(\n')
     out.write('  %s\n\n' % name)
 
@@ -497,6 +497,8 @@ endif()
       out.write('  %s\n' % PathOf(f))
 
     out.write(')\n\n')
+    if libs:
+      out.write('target_link_libraries(%s %s)\n\n' % (name, ' '.join(libs)))
 
   def PrintExe(self, out, name, files, libs):
     out.write('add_executable(\n')
@@ -541,8 +543,8 @@ endif()
 
       self.PrintLibrary(cmake, 'crypto',
           files['crypto'] + ['${CRYPTO_SOURCES_ASM_USED}'])
-      cmake.write('target_include_directories(crypto PUBLIC src/include)\n')
-      self.PrintLibrary(cmake, 'ssl', files['ssl'])
+      cmake.write('target_include_directories(crypto PUBLIC $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/src/include>)\n\n')
+      self.PrintLibrary(cmake, 'ssl', files['ssl'], ['crypto'])
       self.PrintExe(cmake, 'bssl', files['tool'], ['ssl', 'crypto'])
 
       cmake.write(