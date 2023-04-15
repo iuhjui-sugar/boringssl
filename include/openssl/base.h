c01ce583a1692017da087b2401dfc88238c6a312
Leslie Zhai <zhaixiang@loongson.cn>
2023-04-15 11:49:15 +0800

    Compile for LoongArch64.
    Change-Id: I72c7481d9d971a8a8894b9b7e84c3c7d4a8aaefb

--- include/openssl/base.h
+++ include/openssl/base.h
@@ -116,6 +116,9 @@ extern "C" {
 #define OPENSSL_32_BIT
 #elif defined(__myriad2__)
 #define OPENSSL_32_BIT
+#elif defined(__loongarch_lp64)
+#define OPENSSL_64_BIT
+#define OPENSSL_LOONGARCH64
 #else
 // Note BoringSSL only supports standard 32-bit and 64-bit two's-complement,
 // little-endian architectures. Functions will not produce the correct answer
