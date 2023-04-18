2781d7725752e868b4662dc97e15000d7200a55a
Bob Beck <bbe@google.com>
2023-04-18 08:35:15 -0600

    Bring in the core of  chromium certificate verifier as libpki
    Initially this leaves the canonical source in chrome, Additions
    and fillins are committed directly, the chrome files are coverted
    using the IMPORT script run from the pki directory for the moment.
    
    The intention here is to continue frequent automatic conversion
    (and avoid wholesale cosmetic changes in here for now) until
    chrome converts to use these files in place of it's versions.
    At that point these will become the definiative files, and the
    IMPORT script can be tossed out.
    
    A middle step along the way will be to change google3's verify.cc
    in third_party/chromium_certificate_verifier to use this instead
    of it's own extracted copy.
    
    Status (and what is not done yet) being roughly tracked in README.md
    
    Bug: chromium:1322914
    
    Change-Id: Ibdb5479bc68985fa61ce6b10f98f31f6b3a7cbdf

---
+++ pki/fillins/base64.h
@@ -0,0 +1,23 @@
+#ifndef BSSL_FILLINS_BASE64_H
+#define BSSL_FILLINS_BASE64_H
+
+#include <openssl/base.h>
+
+#include <string>
+#include <string_view>
+
+namespace bssl {
+
+namespace fillins {
+
+OPENSSL_EXPORT bool Base64Encode(const std::string_view &input,
+                                 std::string *output);
+
+OPENSSL_EXPORT bool Base64Decode(const std::string_view &input,
+                                 std::string *output);
+
+}  // namespace fillins
+
+}  // namespace bssl
+
+#endif  // BSSL_FILLINS_BASE64_H
---
+++ pki/fillins/file_util.h
@@ -0,0 +1,20 @@
+#ifndef BSSL_FILLINS_FILE_UTIL_H
+#define BSSL_FILLINS_FILE_UTIL_H
+
+#include <openssl/base.h>
+
+#include "path_service.h"
+
+#include <string>
+
+namespace bssl {
+
+namespace fillins {
+
+OPENSSL_EXPORT bool ReadFileToString(const FilePath &path, std::string *out);
+
+}  // namespace fillins
+
+}  // namespace bssl
+
+#endif  // BSSL_FILLINS_FILE_UTIL_H
