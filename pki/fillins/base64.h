26f8059f7c0f7c2b82a8509c1497581ef91fef3f
Bob Beck <bbe@google.com>
2023-04-18 08:35:15 -0600

    Bring in chromium certificate verifier as libpki, with tests
    CRBug: 1322914
    
    Change-Id: Ibdb5479bc68985fa61ce6b10f98f31f6b3a7cbdf

---
+++ pki/fillins/base64.h
@@ -0,0 +1,20 @@
+#ifndef EXPERIMENTAL_USERS_AGL_LIBSLEEVI_FILLINS_BASE64_H
+#define EXPERIMENTAL_USERS_AGL_LIBSLEEVI_FILLINS_BASE64_H
+
+#include <string>
+#include <string_view>
+#include <vector>
+
+namespace bssl {
+
+namespace fillins {
+
+bool Base64Encode(const std::string_view& input, std::string* output);
+
+bool Base64Decode(const std::string_view& input, std::string* output);
+
+}  // namespace fillins
+
+}  // namespace bssl
+
+#endif  // EXPERIMENTAL_USERS_AGL_LIBSLEEVI_FILLINS_BASE64_H
