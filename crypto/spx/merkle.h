936ea442b317bd69d28e7ba49da974d2cb580496
Stefan Koelbl <kste@google.com>
2023-06-21 12:20:20 +0000

    Add basic C implementation of SPHINCS+-SHA2-128s.
    The implementation is based on the current round 3 specification with
    the modifications to FORS indices generation suggest on the mailing
    list. The implementation passes test vectors and uses the default SHA256 implementation of BoringSSL.
    
    Change-Id: Iab2dbaf5f692d490577dc940d9f3e298a72e9193

---
+++ crypto/spx/merkle.h
@@ -0,0 +1,49 @@
+/* Copyright (c) 2023, Google LLC
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+#ifndef OPENSSL_HEADER_CRYPTO_SPX_MERKLE_H
+#define OPENSSL_HEADER_CRYPTO_SPX_MERKLE_H
+
+#include <openssl/base.h>
+#include <sys/types.h>
+
+#include "./params.h"
+
+// Compute the root of a Merkle subtree of WOTS+ public keys.
+void treehash(uint8_t out_pk[SPX_N], const uint8_t sk_seed[SPX_N],
+              uint32_t i /*target node index*/,
+              uint32_t z /*target node height*/, const uint8_t pk_seed[SPX_N],
+              uint8_t addr[32]);
+
+// Generate an XMSS signature
+void xmss_sign(uint8_t *sig, const uint8_t msg[SPX_N], unsigned int idx,
+               const uint8_t sk_seed[SPX_N], const uint8_t pk_seed[SPX_N],
+               uint8_t addr[32]);
+
+// Compute an XMSS public key from an XMSS signature
+void xmss_pk_from_sig(uint8_t *root, const uint8_t *xmss_sig, unsigned int idx,
+                      const uint8_t msg[SPX_N], const uint8_t pk_seed[SPX_N],
+                      uint8_t addr[32]);
+
+// Generate a hypertree signature
+void ht_sign(uint8_t *sig, const uint8_t message[SPX_N], uint64_t idx_tree,
+             uint32_t idx_leaf, const uint8_t sk_seed[SPX_N],
+             const uint8_t pk_seed[SPX_N]);
+
+// Verify a hypertree signature
+int ht_verify(const uint8_t sig[SPX_D * SPX_XMSS_BYTES],
+              const uint8_t message[SPX_N], uint64_t idx_tree,
+              uint32_t idx_leaf, const uint8_t pk_root[SPX_N],
+              const uint8_t pk_seed[SPX_N]);
+
+#endif  // OPENSSL_HEADER_CRYPTO_SPX_MERKLE_H
