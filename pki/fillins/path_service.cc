a945edc3488b0082d9af5b29d95f1460a158d20f
Bob Beck <bbe@google.com>
2023-06-22 14:58:21 -0600

    Add pki test files for android, adapt PathService fillin.
    Change-Id: Ie5cb298398c1944704c74b63f51323800f6dc5b0

--- pki/fillins/path_service.cc
+++ pki/fillins/path_service.cc
@@ -7,7 +7,22 @@
 #include <stdlib.h>
 #include <iostream>
 
+#if !defined(OPENSSL_WINDOWS)
+#include <sys/stat.h>
+#endif
+
 namespace bssl {
+
+namespace {
+
+#if !defined(OPENSSL_WINDOWS)
+bool PathExists(const char *path) {
+  struct stat sb;
+  return stat(path, &sb) == 0;
+}
+#endif  // OPENSSL_WINDOWS
+
+}  // namespace
 
 namespace fillins {
 
@@ -28,6 +43,8 @@ FilePath FilePath::AppendASCII(const std::string &ascii_path_element) const {
 
 // static
 void PathService::Get(PathKey key, FilePath *out) {
+  // Figure out where the source code is for getting test data
+  // right from there.
 #if defined(_BORINGSSL_PKI_SRCDIR_)
   // We stringify the compile parameter because cmake. sigh.
 #define _boringssl_xstr(s) _boringssl_str(s)
@@ -36,7 +53,31 @@ void PathService::Get(PathKey key, FilePath *out) {
 #else
 #error "No _BORINGSSL_PKI_SRCDIR"
 #endif  // defined(BORINGSSL_PKI_SRCDIR
-  *out = FilePath(pki_srcdir);
+  // We want to know where the top of pki is to find testdata.  Some things like
+  // to run tests in random places with the data files copied around. Let's be
+  // flexible, maybe we can't see the source directory...
+#if !defined(OPENSSL_WINDOWS)
+  char *pki_srcdir_env = getenv("PKI_SRCDIR");
+  const char pki_curdir[] = "./pki";
+  const char curdir[] = ".";
+  if (pki_srcdir_env && PathExists(pki_srcdir_env)) {
+    // use the PKI_SRCDIR from the environment.
+    *out = FilePath(pki_srcdir_env);
+  } else if (PathExists(pki_srcdir)) {
+    // Try the source code directory
+    *out = FilePath(pki_srcdir);
+  } else if (PathExists(pki_curdir)) {
+    // Try "pki" in the current directory
+    *out = FilePath(pki_curdir);
+  } else {
+    // As a last resort, try just "." and hope for the best, maybe they stripped
+    // pki and copied just the data directory.
+    *out = FilePath(curdir);
+  }
+#else
+    // Use the source code directory
+    *out = FilePath(pki_srcdir);
+#endif 
 }
 
 }  // namespace fillins
