754e059c655a7a6dcd49e069a0db5d46f1801643
Bob Beck <bbe@google.com>
2023-07-25 10:07:38 -0700

    move the fuzzers into the fuzz directory and make them build.
    Change-Id: I9346a4bf48d756da254dc27842cd645a3a69f847

--- fuzz/verify_name_match_normalizename_fuzzer.cc
+++ fuzz/verify_name_match_normalizename_fuzzer.cc
@@ -2,10 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "verify_name_match.h"
+#include "../pki/verify_name_match.h"
 
-#include "cert_errors.h"
-#include "input.h"
+#include "../pki/cert_errors.h"
+#include "../pki/input.h"
 
 // Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
