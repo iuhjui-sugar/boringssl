4676e55dc7d99bf26146eeec2e65c3a8f1aed94c
David Benjamin <davidben@google.com>
2023-10-11 12:51:36 -0400

    Replace remaining references to __ARM_ARCH__ with __ARM_ARCH
    Since ACLE is now widely implemented, just use the standard one. I've
    left a TODO with __ARM_MAX_ARCH__, which is a bit confusing as it uses a
    slighgtly different numbering scheme. But we probably should just remove
    that one?
    
    Bug: 654
    Change-Id: I85bb3160ec64acdabd11d741f1958ff56199c4c7

--- include/openssl/asm_base.h
+++ include/openssl/asm_base.h
@@ -67,24 +67,6 @@
 #endif
 
 #if defined(OPENSSL_ARM) || defined(OPENSSL_AARCH64)
-
-// We require the ARM assembler provide |__ARM_ARCH| from Arm C Language
-// Extensions (ACLE). This is supported in GCC 4.8+ and Clang 3.2+. MSVC does
-// not implement ACLE, but we require Clang's assembler on Windows.
-#if !defined(__ARM_ARCH)
-#error "ARM assembler must define __ARM_ARCH"
-#endif
-
-// __ARM_ARCH__ is used by OpenSSL assembly to determine the minimum target ARM
-// version.
-//
-// TODO(davidben): Switch the assembly to use |__ARM_ARCH| directly.
-#define __ARM_ARCH__ __ARM_ARCH
-
-// Even when building for 32-bit ARM, support for aarch64 crypto instructions
-// will be included.
-#define __ARM_MAX_ARCH__ 8
-
 // Support macros for
 //   - Armv8.3-A Pointer Authentication and
 //   - Armv8.5-A Branch Target Identification
