6aea6885ab96270fd047beaea02410cc92a83e35
Bob Beck <bbe@google.com>
2023-11-07 13:32:48 +0100

    Mark fuchsia as non-forking
    While we are here add a belt and suspenders check to only mark
    non-forking platforms as such if they do not define LINUX.
    
    Change-Id: I172c8504de31eec5ca8661411d8a226008d90f59

--- crypto/fipsmodule/rand/fork_detect.h
+++ crypto/fipsmodule/rand/fork_detect.h
@@ -29,9 +29,13 @@
 // iOS doesn't normally allow fork in apps, but it's there.
 #define OPENSSL_FORK_DETECTION
 #define OPENSSL_FORK_DETECTION_PTHREAD_ATFORK
-#elif defined(OPENSSL_WINDOWS) || defined(OPENSSL_TRUSTY)
-// These platforms do not fork.
+#elif defined(OPENSSL_WINDOWS) || defined(OPENSSL_TRUSTY) || \
+    defined(OPENSSL_FUCHSIA)
+// These platforms do not fork, Mark them as not needingg fork detection
+// as long as they have not also defined LINUX.
+#if !defined(OPENSSL_LINUX)
 #define OPENSSL_DOES_NOT_FORK
+#endif
 #endif
 
 #if defined(__cplusplus)
