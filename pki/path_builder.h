819da3b5f1a5a0b1edebd7812ab13873d229ab54
Bob Beck <bbe@google.com>
2023-11-21 13:58:30 -0700

    Add a debugging log facility to the delegate.
    This is to permit us to receive log messages with an integer
    log level from the path builder, and we can then potentially
    use them somewhere else (selectively based on the log level if
    we so choose) to put the messages somewhere BoringSSL has no
    knowledge of. In chrome this will enable us to send path builder
    diagnostics to netlog.
    
    Bug: 663
    
    Change-Id: I5c2e04a25db1e41c1b825f68ec68e4e587aa2994

--- pki/path_builder.h
+++ pki/path_builder.h
@@ -99,6 +99,18 @@ class OPENSSL_EXPORT CertPathBuilderDelegate
   // paths. Delegates can cause path building to stop and return indicating
   // the deadline was exceeded by returning true from this function.
   virtual bool IsDeadlineExpired() = 0;
+
+  // This is called during path building to decide if debug logs will be passed
+  // sent from the path building process. No calls to DebugLog (below) will
+  // be made unless this returns true.
+  virtual bool IsDebugLogEnabled() = 0;
+
+  // This is called to send a debug log string |msg| and an integer |log_level|
+  // to the delegate. These are only called if IsDebugLogEnabled returns true.
+  // |log_level| is an integer with larger numbers indicating increasing levels
+  // of expected log detail and verbosity. The delgate may use this value to
+  // decide what level of verbosity it wishes to process.
+  virtual void DebugLog(int log_level, std::string_view msg) = 0;
 };
 
 // Checks whether a certificate is trusted by building candidate paths to trust
