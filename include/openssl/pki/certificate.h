4f3f1199bd2cc2cd9762814c2e2c7ea72fe32449
Bob Beck <bbe@google.com>
2023-11-23 23:08:58 +0000

    Add public API for a certificate.
    This is effectively a derivation of certificate.cc from
    chromium_certificate_verifier, without absl.
    
    Bug: 660
    Change-Id: I628827a00ab80a733582eee5a25e048dd65bf6b9

---
+++ include/openssl/pki/certificate.h
@@ -0,0 +1,87 @@
+/* Copyright (c) 2023, Google Inc.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+
+#ifndef OPENSSL_HEADER_BSSL_PKI_CERTIFICATE_H_
+#define OPENSSL_HEADER_BSSL_PKI_CERTIFICATE_H_
+
+#include <memory>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include <openssl/base.h>
+#include <openssl/span.h>
+
+namespace bssl {
+
+struct CertificateInternals;
+
+// Certificate represents a parsed X.509 certificate. It includes accessors for
+// the various things that one might want to extract from a certificate,
+class OPENSSL_EXPORT Certificate {
+ public:
+  Certificate(Certificate&& other);
+  Certificate(const Certificate& other) = delete;
+  ~Certificate();
+  Certificate& operator=(const Certificate& other) = delete;
+
+  // FromDER returns a certificate from an DER-encoded X.509 object in |der|.
+  // In the event of a failure, it will return no value, and |out_diagnostic|
+  // may be set to a string of human readable debugging information if
+  // information abou the failure is available.
+  static std::optional<std::unique_ptr<Certificate>> FromDER(
+      std::string_view der, std::string *out_diagnostic);
+
+  // FromPEM returns a certificate from the first CERTIFICATE PEM block in
+  // |pem|. In the event of a failure, it will return no value, and
+  // |out_diagnostic| may be set to a string of human readable debugging
+  // informtion if informaiton about the failuew is available.
+  static std::optional<std::unique_ptr<Certificate>> FromPEM(
+      std::string_view pem, std::string *out_diagnostic);
+
+  // IsSelfIssued returns true if the certificate is "self-issued" per RFC 5280
+  // section 6.1. I.e. that the subject and issuer names are equal after
+  // canonicalization. This is often called "self-signed".
+  //
+  // Note that other contexts may define "self-signed" differently. OpenSSL, for
+  // example, also considers matching authority and subject key IDs to make a
+  // certificate self-signed. Go compares the names, but without
+  // canonicalization. No doubt some actually check that the signature is valid.
+  bool IsSelfIssued() const;
+
+  // Validity specifies the temporal validity of a cerificate, expressed in
+  // POSIX time values of seconds since the POSIX epoch. The certificate is
+  // valid at posix time t in second granularity, where not_before <= t <=
+  // not_after.
+  struct Validity {
+    int64_t not_before;
+    int64_t not_after;
+  };
+
+  Validity GetValidity() const;
+
+  // The binary, big-endian, DER representation of the certificate serial
+  // number. It may include a leading 00 byte.
+  bssl::Span<const uint8_t> GetSerialNumber() const;
+
+ private:
+  explicit Certificate(std::unique_ptr<CertificateInternals> internals);
+
+  std::unique_ptr<CertificateInternals> internals_;
+};
+
+}  // namespace bssl
+
+#endif  // OPENSSL_HEADER_BSSL_PKI_CERTIFICATE_H_
---
+++ pki/certificate.cc
@@ -0,0 +1,121 @@
+/* Copyright (c) 2023, Google Inc.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+
+#include <optional>
+#include <string_view>
+
+#include <openssl/pki/certificate.h>
+#include <openssl/pool.h>
+
+#include "cert_errors.h"
+#include "encode_values.h"
+#include "parsed_certificate.h"
+#include "pem.h"
+#include "parse_values.h"
+
+namespace bssl {
+
+namespace {
+
+std::optional<std::shared_ptr<const bssl::ParsedCertificate>>
+ParseCertificateFromDer(std::string_view cert, std::string *out_diagnostic) {
+  bssl::ParseCertificateOptions default_options{};
+  // We follow Chromium in setting |allow_invalid_serial_numbers| in order to
+  // not choke on 21-byte serial numbers, which are common.  davidben explains
+  // why:
+  //
+  // The reason for the discrepancy is that unsigned numbers with the high bit
+  // otherwise set get an extra 0 byte in front to keep them positive. So if you
+  // do:
+  //    var num [20]byte
+  //    fillWithRandom(num[:])
+  //    serialNumber := new(big.Int).SetBytes(num[:])
+  //    encodeASN1Integer(serialNumber)
+  //
+  // Then half of your serial numbers will be encoded with 21 bytes. (And
+  // 1/512th will have 19 bytes instead of 20.)
+  default_options.allow_invalid_serial_numbers = true;
+
+  bssl::UniquePtr<CRYPTO_BUFFER> buffer(CRYPTO_BUFFER_new(
+      reinterpret_cast<const uint8_t*>(cert.data()), cert.size(), nullptr));
+  bssl::CertErrors errors;
+  std::shared_ptr<const bssl::ParsedCertificate> parsed_cert(
+      bssl::ParsedCertificate::Create(std::move(buffer), default_options, &errors));
+  if (!parsed_cert) {
+    *out_diagnostic = errors.ToDebugString();
+    return {};
+  }
+  return parsed_cert;
+}
+
+} // namespace
+
+struct CertificateInternals {
+  std::shared_ptr<const bssl::ParsedCertificate> cert;
+};
+
+Certificate::Certificate(std::unique_ptr<CertificateInternals> internals)
+    : internals_(std::move(internals)) {}
+Certificate::~Certificate() = default;
+Certificate::Certificate(Certificate&& other) = default;
+
+std::optional<std::unique_ptr<Certificate>> Certificate::FromDER(
+    std::string_view der, std::string* out_diagnostic) {
+  std::optional<
+      std::shared_ptr<const bssl::ParsedCertificate>>
+      result = ParseCertificateFromDer(der, out_diagnostic);
+  if (!result.has_value()) {
+    return {};
+  }
+
+  auto internals = std::make_unique<CertificateInternals>();
+  internals->cert = result.value();
+  std::unique_ptr<Certificate> ret(new Certificate(std::move(internals)));
+  return std::move(ret);
+}
+
+std::optional<std::unique_ptr<Certificate>> Certificate::FromPEM(
+    std::string_view pem, std::string *out_diagnostic) {
+  bssl::PEMTokenizer tokenizer(pem, {"CERTIFICATE"});
+  if (!tokenizer.GetNext()) {
+    return {};
+  }
+
+  return FromDER(tokenizer.data(), out_diagnostic);
+}
+
+bool Certificate::IsSelfIssued() const {
+  return internals_->cert->normalized_subject() ==
+         internals_->cert->normalized_issuer();
+}
+
+Certificate::Validity Certificate::GetValidity() const {
+  Certificate::Validity validity;
+
+  // As this is a previously parsed certificate, we know the not_before
+  // and not after are valid, so these conversions can not fail.
+  (void) GeneralizedTimeToPosixTime(
+      internals_->cert->tbs().validity_not_before, &validity.not_before);
+  (void) GeneralizedTimeToPosixTime(
+      internals_->cert->tbs().validity_not_after, &validity.not_after);
+  return validity;
+}
+
+bssl::Span<const uint8_t> Certificate::GetSerialNumber() const {
+  const uint8_t* data = internals_->cert->tbs().serial_number.UnsafeData();
+  const size_t length = internals_->cert->tbs().serial_number.Length();
+  return bssl::Span<const uint8_t>(data, length);
+}
+
+}  // namespace boringssl
