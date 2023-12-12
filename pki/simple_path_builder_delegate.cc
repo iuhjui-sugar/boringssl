7229d0ea5024bd36bd8aa9be6a260a343d21fb07
Bob Beck <bbe@google.com>
2023-12-12 18:49:49 +0000

    Move signature_verify_cache.h to openssl/pki as public api
    Bug: 660
    Change-Id: Ia0ab6e9d16da2a86994ce0dfc6a086e43b79c9ae

--- pki/simple_path_builder_delegate.cc
+++ pki/simple_path_builder_delegate.cc
@@ -12,10 +12,10 @@
 #include <openssl/evp.h>
 #include <openssl/nid.h>
 #include <openssl/rsa.h>
+#include <openssl/pki/signature_verify_cache.h>
 #include "cert_error_params.h"
 #include "cert_errors.h"
 #include "signature_algorithm.h"
-#include "signature_verify_cache.h"
 #include "verify_signed_data.h"
 
 namespace bssl {
