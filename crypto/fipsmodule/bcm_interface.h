57514e28c7d6dee234a28655fa83a3a27754e8f7
Bob Beck <bbe@google.com>
2024-04-29 23:15:36 +0000

    Move urandom and OS crypto support out of BCM
    BCM uses only passive entropy, in that from BCM's
    point of view, entropy is requested and obtained
    from an external source, and fed to BCM's CTR_DRBG
    as required.
    
    We introduce an interface boundary defined in
    fipsmodule/bcm_interface.h between BCM and the rest
    of libcrypto.  Tthe actual gathering of entropy in an
    OS specific manner is handled outside of BCM by libcrypto,
    while the CTR_DRBG remains in BCM. This includes the
    introduction of a bcm_status type to be used as the
    return value for functions inside BCM called from the rest
    of libcrypto.
    
    Support funcitons called by BCM provided by libcrypto
    are now prototyped in bcm_support.h
    
    This changes BCM to use only BCM_RAND_bytes instead of
    the public RAND_bytes. BCM no longer calls
    any public RAND functions directly, and moves the
    public RAND functions not used in BCM outside to
    rand_extra.
    
    Bug: 723
    
    Change-Id: I6b618dfe4df257f67971e88cbd79126c837e21d6

---
+++ crypto/fipsmodule/bcm_interface.h
@@ -0,0 +1,85 @@
+/* Copyright (c) 2024, Google Inc.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+
+#ifndef OPENSSL_HEADER_CRYPTO_BCM_INTERFACE_H
+#define OPENSSL_HEADER_CRYPTO_BCM_INTERFACE_H
+
+// This header will eventually become the interface between BCM and the
+// rest of libcrypto. More cleanly separating the two is still a work in
+// progress (see https://crbug.com/boringssl/722) so, at the moment, we
+// consider this no different from any other header in BCM.
+//
+// Over time, calls from libcrypto to BCM will all move to this header
+// and the separation will become more meaningful.
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#define BCM_SUCCESS_APPROVED (0)
+#define BCM_SUCCESS_NOT_APPROVED (1)
+
+// Enumerated types for return values from bcm functions, both infallible
+// and fallible functions. Two success values are used to correspond to the
+// FIPS service indictor. For the moment, the official service indiacator
+// remains the counter, not these values. Once we fully transition to
+// these return values from bcm we will change that.
+enum bcm_infallible_t {
+  bcm_infallible_approved = BCM_SUCCESS_APPROVED,
+  bcm_infallible_not_approved = BCM_SUCCESS_NOT_APPROVED,
+};
+
+enum bcm_status_t {
+  bcm_status_approved = BCM_SUCCESS_APPROVED,
+  bcm_status_not_approved = BCM_SUCCESS_NOT_APPROVED,
+
+  // Failure codes
+  bcm_status_failure = 2,
+};
+typedef enum bcm_status_t bcm_status;
+typedef enum bcm_infallible_t bcm_infallible;
+
+OPENSSL_INLINE int bcm_success(bcm_status status) {
+  return status == BCM_SUCCESS_APPROVED || status == BCM_SUCCESS_NOT_APPROVED;
+}
+
+#if defined(BORINGSSL_FIPS)
+
+// We overread from /dev/urandom or RDRAND by a factor of 10 and XOR to whiten.
+#define BORINGSSL_FIPS_OVERREAD 10
+
+#endif  // BORINGSSL_FIPS
+
+// BCM_RAND_bytes is the same as the public |RAND_bytes| function, but
+// returns bcm_infallible_approved.
+bcm_infallible BCM_RAND_bytes(uint8_t *out, size_t out_len);
+
+// BCM_RAND_bytes_hwrng attempts to fill |out| with |len| bytes of entropy from
+// the CPU hardware random number generator if one is present. If |fast| is
+// non-zero this will only succeed if the CPU hwrng is thought to be "fast".
+// bcm_status_approvedD is returned on success, and a failure status is
+// returned otherwise.
+bcm_status BCM_RAND_bytes_hwrng(uint8_t *out, size_t len, int fast);
+
+// RAND_bytes_with_additional_data samples from the RNG after mixing 32 bytes
+// from |user_additional_data| in.
+void BCM_RAND_bytes_with_additional_data(
+    uint8_t *out, size_t out_len, const uint8_t user_additional_data[32]);
+
+
+#if defined(__cplusplus)
+}  // extern C
+#endif
+
+#endif  // OPENSSL_HEADER_CRYPTO_BCM_INTERFACE_H
