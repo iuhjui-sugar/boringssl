1a6b65b133052f2b4f754665a7345e28eb714c1d
David Benjamin <davidben@google.com>
2024-05-15 16:58:48 -0400

    Add an executable-relative option for finding test data
    Android, in both their Soong and emulator builds, seem to expect the
    test data to be installed relative to the executable. They then require
    linking against some "base" library, but the two builds have completely
    divergent versions of the library!
    
    Rather than have to maintain different calling code, just give up and
    implement this feature ourselves.
    
    Change-Id: I50ae2c759c58e238f5ad9c17b7b82d3595ce2e49

--- crypto/test/test_data.cc
+++ crypto/test/test_data.cc
@@ -12,6 +12,8 @@
  * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
  * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
 
+#if !defined(BORINGSSL_CUSTOM_GET_TEST_DATA)
+
 #include "test_data.h"
 
 #include <stdio.h>
@@ -19,15 +21,50 @@
 
 #include "file_util.h"
 
+#if defined(OPENSSL_LINUX)
+#include <unistd.h>
+#endif
+
+#if defined(OPENSSL_APPLE)
+#include <mach-o/dyld.h>
+#endif
+
+#if defined(OPENSSL_WINDOWS)
+#include <windows.h>
+#endif
+
 #if defined(BORINGSSL_USE_BAZEL_RUNFILES)
 #include "tools/cpp/runfiles/runfiles.h"
 
 using bazel::tools::cpp::runfiles::Runfiles;
 #endif
 
-#if !defined(BORINGSSL_CUSTOM_GET_TEST_DATA)
-std::string GetTestData(const char *path) {
+
+static std::string ReadFileOrExit(const char *path) {
+  ScopedFILE file(fopen(path, "rb"));
+  if (file == nullptr) {
+    fprintf(stderr, "Could not open '%s'.\n", path);
+    abort();
+  }
+
+  std::string ret;
+  for (;;) {
+    char buf[512];
+    size_t n = fread(buf, 1, sizeof(buf), file.get());
+    if (n == 0) {
+      if (feof(file.get())) {
+        return ret;
+      }
+      fprintf(stderr, "Error reading from '%s'.\n", path);
+      abort();
+    }
+    ret.append(buf, n);
+  }
+}
+
 #if defined(BORINGSSL_USE_BAZEL_RUNFILES)
+
+std::string GetTestData(const char *path) {
   std::string error;
   std::unique_ptr<Runfiles> runfiles(Runfiles::CreateForTest(&error));
   if (runfiles == nullptr) {
@@ -40,33 +77,100 @@ std::string GetTestData(const char *path) {
     fprintf(stderr, "Could not find runfile '%s'.\n", path);
     abort();
   }
+
+  return ReadFileOrExit(full_path.c_str());
+}
+
+#else  // !BORINGSSL_USE_BAZEL_RUNFILES
+
+static std::string GetExecutablePath() {
+#if defined(OPENSSL_LINUX)
+  char buf[4096];
+  ssize_t ret = readlink("/proc/self/exe", buf, sizeof(buf));
+  if (ret < 0) {
+    perror("readlink");
+    abort();
+  }
+  if (ret == sizeof(buf)) {
+    // readlink silently truncates if the buffer is too small. Just abort and
+    // we'll increase this if we ever need to.
+    fprintf(stderr, "Buffer was too small for readlink.\n");
+    abort();
+  }
+  return std::string(buf, ret);
+#elif defined(OPENSSL_APPLE)
+  char buf[4096];
+  uint32_t buf_len = sizeof(buf);
+  if (_NSGetExecutablePath(buf, &buf_len) != 0) {
+    fprintf(stderr, "_NSGetExecutablePath failed.\n");
+    abort();
+  }
+  return buf;
+#elif defined(OPENSSL_WINDOWS)
+  char buf[4096];
+  DWORD ret = GetModuleFileNameA(nullptr, buf, sizeof(buf));
+  if (ret == 0 || ret == sizeof(buf)) {
+    // GetModuleFileNameA returns the buffer size on truncation.
+    fprintf(stderr, "GetModuleFileNameA failed.\n");
+    abort();
+  }
+  return buf;
 #else
+  fprintf(stderr,
+          "BORINGSSL_TEST_DATA_FROM_EXECUTABLE_PATH not supported on this "
+          "platform.\n");
+  abort();
+#endif
+}
+
+static void AppendPath(std::string *path, const std::string &component) {
+  if (component.empty() || component == ".") {
+    return;
+  }
+  if (!path->empty() && path->back() != '/') {
+    path->push_back('/');
+  }
+  path->append(component);
+}
+
+std::string GetTestData(const char *path) {
   const char *root = getenv("BORINGSSL_TEST_DATA_ROOT");
   root = root != nullptr ? root : ".";
 
-  std::string full_path = root;
-  full_path.push_back('/');
-  full_path.append(path);
+  std::string full_path;
+  if (getenv("BORINGSSL_TEST_DATA_FROM_EXECUTABLE_PATH") != nullptr) {
+    // Remove the last component to get the executable directory.
+    full_path = GetExecutablePath();
+#if defined(OPENSSL_WINDOWS)
+    size_t idx = full_path.find_last_of("/\\");
+#else
+    size_t idx = full_path.find_last_of('/');
 #endif
-
-  ScopedFILE file(fopen(full_path.c_str(), "rb"));
-  if (file == nullptr) {
-    fprintf(stderr, "Could not open '%s'.\n", full_path.c_str());
-    abort();
-  }
-
-  std::string ret;
-  for (;;) {
-    char buf[512];
-    size_t n = fread(buf, 1, sizeof(buf), file.get());
-    if (n == 0) {
-      if (feof(file.get())) {
-        return ret;
-      }
-      fprintf(stderr, "Error reading from '%s'.\n", full_path.c_str());
+    if (idx == std::string::npos) {
+      fprintf(stderr, "Invalid executable path: %s\n", full_path.c_str());
       abort();
     }
-    ret.append(buf, n);
+    full_path.resize(idx);
+
+    // It doesn't make sense to request an executable-relative path and then set
+    // an absolute root. This check doesn't handle Windows, but it's not
+    // necessary for correctness, just catches some bugs.
+    if (*root == '/') {
+      fprintf(stderr,
+              "Unexpected absolute BORINGSSL_TEST_DATA_ROOT used with "
+              "BORINGSSL_TEST_DATA_FROM_EXECUTABLE_PATH.\n");
+      abort();
+    }
+
+    AppendPath(&full_path, root);
+  } else {
+    full_path = root;
   }
+
+  AppendPath(&full_path, path);
+  return ReadFileOrExit(full_path.c_str());
 }
+
+#endif  // BORINGSSL_USE_BAZEL_RUNFILES
+
 #endif  // !BORINGSSL_CUSTOM_GET_TEST_DATA
