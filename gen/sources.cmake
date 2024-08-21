6434f526b8deb9a11e6477d161846952b2c35e9b
David Benjamin <davidben@google.com>
2024-08-21 16:38:57 -0400

    Specify the list of Rust files with build.json
    In principle this should be derivable from lib.rs directly, but Rust
    made this surprisingly difficult, so we'll just do it with wildcards.
    
    Bug: 42290412
    Change-Id: I3153613bdddf62eaa0e752a4a24192e2c484a95c

--- gen/sources.cmake
+++ gen/sources.cmake
@@ -266,6 +266,38 @@ set(
 )
 
 set(
+  BSSL_CRYPTO_RUST_SOURCES
+
+  rust/bssl-crypto/src/aead.rs
+  rust/bssl-crypto/src/aes.rs
+  rust/bssl-crypto/src/cipher/aes_cbc.rs
+  rust/bssl-crypto/src/cipher/aes_ctr.rs
+  rust/bssl-crypto/src/cipher/mod.rs
+  rust/bssl-crypto/src/digest.rs
+  rust/bssl-crypto/src/ec.rs
+  rust/bssl-crypto/src/ecdh.rs
+  rust/bssl-crypto/src/ecdsa.rs
+  rust/bssl-crypto/src/ed25519.rs
+  rust/bssl-crypto/src/hkdf.rs
+  rust/bssl-crypto/src/hmac.rs
+  rust/bssl-crypto/src/hpke.rs
+  rust/bssl-crypto/src/lib.rs
+  rust/bssl-crypto/src/macros.rs
+  rust/bssl-crypto/src/mem.rs
+  rust/bssl-crypto/src/rand.rs
+  rust/bssl-crypto/src/rsa.rs
+  rust/bssl-crypto/src/scoped.rs
+  rust/bssl-crypto/src/test_helpers.rs
+  rust/bssl-crypto/src/x25519.rs
+)
+
+set(
+  BSSL_SYS_RUST_SOURCES
+
+  rust/bssl-sys/src/lib.rs
+)
+
+set(
   CRYPTO_SOURCES
 
   crypto/asn1/a_bitstr.c
